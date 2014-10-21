8039c616652c63b2293f76c6fcc23c0c8758b3ce
Petr Tesarik <ptesarik@suse.cz>
2014-10-21 17:09:36 +0200

    Add byteorder macros for compatibility with older glibc versions
    These macros must be defined here if missing from the header files.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.cz>

--- kdumpfile-priv.h
+++ kdumpfile-priv.h
@@ -95,6 +95,44 @@ int kdump_uts_looks_sane(struct new_utsname *uts);
 int kdump_uncompress_rle(unsigned char *dst, size_t *pdstlen,
 			 const unsigned char *src, size_t srclen);
 
+/* Older glibc didn't have the byteorder macros */
+#ifndef be16toh
+
+#include <byteswap.h>
+
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define htobe16(x) bswap_16(x)
+#  define htole16(x) (x)
+#  define be16toh(x) bswap_16(x)
+#  define le16toh(x) (x)
+
+#  define htobe32(x) bswap_32(x)
+#  define htole32(x) (x)
+#  define be32toh(x) bswap_32(x)
+#  define le32toh(x) (x)
+
+#  define htobe64(x) bswap_64(x)
+#  define htole64(x) (x)
+#  define be64toh(x) bswap_64(x)
+#  define le64toh(x) (x)
+# else
+#  define htobe16(x) (x)
+#  define htole16(x) bswap_16(x)
+#  define be16toh(x) (x)
+#  define le16toh(x) bswap_16(x)
+
+#  define htobe32(x) (x)
+#  define htole32(x) bswap_32(x)
+#  define be32toh(x) (x)
+#  define le32toh(x) bswap_32(x)
+
+#  define htobe64(x) (x)
+#  define htole64(x) bswap_64(x)
+#  define be64toh(x) (x)
+#  define le64toh(x) bswap_64(x)
+# endif
+#endif
+
 /* Inline utility functions */
 
 static inline unsigned
