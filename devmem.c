5cf4f89ae1056647722b8184b37bc3e7ddba6b69
Petr Tesarik <ptesarik@suse.cz>
2014-10-23 15:52:50 +0200

    live source: use kdump_set_arch()
    Let's use the same unified access for live sources: kdump_set_arch()
    may be extended, and it's a pain to keep updating devmem.c as a special
    case.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.cz>

--- devmem.c
+++ devmem.c
@@ -47,6 +47,7 @@ static kdump_status
 devmem_probe(kdump_ctx *ctx)
 {
 	struct stat st;
+	kdump_status ret;
 
 	if (fstat(ctx->fd, &st))
 		return kdump_syserr;
@@ -57,35 +58,37 @@ devmem_probe(kdump_ctx *ctx)
 		return kdump_unsupported;
 
 #if defined(__x86_64__)
-	ctx->arch = ARCH_X86_64;
+	ret = kdump_set_arch(ctx, ARCH_X86_64);
 #elif defined(__i386__)
-	ctx->arch = ARCH_X86;
+	ret = kdump_set_arch(ctx, ARCH_X86);
 #elif defined(__powerpc64__)
 # if __BYTE_ORDER == __LITTLE_ENDIAN
-	ctx->arch = ARCH_PPC64LE;
+	ret = kdump_set_arch(ctx, ARCH_PPC64LE);
 # else
-	ctx->arch = ARCH_PPC64;
+	ret = kdump_set_arch(ctx, ARCH_PPC64);
 # endif
 #elif defined(__powerpc__)
-	ctx->arch = ARCH_PPC;
+	ret = kdump_set_arch(ctx, ARCH_PPC);
 #elif defined(__s390x__)
-	ctx->arch = ARCH_S390X;
+	ret = kdump_set_arch(ctx, ARCH_S390X);
 #elif defined(__s390__)
-	ctx->arch = ARCH_S390;
+	ret = kdump_set_arch(ctx, ARCH_S390);
 #elif defined(__ia64__)
-	ctx->arch = ARCH_IA64;
+	ret = kdump_set_arch(ctx, ARCH_IA64);
 #elif defined(__aarch64__)
-	ctx->arch = ARCH_AARCH64;
+	ret = kdump_set_arch(ctx, ARCH_AARCH64);
 #elif defined(__arm__)
-	ctx->arch = ARCH_ARM;
+	ret = kdump_set_arch(ctx, ARCH_ARM);
 #elif defined(__alpha__)
-	ctx->arch = ARCH_ALPHA;
+	ret = kdump_set_arch(ctx, ARCH_ALPHA);
 #else
-	ctx->arch = ARCH_UNKNOWN;
+	ret = kdump_set_arch(ctx, ARCH_UNKNOWN);
 #endif
+	if (ret != kdump_ok)
+		return ret;
+
 	ctx->format = "live source";
 	ctx->endian = __BYTE_ORDER;
-	ctx->ptr_size = sizeof(void*);
 	ctx->page_size = sysconf(_SC_PAGESIZE);
 
 	return kdump_ok;
