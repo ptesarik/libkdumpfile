ptesarik/libkdumpfile

commit aa0fde718a915250d1543fec9a72b90f6dc2afca

Use a fixed-size buffer in set_error
Do not allocate the error string dynamically, but use an on-stack buffer.
This also fixes a memory leak.

Some minor code cleanups.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/util.c
@@ -40,8 +40,10 @@ set_error(kdump_ctx *ctx, kdump_status ret, const char *msgfmt, ...)
 {
 	static const char failure[] = "(set_error failed)";
 	static const char delim[] = { ':', ' ' };
+	static const char ellipsis[] = { '.', '.', '.' };
 
 	va_list ap;
+	char msgbuf[ERRBUF];
 	const char *msg;
 	int msglen;
 	size_t remain;
@@ -50,10 +52,16 @@ set_error(kdump_ctx *ctx, kdump_status ret, const char *msgfmt, ...)
 		return ret;
 
 	va_start(ap, msgfmt);
-	msglen = vasprintf((char**)&msg, msgfmt, ap);
+	msglen = snprintf(msgbuf, sizeof(msgbuf), msgfmt, ap);
+	va_end(ap);
+
 	if (msglen < 0) {
 		msg = failure;
 		msglen = sizeof(failure) - 1;
+	} else {
+		msg = msgbuf;
+		if (msglen >= sizeof(msgbuf))
+			msglen = sizeof(msgbuf) - 1;
 	}
 
 	if (!ctx->err_str) {
@@ -75,12 +83,9 @@ set_error(kdump_ctx *ctx, kdump_status ret, const char *msgfmt, ...)
 	} else {
 		ctx->err_str = ctx->err_buf;
 		memcpy(ctx->err_str, msg + msglen - remain, remain);
+		memcpy(ctx->err_str, ellipsis, sizeof(ellipsis));
 	}
 
-	if (remain < msglen)
-		memcpy(ctx->err_buf, "...", 3);
-
-	va_end(ap);
 	return ret;
 }
 

commit 216f948a5103bee58f6ae6f1d45c95993ab76ecd

Fix a silly bug in set_error()
The set_error() function uses va_list to pass the variadic arguments
to a printf() function, but it uses the wrong function for va_list...

Regression introduced by commit aa0fde718a915250d1543fec9a72b90f6dc2afca.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/util.c
@@ -52,7 +52,7 @@ set_error(kdump_ctx *ctx, kdump_status ret, const char *msgfmt, ...)
 		return ret;
 
 	va_start(ap, msgfmt);
-	msglen = snprintf(msgbuf, sizeof(msgbuf), msgfmt, ap);
+	msglen = vsnprintf(msgbuf, sizeof(msgbuf), msgfmt, ap);
 	va_end(ap);
 
 	if (msglen < 0) {