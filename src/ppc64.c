87172572c49dcf79376c0e7b6485e454a44af8e0
Petr Tesarik <ptesarik@suse.com>
2016-02-26 17:32:38 +0100

    ppc64: Big clean up
    Rename l1..l4 to pte..pgd.
    Replace kdump_readp() with read_u64().
    Provide more information in the error messages.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- src/ppc64.c
+++ src/ppc64.c
@@ -44,23 +44,23 @@
  */
 struct paging_form {
 	unsigned page_bits;
-	unsigned l1_bits;
-	unsigned l2_bits;
-	unsigned l3_bits;
-	unsigned l4_bits;
+	unsigned pte_bits;
+	unsigned pmd_bits;
+	unsigned pud_bits;
+	unsigned pgd_bits;
 };
 
 struct ppc64_data {
 	struct paging_form pgform;
 	struct {
-		kdump_vaddr_t l2_mask;
-		kdump_vaddr_t l3_mask;
-		kdump_vaddr_t l4_mask;
+		kdump_vaddr_t pmd_mask;
+		kdump_vaddr_t pud_mask;
+		kdump_vaddr_t pgd_mask;
 
-		unsigned l1_shift;
-		unsigned l2_shift;
-		unsigned l3_shift;
-		unsigned l4_shift;
+		unsigned pte_shift;
+		unsigned pmd_shift;
+		unsigned pud_shift;
+		unsigned pgd_shift;
 
 		int rpn_shift;	/**< Real Page Number shift. */
 
@@ -144,10 +144,10 @@ static const struct attr_template reg_names[] = {
  */
 struct vaddr_parts {
 	unsigned off;		/**< Offset inside page */
-	unsigned l1;		/**< Level-1 page table index */
-	unsigned l2;		/**< Level-2 page table index */
-	unsigned l3;		/**< Level-3 page table index */
-	unsigned l4;		/**< Level-4 page table index */
+	unsigned pte;		/**< PTE index */
+	unsigned pmd;		/**< PMD index */
+	unsigned pud;		/**< PUD index */
+	unsigned pgd;		/**< PGD index */
 };
 
 /**  Split a virtual addres into individual parts.
@@ -161,13 +161,13 @@ vaddr_split(const struct paging_form *pgform, kdump_vaddr_t addr,
 {
 	parts->off = addr & (((kdump_vaddr_t)1 << pgform->page_bits) - 1);
 	addr >>= pgform->page_bits;
-	parts->l1 = addr & (((kdump_vaddr_t)1 << pgform->l1_bits) - 1);
-	addr >>= pgform->l1_bits;
-	parts->l2 = addr & (((kdump_vaddr_t)1 << pgform->l2_bits) - 1);
-	addr >>= pgform->l2_bits;
-	parts->l3 = addr & (((kdump_vaddr_t)1 << pgform->l3_bits) - 1);
-	addr >>= pgform->l3_bits;
-	parts->l4 = addr & (((kdump_vaddr_t)1 << pgform->l4_bits) - 1);
+	parts->pte = addr & (((kdump_vaddr_t)1 << pgform->pte_bits) - 1);
+	addr >>= pgform->pte_bits;
+	parts->pmd = addr & (((kdump_vaddr_t)1 << pgform->pmd_bits) - 1);
+	addr >>= pgform->pmd_bits;
+	parts->pud = addr & (((kdump_vaddr_t)1 << pgform->pud_bits) - 1);
+	addr >>= pgform->pud_bits;
+	parts->pgd = addr & (((kdump_vaddr_t)1 << pgform->pgd_bits) - 1);
 }
 
 /**  Get the translated address using its PTE and page shift.
@@ -224,28 +224,22 @@ ppc64_vtop_hugepd(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr,
 		  uint64_t hpde, unsigned pdshift)
 {
 	struct ppc64_data *archdata = ctx->archdata;
-	kdump_vaddr_t mask, base;
+	kdump_vaddr_t mask, ptep;
 	unsigned long idx;
 	uint64_t pte;
-	size_t sz;
 	kdump_status res;
 
-	base = (hpde & ~HUGEPD_SHIFT_MASK) | PD_HUGE;
 	mask = ((kdump_vaddr_t)1 << pdshift) - 1;
 	idx = (vaddr & mask) >> hugepd_shift(hpde);
+	ptep = ((hpde & ~HUGEPD_SHIFT_MASK) | PD_HUGE) +
+		idx * sizeof(uint64_t);
 
-	sz = sizeof(uint64_t);
-	res = kdump_readp(ctx,
-			  KDUMP_KVADDR, base + idx * sizeof(uint64_t),
-			  &pte, &sz);
+	res = read_u64(ctx, KDUMP_KVADDR, ptep, "huge PTE", &pte);
 	if (res != kdump_ok)
-		return set_error(ctx, res, "Cannot read hugepage");
-
-	pte = dump64toh(ctx, pte);
+		return res;
 	if (!pte)
-		return set_error(ctx, kdump_nodata,
-				 "Page not present: pte[%lu] = 0x%llx",
-				 idx, (unsigned long long) pte);
+		return set_error(ctx, kdump_nodata, "huge_pte[%lu] is none",
+				 idx);
 
 	*paddr = vtop_final(vaddr, pte, archdata->pg.rpn_shift,
 			    hugepd_shift(hpde));
@@ -257,86 +251,83 @@ ppc64_vtop(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr)
 {
 	struct ppc64_data *archdata = ctx->archdata;
 	struct vaddr_parts split;
-	kdump_vaddr_t l1e, l2e, l3e, l4e, e;
-	size_t ps = kdump_ptr_size(ctx);
+	kdump_vaddr_t pgdp, pudp, pmdp, ptep;
+	uint64_t pgd, pud, pmd, pte;
 	kdump_status res;
 
 	vaddr_split(&archdata->pgform, vaddr, &split);
 
-	res = kdump_readp(ctx, KDUMP_KVADDR, archdata->pg.pg + split.l4*ps,
-			  &l4e, &ps);
+	pgdp = archdata->pg.pg +
+		split.pgd * sizeof(uint64_t);
+	res = read_u64(ctx, KDUMP_KVADDR, pgdp, "PGD entry", &pgd);
 	if (res != kdump_ok)
-		return set_error(ctx, res, "Cannot read L4");
+		return res;
+	if (!pgd)
+		return set_error(ctx, kdump_nodata, "pgd[%u] is none",
+				 split.pgd);
 
-	l4e = dump64toh(ctx, l4e);
-
-	if (!l4e) goto notfound;
-
-	if (is_hugepte(l4e)) {
-		*paddr = vtop_final(vaddr, l4e, archdata->pg.rpn_shift,
-				    archdata->pg.l4_shift);
+	if (is_hugepte(pgd)) {
+		*paddr = vtop_final(vaddr, pgd, archdata->pg.rpn_shift,
+				    archdata->pg.pgd_shift);
 		return kdump_ok;
 	}
-	if (is_hugepd(l4e))
+	if (is_hugepd(pgd))
 		return ppc64_vtop_hugepd(ctx, vaddr, paddr,
-					 l4e, archdata->pg.l4_shift);
+					 pgd, archdata->pg.pgd_shift);
 
-	e = (l4e & (~archdata->pg.l4_mask)) + split.l3 * ps;
-
-	if (archdata->pgform.l3_bits != 0) {
-		res = kdump_readp(ctx, KDUMP_KVADDR, e, &l3e, &ps);
+	pudp = (pgd & (~archdata->pg.pgd_mask));
+	if (archdata->pgform.pud_bits != 0) {
+		pudp += split.pud * sizeof(uint64_t);
+		res = read_u64(ctx, KDUMP_KVADDR, pudp, "PUD entry", &pud);
 		if (res != kdump_ok)
-			return set_error(ctx, res, "Cannot read L3");
+			return res;
 
-		l3e = dump64toh(ctx, l3e);
+		if (!pud)
+			return set_error(ctx, kdump_nodata, "pud[%u] is none",
+					 split.pud);
 
-		if (is_hugepte(l3e)) {
-			*paddr = vtop_final(vaddr, l3e, archdata->pg.rpn_shift,
-					    archdata->pg.l3_shift);
+		if (is_hugepte(pud)) {
+			*paddr = vtop_final(vaddr, pud, archdata->pg.rpn_shift,
+					    archdata->pg.pud_shift);
 			return kdump_ok;
 		}
-		if (is_hugepd(l3e))
+		if (is_hugepd(pud))
 			return ppc64_vtop_hugepd(ctx, vaddr, paddr,
-						 l3e, archdata->pg.l3_shift);
+						 pud, archdata->pg.pud_shift);
+
+		pmdp = (pud & (~archdata->pg.pud_mask));
 	} else
-		l3e = e;
+		pmdp = pudp;
 
-	e = (l3e & (~archdata->pg.l3_mask)) + split.l2 *ps;
-
-	res = kdump_readp(ctx, KDUMP_KVADDR, e, &l2e, &ps);
+	pmdp += split.pmd * sizeof(uint64_t);
+	res = read_u64(ctx, KDUMP_KVADDR, pmdp, "PMD entry", &pmd);
 	if (res != kdump_ok)
-		return set_error(ctx, res, "Cannot read L2");
+		return res;
+	if (!pmd)
+		return set_error(ctx, kdump_nodata, "pmd[%u] is none",
+				 split.pmd);
 
-	l2e = dump64toh(ctx, l2e);
-
-	if (!l2e) goto notfound;
-
-	if (is_hugepte(l2e)) {
-		*paddr = vtop_final(vaddr, l2e, archdata->pg.rpn_shift,
-				    archdata->pg.l2_shift);
+	if (is_hugepte(pmd)) {
+		*paddr = vtop_final(vaddr, pmd, archdata->pg.rpn_shift,
+				    archdata->pg.pmd_shift);
 		return kdump_ok;
 	}
-	if (is_hugepd(l2e))
+	if (is_hugepd(pmd))
 		return ppc64_vtop_hugepd(ctx, vaddr, paddr,
-					 l2e, archdata->pg.l2_shift);
+					 pmd, archdata->pg.pmd_shift);
 
-	e = (l2e & (~archdata->pg.l2_mask)) + split.l1 * ps;
-
-	res = kdump_readp(ctx, KDUMP_KVADDR, e, &l1e, &ps);
+	ptep = (pmd & (~archdata->pg.pmd_mask)) +
+		split.pte * sizeof(uint64_t);
+	res = read_u64(ctx, KDUMP_KVADDR, ptep, "PTE entry", &pte);
 	if (res != kdump_ok)
-		return set_error(ctx, res, "Cannot read L1");
+		return res;
+	if (!pte)
+		return set_error(ctx, kdump_nodata, "pte[%u] is none",
+				 split.pte);
 
-	l1e = dump64toh(ctx, l1e);
-
-	if (!l1e) goto notfound;
-
-	*paddr = vtop_final(vaddr, l1e,
-			    archdata->pg.rpn_shift, archdata->pg.l1_shift);
-
+	*paddr = vtop_final(vaddr, pte, archdata->pg.rpn_shift,
+			    archdata->pg.pte_shift);
 	return kdump_ok;
-
-notfound:
-	return kdump_nodata;
 }
 
 
@@ -413,26 +404,26 @@ ppc64_init(kdump_ctx *ctx)
 
 	if (pagesize == _64K) {
 		archdata->pgform.page_bits = 16;
-		archdata->pgform.l1_bits = 12;
-		archdata->pgform.l2_bits = 12;
-		archdata->pgform.l3_bits = 0;
-		archdata->pgform.l4_bits = 4;
-		archdata->pg.l2_mask = 0x1ff;
-		archdata->pg.l3_mask = 0x1ff;
-		archdata->pg.l4_mask = 0x1ff;
+		archdata->pgform.pte_bits = 12;
+		archdata->pgform.pmd_bits = 12;
+		archdata->pgform.pud_bits = 0;
+		archdata->pgform.pgd_bits = 4;
+		archdata->pg.pmd_mask = 0x1ff;
+		archdata->pg.pud_mask = 0x1ff;
+		archdata->pg.pgd_mask = 0x1ff;
 		archdata->pg.rpn_shift = 30;
 
 	} else
 		return set_error(ctx, kdump_nodata, "PAGESIZE == %d", pagesize);
 
 	shift = get_page_shift(ctx);
-	archdata->pg.l1_shift = shift;
-	shift += archdata->pgform.l1_bits;
-	archdata->pg.l2_shift = shift;
-	shift += archdata->pgform.l2_bits;
-	archdata->pg.l3_shift = shift;
-	shift += archdata->pgform.l3_bits;
-	archdata->pg.l4_shift = shift;
+	archdata->pg.pte_shift = shift;
+	shift += archdata->pgform.pte_bits;
+	archdata->pg.pmd_shift = shift;
+	shift += archdata->pgform.pmd_bits;
+	archdata->pg.pud_shift = shift;
+	shift += archdata->pgform.pgd_bits;
+	archdata->pg.pgd_shift = shift;
 
 	ret = get_symbol_val(ctx, "swapper_pg_dir", &pgtaddr);
 	if (ret == kdump_ok) {
