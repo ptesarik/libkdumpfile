290f90a6ac53e97cd3ca8265c2f67a1f4134667a
Petr Tesarik <ptesarik@suse.com>
2016-03-16 14:47:32 +0100

    Define byteorder macros early
    This allows to use these macros in the header file itself.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- src/kdumpfile-priv.h
+++ src/kdumpfile-priv.h
@@ -43,6 +43,44 @@
 #define INTERNAL_NAME(x)	x
 #endif
 
+/* Older glibc didn't have the byteorder macros */
+#ifndef be16toh
+
+#include <byteswap.h>
+
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define htobe16(x) bswap_16(x)
+#  define htole16(x) (x)
+#  define be16toh(x) bswap_16(x)
+#  define le16toh(x) (x)
+
+#  define htobe32(x) bswap_32(x)
+#  define htole32(x) (x)
+#  define be32toh(x) bswap_32(x)
+#  define le32toh(x) (x)
+
+#  define htobe64(x) bswap_64(x)
+#  define htole64(x) (x)
+#  define be64toh(x) bswap_64(x)
+#  define le64toh(x) (x)
+# else
+#  define htobe16(x) (x)
+#  define htole16(x) bswap_16(x)
+#  define be16toh(x) (x)
+#  define le16toh(x) bswap_16(x)
+
+#  define htobe32(x) (x)
+#  define htole32(x) bswap_32(x)
+#  define be32toh(x) (x)
+#  define le32toh(x) bswap_32(x)
+
+#  define htobe64(x) (x)
+#  define htole64(x) bswap_64(x)
+#  define be64toh(x) (x)
+#  define le64toh(x) bswap_64(x)
+# endif
+#endif
+
 /* Multi-threading */
 #if USE_PTHREAD
 
@@ -969,44 +1007,6 @@ cache_entry_valid(struct cache_entry *entry)
 {
 	return CACHE_PFN_FLAGS(entry->pfn) == cf_valid;
 }
-
-/* Older glibc didn't have the byteorder macros */
-#ifndef be16toh
-
-#include <byteswap.h>
-
-# if __BYTE_ORDER == __LITTLE_ENDIAN
-#  define htobe16(x) bswap_16(x)
-#  define htole16(x) (x)
-#  define be16toh(x) bswap_16(x)
-#  define le16toh(x) (x)
-
-#  define htobe32(x) bswap_32(x)
-#  define htole32(x) (x)
-#  define be32toh(x) bswap_32(x)
-#  define le32toh(x) (x)
-
-#  define htobe64(x) bswap_64(x)
-#  define htole64(x) (x)
-#  define be64toh(x) bswap_64(x)
-#  define le64toh(x) (x)
-# else
-#  define htobe16(x) (x)
-#  define htole16(x) bswap_16(x)
-#  define be16toh(x) (x)
-#  define le16toh(x) bswap_16(x)
-
-#  define htobe32(x) (x)
-#  define htole32(x) bswap_32(x)
-#  define be32toh(x) (x)
-#  define le32toh(x) bswap_32(x)
-
-#  define htobe64(x) (x)
-#  define htole64(x) bswap_64(x)
-#  define be64toh(x) (x)
-#  define le64toh(x) bswap_64(x)
-# endif
-#endif
 
 /* Inline utility functions */
 
