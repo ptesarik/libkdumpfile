b1e74ca137ff90a253ab14e8c98aa09d0fa2c2f3
Petr Tesarik <ptesarik@suse.com>
2016-04-06 23:35:44 +0200

    tests: Extend vmci-post to check vmcoreinfo.lines
    Also test the semi-parsed lines.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- tests/vmci-post.c
+++ tests/vmci-post.c
@@ -44,6 +44,8 @@
 #define SYM_NAME	"test_symbol"
 #define SYM_VALUE	0x123456
 
+#define ATTR_LINES	"linux.vmcoreinfo.lines"
+
 static const char vmcore[] =
 	"OSRELEASE=" OSRELEASE			"\n"
 	"PAGESIZE=" str(PAGESIZE)		"\n"
@@ -51,12 +53,39 @@ static const char vmcore[] =
 	"";
 
 static int
+check_string(kdump_ctx *ctx, const char *attrpath, const char *expect)
+{
+	kdump_attr_t attr;
+	kdump_status status;
+
+	status = kdump_get_attr(ctx, attrpath, &attr);
+	if (status != kdump_ok) {
+		fprintf(stderr, "%s: Cannot get value: %s\n",
+			attrpath, kdump_err_str(ctx));
+		return TEST_ERR;
+	}
+	if (attr.type != kdump_string) {
+		fprintf(stderr, "%s: Wrong attribute type: %d\n",
+			attrpath, (int) attr.type);
+		return TEST_FAIL;
+	}
+	if (strcmp(attr.val.string, expect)) {
+		fprintf(stderr, "%s: Invalid attribute value: '%s' != '%s'\n",
+			attrpath, attr.val.string, expect);
+		return TEST_FAIL;
+	}
+
+	printf("%s: %s\n", attrpath, attr.val.string);
+	return TEST_OK;
+}
+
+static int
 check(kdump_ctx *ctx)
 {
 	kdump_attr_t attr;
 	kdump_addr_t symval;
 	kdump_status status;
-	int rc;
+	int rc, tmprc;
 
 	attr.type = kdump_string;
 	attr.val.string = vmcore;
@@ -69,22 +98,31 @@ check(kdump_ctx *ctx)
 
 	rc = TEST_OK;
 
-	status = kdump_get_attr(ctx, ATTR_OSRELEASE, &attr);
-	if (status != kdump_ok) {
-		fprintf(stderr, "%s: Cannot get value: %s\n",
-			ATTR_OSRELEASE, kdump_err_str(ctx));
-		return TEST_ERR;
-	}
-	if (attr.type != kdump_string) {
-		fprintf(stderr, "%s: Wrong attribute type: %d\n",
-			ATTR_OSRELEASE, (int) attr.type);
-		rc = TEST_FAIL;
-	} else if (strcmp(attr.val.string, OSRELEASE)) {
-		fprintf(stderr, "%s: Invalid attribute value: '%s' != '%s'\n",
-			ATTR_OSRELEASE, attr.val.string, OSRELEASE);
-		rc = TEST_FAIL;
-	} else
-		printf("%s: %s\n", ATTR_OSRELEASE, attr.val.string);
+	tmprc = check_string(ctx, ATTR_LINES ".OSRELEASE", OSRELEASE);
+	if (tmprc == TEST_FAIL)
+		return tmprc;
+	if (tmprc != TEST_OK)
+		rc = tmprc;
+
+	tmprc = check_string(ctx, ATTR_LINES ".PAGESIZE", str(PAGESIZE));
+	if (tmprc == TEST_FAIL)
+		return tmprc;
+	if (tmprc != TEST_OK)
+		rc = tmprc;
+
+
+	tmprc = check_string(ctx, ATTR_LINES ".SYMBOL(" SYM_NAME ")",
+			     str(SYM_VALUE));
+	if (tmprc == TEST_FAIL)
+		return tmprc;
+	if (tmprc != TEST_OK)
+		rc = tmprc;
+
+	tmprc = check_string(ctx, ATTR_OSRELEASE, OSRELEASE);
+	if (tmprc == TEST_FAIL)
+		return tmprc;
+	if (tmprc != TEST_OK)
+		rc = tmprc;
 
 	status = kdump_get_attr(ctx, ATTR_PAGESIZE, &attr);
 	if (status != kdump_ok) {
@@ -116,7 +154,7 @@ check(kdump_ctx *ctx)
 			SYM_NAME, (long long) symval, (long long) SYM_VALUE);
 		rc = TEST_FAIL;
 	} else
-		printf("%s: %llx\n", SYM_NAME, (long long) symval);
+		printf("%s = %llx\n", SYM_NAME, (long long) symval);
 
 	return rc;
 }
