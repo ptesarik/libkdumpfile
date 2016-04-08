ptesarik/libkdumpfile

commit 11a91a52ad6ac5554b8e0a9557be64baa454f61e

Use read_u64 for page table walks
This helper is used for ppc64, but it should also be used by other
architectures to reduce code duplication and unify error paths.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/x86_64.c
@@ -697,40 +697,32 @@ static kdump_status
 x86_64_pt_walk(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr,
 	       kdump_addrspace_t pml4_as, kdump_addr_t pml4)
 {
-	uint64_t *tbl;
+	kdump_addr_t pgdp, pudp, pmdp, ptep;
 	uint64_t pgd, pud, pmd, pte;
 	kdump_paddr_t base;
-	struct page_io pio;
 	kdump_status ret;
 
 	if (pml4 == ~(kdump_addr_t)0)
 		return set_error(ctx, kdump_invalid,
 				 "VTOP translation not initialized");
 
-	/* These page table levels are likely to be hit again soon. */
-	pio.precious = 1;
-
-	pio.pfn = pml4 >> PAGE_SHIFT;
-	ret = raw_read_page(ctx, pml4_as, &pio);
+	pgdp = pml4 + pgd_index(vaddr) * sizeof(uint64_t);
+	ret = read_u64(ctx, pml4_as, pgdp, 1, "PGD entry", &pgd);
 	if (ret != kdump_ok)
 		return ret;
-	tbl = pio.buf;
 
-	pgd = dump64toh(ctx, tbl[pgd_index(vaddr)]);
 	if (!(pgd & _PAGE_PRESENT))
 		return set_error(ctx, kdump_nodata,
 				 "Page directory pointer not present:"
 				 " pgd[%u] = 0x%llx",
 				 (unsigned) pgd_index(vaddr),
 				 (unsigned long long) pgd);
-	pio.pfn = (pgd & ~PHYSADDR_MASK) >> PAGE_SHIFT;
 
-	ret = raw_read_page(ctx, KDUMP_MACHPHYSADDR, &pio);
+	pudp = (pgd & ~PHYSADDR_MASK) + pud_index(vaddr) * sizeof(uint64_t);
+	ret = read_u64(ctx, KDUMP_MACHPHYSADDR, pudp, 1, "PUD entry", &pud);
 	if (ret != kdump_ok)
 		return ret;
-	tbl = pio.buf;
 
-	pud = dump64toh(ctx, tbl[pud_index(vaddr)]);
 	if (!(pud & _PAGE_PRESENT))
 		return set_error(ctx, kdump_nodata,
 				 "Page directory not present:"
@@ -742,17 +734,12 @@ x86_64_pt_walk(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr,
 		*paddr = base + (vaddr & ~PUD_PSE_MASK);
 		return kdump_ok;
 	}
-	pio.pfn = (pud & ~PHYSADDR_MASK) >> PAGE_SHIFT;
 
-	/* Let the default cache algorithm decide the lowest 2 levels. */
-	pio.precious = 0;
-
-	ret = raw_read_page(ctx, KDUMP_MACHPHYSADDR, &pio);
+	pmdp = (pud & ~PHYSADDR_MASK) + pmd_index(vaddr) * sizeof(uint64_t);
+	ret = read_u64(ctx, KDUMP_MACHPHYSADDR, pmdp, 0, "PMD entry", &pmd);
 	if (ret != kdump_ok)
 		return ret;
-	tbl = pio.buf;
 
-	pmd = dump64toh(ctx, tbl[pmd_index(vaddr)]);
 	if (!(pmd & _PAGE_PRESENT))
 		return set_error(ctx, kdump_nodata,
 				 "Page table not present: pmd[%u] = 0x%llx",
@@ -763,14 +750,12 @@ x86_64_pt_walk(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr,
 		*paddr = base + (vaddr & ~PMD_PSE_MASK);
 		return kdump_ok;
 	}
-	pio.pfn = (pmd & ~PHYSADDR_MASK) >> PAGE_SHIFT;
 
-	ret = raw_read_page(ctx, KDUMP_MACHPHYSADDR, &pio);
+	ptep = (pmd & ~PHYSADDR_MASK) + pte_index(vaddr) * sizeof(uint64_t);
+	ret = read_u64(ctx, KDUMP_MACHPHYSADDR, ptep, 0, "PTE entry", &pte);
 	if (ret != kdump_ok)
 		return ret;
-	tbl = pio.buf;
 
-	pte = dump64toh(ctx, tbl[pte_index(vaddr)]);
 	if (!(pte & _PAGE_PRESENT))
 		return set_error(ctx, kdump_nodata,
 				 "Page not present: pte[%u] = 0x%llx",

src/s390x.c
@@ -135,22 +135,10 @@ read_pte(kdump_ctx *ctx, struct vtop_control *ctl, unsigned idx,
 	 uint64_t *pentry)
 {
 	kdump_paddr_t addr;
-	struct page_io pio;
-	uint64_t *p;
-	kdump_status ret;
 
 	addr = ctl->paddr + idx * sizeof(uint64_t);
-	pio.pfn = addr >> PAGE_SHIFT;
-	pio.precious = ctl->tbltype > 0;
-	ret = raw_read_page(ctx, KDUMP_KPHYSADDR, &pio);
-	if (ret != kdump_ok)
-		return set_error(ctx, ret,
-				 "Cannot read page table entry at %016llx",
-				 (unsigned long long)addr);
-
-	p = pio.buf + (addr & ~PAGE_MASK);
-	*pentry = dump64toh(ctx, *p);
-	return kdump_ok;
+	return read_u64(ctx, KDUMP_KPHYSADDR, addr,
+			(ctl->tbltype > 0), "page table entry", pentry);
 }
 
 static kdump_status

commit 790be03443a248921a3572ca7dfed980b6013562

x86_64: Mask off low 12 bits in higher-order page tables
This bug was introduced by commit 11a91a52. Originally, the low bits
were ignored, because the address was shifted right by PAGE_SHIFT to
form a PFN. But since the full address is passed to read_u64, they
must be masked explicitly now.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/x86_64.c
@@ -728,7 +728,8 @@ x86_64_pt_walk(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr,
 				 (unsigned) pgd_index(vaddr),
 				 (unsigned long long) pgd);
 
-	pudp = (pgd & ~PHYSADDR_MASK) + pud_index(vaddr) * sizeof(uint64_t);
+	pudp = (pgd & ~PHYSADDR_MASK & PAGE_MASK) +
+		pud_index(vaddr) * sizeof(uint64_t);
 	ret = read_u64(ctx, KDUMP_MACHPHYSADDR, pudp, 1, "PUD entry", &pud);
 	if (ret != kdump_ok)
 		return ret;
@@ -745,7 +746,8 @@ x86_64_pt_walk(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr,
 		return kdump_ok;
 	}
 
-	pmdp = (pud & ~PHYSADDR_MASK) + pmd_index(vaddr) * sizeof(uint64_t);
+	pmdp = (pud & ~PHYSADDR_MASK & PAGE_MASK) +
+		pmd_index(vaddr) * sizeof(uint64_t);
 	ret = read_u64(ctx, KDUMP_MACHPHYSADDR, pmdp, 0, "PMD entry", &pmd);
 	if (ret != kdump_ok)
 		return ret;
@@ -761,7 +763,8 @@ x86_64_pt_walk(kdump_ctx *ctx, kdump_vaddr_t vaddr, kdump_paddr_t *paddr,
 		return kdump_ok;
 	}
 
-	ptep = (pmd & ~PHYSADDR_MASK) + pte_index(vaddr) * sizeof(uint64_t);
+	ptep = (pmd & ~PHYSADDR_MASK & PAGE_MASK) +
+		pte_index(vaddr) * sizeof(uint64_t);
 	ret = read_u64(ctx, KDUMP_MACHPHYSADDR, ptep, 0, "PTE entry", &pte);
 	if (ret != kdump_ok)
 		return ret;