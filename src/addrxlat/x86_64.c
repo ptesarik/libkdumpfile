8262db2d297162cf6e8ae0628e36a8069317ba75
Petr Tesarik <ptesarik@suse.com>
2016-07-07 11:49:07 +0200

    addrxlat: support for x86_64
    Add test case and handling for x86_64 pagetable walk.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- src/addrxlat/x86_64.c
+++ src/addrxlat/x86_64.c
@@ -28,7 +28,20 @@
    not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <inttypes.h>
+
 #include "addrxlat-priv.h"
+
+/* Maximum physical address bits (architectural limit) */
+#define PHYSADDR_BITS_MAX	52
+#define PHYSADDR_SIZE		((uint64_t)1 << PHYSADDR_BITS_MAX)
+#define PHYSADDR_MASK		(~(PHYSADDR_SIZE-1))
+
+#define _PAGE_BIT_PRESENT	0
+#define _PAGE_BIT_PSE		7
+
+#define _PAGE_PRESENT	(1UL << _PAGE_BIT_PRESENT)
+#define _PAGE_PSE	(1UL << _PAGE_BIT_PSE)
 
 /** AMD64 (Intel 64) vtop function.
  * @param ctx    Address translation object.
@@ -38,6 +51,52 @@
 addrxlat_status
 vtop_x86_64(addrxlat_ctx *ctx, addrxlat_vtop_state_t *state)
 {
-	return set_error(ctx, addrxlat_notimplemented,
-			 "x86-64 not yet implemented");
+	static const char pgt_full_name[][16] = {
+		"Page",
+		"Page table",
+		"Page directory",
+		"PDPT table",
+	};
+	static const char pte_name[][4] = {
+		"pte",
+		"pmd",
+		"pud",
+		"pgd",
+	};
+
+	addrxlat_fulladdr_t ptep;
+	uint64_t pte;
+	addrxlat_status status;
+
+	ptep = state->base;
+	ptep.addr += state->idx[state->level] * sizeof(uint64_t);
+	status = ctx->cb_read64(ctx, ptep, &pte, NULL);
+	if (status != addrxlat_ok)
+		return status;
+
+	if (!(pte & _PAGE_PRESENT))
+		return set_error(ctx, addrxlat_notpresent,
+				 "%s not present: %s[%u] = 0x%" PRIx64,
+				 pgt_full_name[state->level - 1],
+				 pte_name[state->level - 1],
+				 (unsigned) state->idx[state->level],
+				 pte);
+
+	state->base.as = ADDRXLAT_MACHPHYSADDR;
+	state->base.addr = pte & ~PHYSADDR_MASK;
+	if (state->level >= 2 && state->level <= 3 && (pte & _PAGE_PSE)) {
+		addrxlat_addr_t off;
+
+		state->base.addr &= ctx->pgt_mask[state->level - 1];
+		off = 0;
+		while (state->level > 1) {
+			--state->level;
+			off |= state->idx[state->level];
+			off <<= ctx->pf->bits[state->level - 1];
+		}
+		state->idx[0] |= off;
+	} else
+		state->base.addr &= ctx->pgt_mask[0];
+
+	return addrxlat_continue;
 }
