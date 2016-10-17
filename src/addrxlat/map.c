5ba7bcffa2d207b9c77a9d9faa96fa0122bc28a9
Petr Tesarik <ptesarik@suse.com>
2016-10-17 13:54:05 +0200

    Rewrite addrxlat_map_set() to pass the test case
    Replace the buggy code with a commented reimplementation. It is
    surprizingly hard to get all cases right...
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- src/addrxlat/map.c
+++ src/addrxlat/map.c
@@ -39,50 +39,76 @@ addrxlat_map_t *
 addrxlat_map_set(addrxlat_map_t *map, addrxlat_addr_t addr,
 		 const addrxlat_range_t *range)
 {
-	addrxlat_range_t *r = NULL;
-	addrxlat_range_t *prev;
+	addrxlat_range_t *first, *last;
+	addrxlat_addr_t raddr, rend;
 	addrxlat_addr_t end;
-	addrxlat_addr_t raddr = 0;
-	addrxlat_addr_t rend = ADDRXLAT_ADDR_MAX;
-	addrxlat_addr_t extend = 0;
-	size_t left = 0;
-	int delta = 3;
+	addrxlat_addr_t extend;
+	size_t left;
+	int delta;
 	int i;
 
-	if (map)
-		for (r = map->ranges, left = map->n; left > 0; ++r, --left) {
-			if (addr <= raddr + r->endoff)
-				break;
-			raddr += r->endoff + 1;
-		}
-	prev = r;
-
 	end = addr + range->endoff;
-	rend = raddr - 1;
-	while (left > 0) {
-		--delta;
-		rend += r->endoff + 1;
-		if (rend > end)
-			break;
-		++r;
-		--left;
+
+	extend = 0;
+	raddr = 0;
+	if (map) {
+		delta = 2;
+		left = map->n;
+
+		/* find first affected region */
+		first = map->ranges;
+		while (left > 0) {
+			if (raddr + first->endoff >= addr)
+				break;
+			raddr += first->endoff + 1;
+			++first, --left;
+		}
+		/* include the previous region if it can be merged */
+		if (raddr && raddr == addr && first[-1].def == range->def) {
+			--first, ++left;
+			raddr -= first->endoff + 1;
+		}
+
+		/* find last affected region */
+		last = first;
+		rend = raddr + first->endoff;
+		while (left > 0) {
+			if (rend >= end)
+				break;
+			--delta;
+			++last, --left;
+			rend += last->endoff + 1;
+		}
+		/* include the following region if it can be merged */
+		if (left > 0 && rend == end && last[1].def == range->def) {
+			--delta;
+			++last, --left;
+			rend += last->endoff + 1;
+		}
+
+		/* merge up and/or down */
+		if (first->def == range->def) {
+			extend += addr - raddr;
+			raddr = addr;
+		}
+		if (last->def == range->def) {
+			extend += rend - end;
+			rend = end;
+		}
+	} else {
+		delta = 3;
+		left = 0;
+		first = last = NULL;
+		rend = ADDRXLAT_ADDR_MAX;
 	}
 
+	/* split begin and/or end */
 	if (addr == raddr)
 		--delta;
-	else if (prev && prev->def == range->def) {
-		extend = addr - raddr;
-		raddr = addr;
-		--delta;
-	}
 	if (rend == end)
 		--delta;
-	else if (r && r->def == range->def) {
-		extend += rend - (addr + range->endoff);
-		rend = end;
-		--delta;
-	}
 
+	/* (re-)allocate if growing */
 	if (delta > 0) {
 		size_t newn = delta + (map ? map->n : 0);
 		addrxlat_map_t *newmap =
@@ -93,38 +119,52 @@ addrxlat_map_set(addrxlat_map_t *map, addrxlat_addr_t addr,
 
 		if (!map) {
 			newmap->n = 1;
-			r = prev = newmap->ranges;
-			r->endoff = ADDRXLAT_ADDR_MAX;
-			r->def = NULL;
+			first = last = newmap->ranges;
+			first->endoff = ADDRXLAT_ADDR_MAX;
+			first->def = NULL;
 			++left;
 			--delta;
 		} else {
-			r = &newmap->ranges[r - map->ranges];
-			prev = &newmap->ranges[prev - map->ranges];
+			first = &newmap->ranges[first - map->ranges];
+			last = &newmap->ranges[last - map->ranges];
 		}
 		map = newmap;
 	}
-	map->n += delta;
 
+	/* drop references to overlapped regions */
 	for (i = delta; i < 0; ++i)
-		if (r[i].def)
-			internal_def_decref(r[i].def);
-	if (delta > 0 && r->def)
-		internal_def_incref(r->def);
-	memmove(r + delta, r, left * sizeof(*r));
+		if (last[i].def)
+			internal_def_decref(last[i].def);
+	if (delta) {
+		/* if a region is split, take an extra reference  */
+		if (delta > 1 && last->def)
+			internal_def_incref(last->def);
 
-	if (rend != end)
-		r[delta].endoff = rend - end - 1;
-	if (raddr != addr) {
-		prev->endoff = addr - raddr - 1;
-		++prev;
+		memmove(last + delta, last, left * sizeof(*last));
+		last += delta;
+		map->n += delta;
 	}
 
-	if (prev->def)
-		internal_def_decref(prev->def);
+	/* resize adjacent regions if necessary */
+	if (raddr != addr) {
+		first->endoff = addr - raddr - 1;
+		++first;
+	}
+	if (rend != end) {
+		last->endoff = rend - end - 1;
+		--last;
+	}
+
+	/* take an extra reference to the new region */
 	internal_def_incref(range->def);
-	prev->endoff = range->endoff + extend;
-	prev->def = range->def;
+
+	/* drop reference to the previous value, unless an unitialized
+	 * array entry is being inserted */
+	if (delta <= 0 && first->def)
+		internal_def_decref(first->def);
+
+	first->endoff = range->endoff + extend;
+	first->def = range->def;
 	return map;
 }
 
