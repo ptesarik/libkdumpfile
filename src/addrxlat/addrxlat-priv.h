49a7734f5f79c749476c515c16c613c892104f2d
Petr Tesarik <ptesarik@suse.com>
2017-02-10 11:12:08 +0100

    addrxlat: Re-design handling of internal-only symbols
    Some symbols are used only internally by the library, but from
    different object files, so they must be extern. To protect the
    global namespace in a static link, these symbols are prefixed
    with "_libaddrxlat_priv". However, I don't want to use such
    cumbersome names in the source code, so they are remapped
    automatically.
    
    So far the mapping has been done by the preprocessor. This is
    suboptimal, because the preprocessor changes all occurences of
    that symbol (e.g. arguments or structure field names with the
    same name).
    
    Instead, let's exploit GCC's ability to assign an arbitrary
    name to a symbol with the __asm__() directive in the declaration.
    
    As a side note, the prefix is now shortened to "_addrxlat_priv".
    This should still be good enough to avoid potential clashes with
    other libraries.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- src/addrxlat/addrxlat-priv.h
+++ src/addrxlat/addrxlat-priv.h
@@ -35,11 +35,21 @@
 #include "addrxlat.h"
 #pragma GCC visibility pop
 
+#define STRINGIFY(x)	#x
+#define XSTRINGIFY(x)	STRINGIFY(x)
+#define CONCATENATE(a, b)	a ## b
+#define XCONCATENATE(a, b)	CONCATENATE(a, b)
+
 /* Minimize chance of name clashes (in a static link) */
 #ifndef PIC
-#define INTERNAL_NAME(x)	_libaddrxlat_priv_ ## x
+#define INTERNAL_ASM_NAME(sym) \
+	XCONCATENATE(__USER_LABEL_PREFIX__, _addrxlat_priv_ ## sym)
+#define INTERNAL_DECL(type, sym, param)	\
+	type sym param			\
+	__asm__(XSTRINGIFY(INTERNAL_ASM_NAME(sym)))
 #else
-#define INTERNAL_NAME(x)	x
+#define INTERNAL_DECL(type, sym, param)	\
+	type sym param
 #endif
 
 #ifndef PIC
@@ -146,26 +156,20 @@ struct _addrxlat_sys {
 
 /* vtop */
 
-#define pgt_huge_page INTERNAL_NAME(pgt_huge_page)
-addrxlat_status pgt_huge_page(addrxlat_walk_t *state);
+INTERNAL_DECL(addrxlat_status, pgt_huge_page, (addrxlat_walk_t *state));
 
-#define pgt_ia32 INTERNAL_NAME(pgt_ia32)
-addrxlat_walk_step_fn pgt_ia32;
+INTERNAL_DECL(addrxlat_walk_step_fn, pgt_ia32, );
 
-#define pgt_ia32_pae INTERNAL_NAME(pgt_ia32_pae)
-addrxlat_walk_step_fn pgt_ia32_pae;
+INTERNAL_DECL(addrxlat_walk_step_fn, pgt_ia32_pae, );
 
-#define pgt_x86_64 INTERNAL_NAME(pgt_x86_64)
-addrxlat_walk_step_fn pgt_x86_64;
+INTERNAL_DECL(addrxlat_walk_step_fn, pgt_x86_64, );
 
-#define pgt_s390x INTERNAL_NAME(pgt_s390x)
-addrxlat_walk_step_fn pgt_s390x;
+INTERNAL_DECL(addrxlat_walk_step_fn, pgt_s390x, );
 
-#define pgt_ppc64_linux_rpn30 INTERNAL_NAME(pgt_ppc64_linux_rpn30)
-addrxlat_walk_step_fn pgt_ppc64_linux_rpn30;
+INTERNAL_DECL(addrxlat_walk_step_fn, pgt_ppc64_linux_rpn30, );
 
-#define paging_max_index INTERNAL_NAME(paging_max_index)
-addrxlat_addr_t paging_max_index(const addrxlat_paging_form_t *pf);
+INTERNAL_DECL(addrxlat_addr_t, paging_max_index,
+	      (const addrxlat_paging_form_t *pf));
 
 /* Option parsing. */
 
@@ -202,9 +206,9 @@ struct parsed_opts {
 	struct optval val[OPT_NUM];
 };
 
-#define parse_opts INTERNAL_NAME(parse_opts)
-addrxlat_status parse_opts(
-	struct parsed_opts *popt, addrxlat_ctx_t *ctx, const char *opts);
+INTERNAL_DECL(addrxlat_status, parse_opts,
+	      (struct parsed_opts *popt, addrxlat_ctx_t *ctx,
+	       const char *opts));
 
 /* Translation system */
 
@@ -229,17 +233,13 @@ struct sys_init_data {
  */
 typedef addrxlat_status sys_arch_fn(struct sys_init_data *ctl);
 
-#define sys_ia32 INTERNAL_NAME(sys_ia32)
-sys_arch_fn sys_ia32;
+INTERNAL_DECL(sys_arch_fn, sys_ia32, );
 
-#define sys_ppc64 INTERNAL_NAME(sys_ppc64)
-sys_arch_fn sys_ppc64;
+INTERNAL_DECL(sys_arch_fn, sys_ppc64, );
 
-#define sys_s390x INTERNAL_NAME(sys_s390x)
-sys_arch_fn sys_s390x;
+INTERNAL_DECL(sys_arch_fn, sys_s390x, );
 
-#define sys_x86_64 INTERNAL_NAME(sys_x86_64)
-sys_arch_fn sys_x86_64;
+INTERNAL_DECL(sys_arch_fn, sys_x86_64, );
 
 /** Optional action associated with a translation system region. */
 enum sys_action {
@@ -259,24 +259,21 @@ struct sys_region {
 /** OS-map layout table end marker. */
 #define SYS_REGION_END	{ 0, 0, ADDRXLAT_SYS_METH_NUM }
 
-#define sys_ensure_meth INTERNAL_NAME(sys_ensure_meth)
-addrxlat_status sys_ensure_meth(
-	struct sys_init_data *ctl, addrxlat_sys_meth_t idx);
+INTERNAL_DECL(addrxlat_status, sys_ensure_meth,
+	      (struct sys_init_data *ctl, addrxlat_sys_meth_t idx));
 
-#define sys_set_layout INTERNAL_NAME(sys_set_layout)
-addrxlat_status sys_set_layout(
-	struct sys_init_data *ctl, addrxlat_sys_map_t idx,
-	const struct sys_region layout[]);
+INTERNAL_DECL(addrxlat_status, sys_set_layout,
+	      (struct sys_init_data *ctl, addrxlat_sys_map_t idx,
+	       const struct sys_region layout[]));
 
-#define sys_set_physmaps INTERNAL_NAME(sys_set_physmaps)
-addrxlat_status
-sys_set_physmaps(struct sys_init_data *ctl, addrxlat_addr_t maxaddr);
+INTERNAL_DECL(addrxlat_status, sys_set_physmaps,
+	      (struct sys_init_data *ctl, addrxlat_addr_t maxaddr));
 
 /* internal aliases */
 
 #define internal_ctx_set_error INTERNAL_ALIAS(ctx_err)
 DECLARE_INTERNAL(ctx_err);
-#define set_error internal_ctx_err
+#define set_error internal_ctx_set_error
 
 #define internal_meth_new INTERNAL_ALIAS(meth_new)
 DECLARE_INTERNAL(meth_new)
@@ -316,35 +313,29 @@ DECLARE_INTERNAL(by_sys)
 
 /* utils */
 
-#define read32 INTERNAL_NAME(read32)
-addrxlat_status read32(
-	addrxlat_ctx_t *ctx, const addrxlat_fulladdr_t *addr, uint32_t *val,
-	const char *what);
+INTERNAL_DECL(addrxlat_status, read32,
+	      (addrxlat_ctx_t *ctx, const addrxlat_fulladdr_t *addr,
+	       uint32_t *val, const char *what));
 
-#define read64 INTERNAL_NAME(read64)
-addrxlat_status read64(
-	addrxlat_ctx_t *ctx, const addrxlat_fulladdr_t *addr, uint64_t *val,
-	const char *what);
+INTERNAL_DECL(addrxlat_status, read64,
+	      (addrxlat_ctx_t *ctx, const addrxlat_fulladdr_t *addr,
+	       uint64_t *val, const char *what));
 
-#define def_choose_pgtroot INTERNAL_NAME(def_choose_pgtroot)
-void def_choose_pgtroot(addrxlat_def_t *def, const addrxlat_meth_t *meth);
+INTERNAL_DECL(void, def_choose_pgtroot,
+	      (addrxlat_def_t *def, const addrxlat_meth_t *meth));
 
-#define get_reg INTERNAL_NAME(get_reg)
-addrxlat_status get_reg(
-	addrxlat_ctx_t *ctx, const char *name, addrxlat_addr_t *val);
+INTERNAL_DECL(addrxlat_status, get_reg,
+	      (addrxlat_ctx_t *ctx, const char *name, addrxlat_addr_t *val));
 
-#define get_symval INTERNAL_NAME(get_symval)
-addrxlat_status get_symval(
-	addrxlat_ctx_t *ctx, const char *name, addrxlat_addr_t *val);
+INTERNAL_DECL(addrxlat_status, get_symval,
+	      (addrxlat_ctx_t *ctx, const char *name, addrxlat_addr_t *val));
 
-#define get_sizeof INTERNAL_NAME(get_sizeof)
-addrxlat_status get_sizeof(
-	addrxlat_ctx_t *ctx, const char *name, addrxlat_addr_t *sz);
+INTERNAL_DECL(addrxlat_status, get_sizeof,
+	      (addrxlat_ctx_t *ctx, const char *name, addrxlat_addr_t *sz));
 
-#define get_offsetof INTERNAL_NAME(get_offsetof)
-addrxlat_status get_offsetof(
-	addrxlat_ctx_t *ctx, const char *type, const char *memb,
-	addrxlat_addr_t *off);
+INTERNAL_DECL(addrxlat_status, get_offsetof,
+	      (addrxlat_ctx_t *ctx, const char *type, const char *memb,
+	       addrxlat_addr_t *off));
 
 /** Clear the error message.
  * @param ctx     Address translation context.
