40826a1da1fd223a5261fdc177a63a443a986078
Petr Tesarik <ptesarik@suse.com>
2017-03-10 19:43:22 +0100

    Add listxendoms to examples
    This is a trivial example which can list all Xen domains found in
    a system dump. It does not print a lot of information, but that's
    because it should work only with information found in VMCOREINFO_XEN.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

---
+++ examples/listxendoms.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#include <kdumpfile.h>
+
+static kdump_vaddr_t
+read_ptr(kdump_ctx *ctx, kdump_vaddr_t addr)
+{
+	uint64_t ptr;
+	size_t sz = sizeof ptr;
+
+	if (kdump_readp(ctx, KDUMP_KVADDR, addr, &ptr, &sz) != kdump_ok) {
+		fprintf(stderr, "read failed at 0x%llx: %s\n",
+			(unsigned long long) addr, kdump_err_str(ctx));
+		return 0;
+	}
+	return ptr;
+}
+
+static kdump_status
+print_xen_domains(kdump_ctx *ctx)
+{
+	kdump_addr_t domain;
+	uint64_t id;
+	size_t sz, off_id, off_next;
+	kdump_attr_t attr;
+	kdump_status status;
+
+	status = kdump_get_attr(
+		ctx, "xen.vmcoreinfo.OFFSET.domain.domain_id", &attr);
+	if (status != kdump_ok)
+		return status;
+	off_id = attr.val.number;
+
+	status = kdump_get_attr(
+		ctx, "xen.vmcoreinfo.OFFSET.domain.next_in_list", &attr);
+	if (status != kdump_ok)
+		return status;
+	off_next = attr.val.number;
+
+	status = kdump_vmcoreinfo_symbol(ctx, "domain_list", &domain);
+	if (status != kdump_ok)
+		return status;
+
+	domain -= off_next;
+	while ( (domain = read_ptr(ctx, domain + off_next)) ) {
+		sz = sizeof id;
+		status = kdump_readp(ctx, KDUMP_KVADDR, domain + off_id,
+				     &id, &sz);
+		if (status != kdump_ok)
+			return status;
+
+		printf("Domain ID 0x%"PRIx64" at 0x%llx\n",
+		       id, (unsigned long long)domain);
+	}
+
+	return kdump_ok;
+}
+
+int
+main(int argc, char **argv)
+{
+	kdump_ctx *ctx;
+	int fd;
+	kdump_attr_t attr;
+	kdump_status status;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <dumpfile>\n", argv[0]);
+		return 1;
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		perror(argv[1]);
+		return 2;
+	}
+
+	ctx = kdump_new();
+	if (!ctx) {
+		perror("Cannot allocate kdump context");
+		return -1;
+	}
+
+	status = kdump_set_fd(ctx, fd);
+	if (status != kdump_ok) {
+		fprintf(stderr, "kdump_set_fd failed: %s\n",
+			kdump_err_str(ctx));
+		kdump_free(ctx);
+		return 2;
+	}
+
+	if (kdump_xen_type(ctx) != kdump_xen_system) {
+		fputs("Not a Xen system dump\n", stderr);
+		return 1;
+	}
+
+	attr.type = kdump_string;
+	attr.val.string = "xen";
+	status = kdump_set_attr(ctx, "addrxlat.ostype", &attr);
+	if (status != kdump_ok) {
+		fprintf(stderr, "Cannot set ostype: %s\n",
+			kdump_err_str(ctx));
+		return 1;
+	}
+
+	if (print_xen_domains(ctx) != kdump_ok)
+		printf("Cannot read domains: %s\n", kdump_err_str(ctx));
+
+	kdump_free(ctx);
+	close(fd);
+
+	return 0;
+}
