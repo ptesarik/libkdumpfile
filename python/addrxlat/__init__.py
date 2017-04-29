6611ca3569ebe50309cf0ee0776d3ad80f66b487
Petr Tesarik <ptesarik@suse.com>
2017-04-29 21:20:39 +0200

    python/addrxlat: Inherit docstrings from _addrxlat
    If a method does not provide a docstring, copy it from a superclass.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- python/addrxlat/__init__.py
+++ python/addrxlat/__init__.py
@@ -234,3 +234,18 @@ convert.Map = Map
 convert.System = System
 convert.Step = Step
 convert.Operator = Operator
+
+import inspect as _inspect
+for _cls in globals().values():
+    if not _inspect.isclass(_cls):
+        continue
+    for _name, _method in _inspect.getmembers(_cls, _inspect.ismethod):
+        if _method.__doc__:
+            continue
+        for _parent in _inspect.getmro(_cls)[1:]:
+            if hasattr(_parent, _name):
+                _method.__func__.__doc__ = getattr(_parent, _name).__doc__
+                break
+
+# Free up temporary variables
+del _cls, _name, _method, _parent, _inspect
