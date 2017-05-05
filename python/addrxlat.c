ptesarik/libkdumpfile

commit 71b1811671bbf8d43812b282df9bb4ef7d48c487

python/addrxlat: Track modifications of step.base
Right now, the "base" attribute is calculated. However, since
FullAddress is a mutable object, callers may be tempted to modify
it in-place, e.g.:

  step.base.addr = 0xbadbadbad

This does not work, because Python first creates a new FullAddress
object for the current content of addrxlat_step_t, then modifies
that new object's "addr" attribute and then throws away the modified
object again.

Let's reuse the work I did on Description attributes, so the above
actually works. After the change, Step objects contain a reference
to an actual FullAddress object, which is synchronized with the
addrxlat_step_t structure.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/addrxlat.c
@@ -3850,6 +3850,9 @@ static PyTypeObject sys_type =
 	sys_new,			/* tp_new */
 };
 
+/** Number of parameter locations in @ref step_object. */
+#define STEP_NLOC	2
+
 /** Python representation of @ref addrxlat_step_t.
  */
 typedef struct {
@@ -3859,6 +3862,12 @@ typedef struct {
 	PyObject *ctx;
 	/** Translation step in libaddrxlat format. */
 	addrxlat_step_t step;
+
+	/** FullAddress object for @c base. */
+	PyObject *base;
+
+	/** Location configuration for @c step. */
+	param_loc loc[STEP_NLOC];
 
 	PyObject *convert;
 } step_object;
@@ -3897,8 +3906,18 @@ step_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 	}
 	Py_INCREF(ctxobj);
 	self->ctx = ctxobj;
+	Py_INCREF(Py_None);
+	self->base = Py_None;
 	Py_INCREF(convert);
 	self->convert = convert;
+
+	self->loc[0].ptr = &self->step;
+	self->loc[0].off = 0;
+	self->loc[0].len = sizeof(addrxlat_step_t);
+
+	self->loc[1].ptr = NULL;
+	self->loc[1].off = offsetof(addrxlat_step_t, base);
+	self->loc[1].len = sizeof(addrxlat_fulladdr_t);
 
 	return (PyObject*)self;
 }
@@ -4052,38 +4071,11 @@ step_set_meth(PyObject *_self, PyObject *value, void *data)
 PyDoc_STRVAR(step_base__doc__,
 "base address for next translation step");
 
-/** Getter for the base attribute.
- * @param _self  step object
- * @param data   ignored
- * @returns      FullAddress object (or @c NULL on failure)
- */
-static PyObject *
-step_get_base(PyObject *_self, void *data)
-{
-	step_object *self = (step_object*)_self;
-
-	return fulladdr_FromPointer(self->convert, &self->step.base);
-}
-
-/** Setter for the base attribute.
- * @param _self  step object
- * @param value  new value (a @c FullAddress)
- * @param data   ignored
- * @returns      zero on success, -1 otherwise
- */
-static int
-step_set_base(PyObject *_self, PyObject *value, void *data)
-{
-	step_object *self = (step_object*)_self;
-	const addrxlat_fulladdr_t *base;
-
-	base = fulladdr_AsPointer(value);
-	if (!base)
-		return -1;
-
-	self->step.base = *base;
-	return 0;
-}
+static fulladdr_loc step_base_loc = {
+	offsetof(step_object, base),
+	offsetof(step_object, loc[1]),
+	"base"
+};
 
 PyDoc_STRVAR(step_raw__doc__,
 "raw value from last step");
@@ -4248,7 +4240,8 @@ static PyGetSetDef step_getset[] = {
 	  OFFSETOF_PTR(step_object, ctx) },
 	{ "sys", step_get_sys, step_set_sys, step_sys__doc__ },
 	{ "meth", step_get_meth, step_set_meth, step_meth__doc__ },
-	{ "base", step_get_base, step_set_base, step_base__doc__ },
+	{ "base", get_fulladdr, set_fulladdr, step_base__doc__,
+	  &step_base_loc },
 	{ "raw", step_get_raw, step_set_raw,
 	  step_raw__doc__ },
 	{ "idx", step_get_idx, step_set_idx,
@@ -4946,7 +4939,7 @@ static PyMemberDef convert_members[] = {
  * structure embedded in the Python object, i.e. the pointer is
  * valid only as long as the containing Python object exists.
  *
- * If @c param is @c NULL, return a pointer to a null address singleton.
+ * If @c value is @c Py_None, return a pointer to a null address singleton.
  * This singleton should not be modified, as it would affect all other
  * @c None full addresses.
  */
@@ -5352,13 +5345,17 @@ sys_FromPointer(PyObject *_conv, addrxlat_sys_t *sys)
 static addrxlat_step_t *
 step_AsPointer(PyObject *value)
 {
+	step_object *stepobj;
+
 	if (!PyObject_TypeCheck(value, &step_type)) {
 		PyErr_Format(PyExc_TypeError, "need a Step, not '%.200s'",
 			     Py_TYPE(value)->tp_name);
 		return NULL;
 	}
 
-	return &((step_object*)value)->step;
+	stepobj = (step_object*)value;
+	loc_gather(stepobj->loc, STEP_NLOC, &stepobj->step);
+	return &stepobj->step;
 }
 
 /** Construct a step object from @c addrxlat_step_t.
@@ -5398,6 +5395,7 @@ step_Init(PyObject *_self, const addrxlat_step_t *step)
 {
 	step_object *self = (step_object *)_self;
 	PyObject *obj, *oldobj;
+	int result;
 
 	obj = ctx_FromPointer(self->convert, step->ctx);
 	if (!obj)
@@ -5406,7 +5404,15 @@ step_Init(PyObject *_self, const addrxlat_step_t *step)
 	self->ctx = obj;
 	Py_XDECREF(oldobj);
 
-	self->step = *step;
+	obj = fulladdr_FromPointer(self->convert, &step->base);
+	if (!obj)
+		return -1;
+	result = set_fulladdr((PyObject*)self, obj, &step_base_loc);
+	Py_DECREF(obj);
+	if (result)
+		return result;
+
+	loc_scatter(self->loc, STEP_NLOC, step);
 	return 0;
 }
 

python/test_addrxlat.py
@@ -470,7 +470,7 @@ class TestStep(unittest.TestCase):
         self.assertIs(step.meth, None)
         self.assertEqual(step.remain, 0)
         self.assertEqual(step.elemsz, 0)
-        self.assertEqual(step.base, addrxlat.FullAddress(0, 0))
+        self.assertIs(step.base, None)
         self.assertIs(step.raw, None)
         idx = (0,) * (addrxlat.FIELDS_MAX + 1)
         self.assertEqual(step.idx, idx)
@@ -483,7 +483,7 @@ class TestStep(unittest.TestCase):
         self.assertIs(step.meth, None)
         self.assertEqual(step.remain, 0)
         self.assertEqual(step.elemsz, 0)
-        self.assertEqual(step.base, addrxlat.FullAddress(0, 0))
+        self.assertIs(step.base, None)
         self.assertIs(step.raw, None)
         idx = (0,) * (addrxlat.FIELDS_MAX + 1)
         self.assertEqual(step.idx, idx)
@@ -496,7 +496,7 @@ class TestStep(unittest.TestCase):
         self.assertEqual(step.meth, meth)
         self.assertEqual(step.remain, 0)
         self.assertEqual(step.elemsz, 0)
-        self.assertEqual(step.base, addrxlat.FullAddress(0, 0))
+        self.assertIs(step.base, None)
         self.assertIs(step.raw, None)
         idx = (0,) * (addrxlat.FIELDS_MAX + 1)
         self.assertEqual(step.idx, idx)
@@ -510,7 +510,7 @@ class TestStep(unittest.TestCase):
         self.assertIs(step.meth, None)
         self.assertEqual(step.remain, 3)
         self.assertEqual(step.elemsz, 0)
-        self.assertEqual(step.base, addrxlat.FullAddress(0, 0))
+        self.assertIs(step.base, None)
         self.assertIs(step.raw, None)
         idx = (0,) * (addrxlat.FIELDS_MAX + 1)
         self.assertEqual(step.idx, idx)
@@ -524,7 +524,7 @@ class TestStep(unittest.TestCase):
         self.assertIs(step.meth, None)
         self.assertEqual(step.remain, 0)
         self.assertEqual(step.elemsz, 8)
-        self.assertEqual(step.base, addrxlat.FullAddress(0, 0))
+        self.assertIs(step.base, None)
         self.assertIs(step.raw, None)
         idx = (0,) * (addrxlat.FIELDS_MAX + 1)
         self.assertEqual(step.idx, idx)
@@ -538,7 +538,35 @@ class TestStep(unittest.TestCase):
         self.assertIs(step.meth, None)
         self.assertEqual(step.remain, 0)
         self.assertEqual(step.elemsz, 0)
-        self.assertEqual(step.base, base)
+        self.assertIs(step.base, base)
+        self.assertIs(step.raw, None)
+        idx = (0,) * (addrxlat.FIELDS_MAX + 1)
+        self.assertEqual(step.idx, idx)
+
+    def test_step_base_addrspace(self):
+        step = addrxlat.Step(self.ctx)
+        step.base = addrxlat.FullAddress(addrxlat.NOADDR, 0)
+        step.base.addrspace = addrxlat.KVADDR
+        self.assertIs(step.ctx, self.ctx)
+        self.assertIs(step.sys, None)
+        self.assertIs(step.meth, None)
+        self.assertEqual(step.remain, 0)
+        self.assertEqual(step.elemsz, 0)
+        self.assertEqual(step.base, addrxlat.FullAddress(addrxlat.KVADDR, 0))
+        self.assertIs(step.raw, None)
+        idx = (0,) * (addrxlat.FIELDS_MAX + 1)
+        self.assertEqual(step.idx, idx)
+
+    def test_step_base_addr(self):
+        step = addrxlat.Step(self.ctx)
+        step.base = addrxlat.FullAddress(addrxlat.NOADDR, 0)
+        step.base.addr = 0x1234
+        self.assertIs(step.ctx, self.ctx)
+        self.assertIs(step.sys, None)
+        self.assertIs(step.meth, None)
+        self.assertEqual(step.remain, 0)
+        self.assertEqual(step.elemsz, 0)
+        self.assertEqual(step.base, addrxlat.FullAddress(addrxlat.NOADDR, 0x1234))
         self.assertIs(step.raw, None)
         idx = (0,) * (addrxlat.FIELDS_MAX + 1)
         self.assertEqual(step.idx, idx)
@@ -555,7 +583,7 @@ class TestStep(unittest.TestCase):
         self.assertEqual(step.meth, meth)
         self.assertEqual(step.remain, 0)
         self.assertEqual(step.elemsz, 0)
-        self.assertEqual(step.base, addrxlat.FullAddress(0, 0))
+        self.assertIs(step.base, None)
         self.assertEqual(step.raw, 0xabcd)
         idx = (0,) * (addrxlat.FIELDS_MAX + 1)
         self.assertEqual(step.idx, idx)
@@ -569,7 +597,7 @@ class TestStep(unittest.TestCase):
         self.assertIs(step.meth, None)
         self.assertEqual(step.remain, 0)
         self.assertEqual(step.elemsz, 0)
-        self.assertEqual(step.base, addrxlat.FullAddress(0, 0))
+        self.assertIs(step.base, None)
         self.assertIs(step.raw, None)
         idx = idx + (0,) * (addrxlat.FIELDS_MAX + 1 - len(idx))
         self.assertEqual(step.idx, idx)

commit c98089ccd022d1c609b2a6b10edfdfb52c718c5f

python/addrxlat: Fix missing re-init after calling launch/step
The call to addrxlat_launch / addrxlat_step usually modifies the
content of the step structure. However, the internal state is not
re-initialized, so changes to "base" are lost...

Fixes: 71b1811671bb (python/addrxlat: Track modifications of step.base)

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/addrxlat.c
@@ -4435,6 +4435,7 @@ step_launch(PyObject *_self, PyObject *args, PyObject *kwargs)
 		return NULL;
 
 	status = addrxlat_launch(&self->step, addr);
+	step_Init((PyObject*)self, &self->step);
 	return ctx_status_result(self->ctx, status);
 }
 
@@ -4455,6 +4456,7 @@ step_step(PyObject *_self, PyObject *args)
 	addrxlat_status status;
 
 	status = addrxlat_step(&self->step);
+	step_Init((PyObject*)self, &self->step);
 	return ctx_status_result(self->ctx, status);
 }
 