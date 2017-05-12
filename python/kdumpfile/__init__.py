b6d2beef5f81f7923df7b639e8371ac263ebbe72
Petr Tesarik <ptesarik@suse.com>
2017-05-12 11:28:06 +0200

    python: initialize kdumpfile with a friendlier addrxlat TypeConvert
    In the long run, the whole _kdumpfile/kdumpfile module pair should be
    rewritten to match _addrxlat/addrxlat. For now, at least let's use
    user-friendly classes from addrxlat by default when using kdumpfile.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- python/kdumpfile/__init__.py
+++ python/kdumpfile/__init__.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 
 from _kdumpfile import *
+from addrxlat import convert as _convert
+
+class kdumpfile(kdumpfile):
+    def __init__(self, *args, **kwargs):
+        super(kdumpfile, self).__init__(*args, **kwargs)
+        self.addrxlat_convert = _convert
+
+import inspect as _inspect
+for _cls in globals().values():
+    if not _inspect.isclass(_cls):
+        continue
+    for _name, _method in _inspect.getmembers(_cls, _inspect.ismethod):
+        if _method.__doc__:
+            continue
+        for _parent in _inspect.getmro(_cls)[1:]:
+            if hasattr(_parent, _name):
+                _method.__func__.__doc__ = getattr(_parent, _name).__doc__
+                break
+
+# Free up temporary variables
+del _cls, _name, _method, _parent, _inspect
