ptesarik/libkdumpfile

commit ecbafcdff3def300b8208b2944912d6a34af6695

addrxlat: Rename addrxlat_sys_init to addrxlat_sys_os_init
The idea came through Python bindings, where init() was very similar
to __init__(), but could not be really merged, as it is possible to
manually fill in the mapping in an addrxlat_sys_t object without
calling sys_init() on it.

The main point is that the automatic initiliazation uses some
hard-wired heuristics for Linux and Xen. So, let's just add "os_"
into the name to make it obvious that this method is somehow related
to OS details.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/addrxlat.c
@@ -1154,7 +1154,7 @@ ctx_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 }
 
 static int
-ctx__init(PyObject *self, PyObject *args, PyObject *kwargs)
+ctx_init(PyObject *self, PyObject *args, PyObject *kwargs)
 {
 	return c_pointer_super_init(&ctx_type, self, args, kwargs);
 }
@@ -1541,7 +1541,7 @@ static PyTypeObject ctx_type =
 	0,				/* tp_descr_get */
 	0,				/* tp_descr_set */
 	0,				/* tp_dictoffset */
-	ctx__init,			/* tp_init */
+	ctx_init,			/* tp_init */
 	0,				/* tp_alloc */
 	ctx_new,			/* tp_new */
 };
@@ -2623,7 +2623,7 @@ meth_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 }
 
 static int
-meth__init(PyObject *self, PyObject *args, PyObject *kwargs)
+meth_init(PyObject *self, PyObject *args, PyObject *kwargs)
 {
 	return c_pointer_super_init(&meth_type, self, args, kwargs);
 }
@@ -2763,7 +2763,7 @@ static PyTypeObject meth_type =
 	0,				/* tp_descr_get */
 	0,				/* tp_descr_set */
 	0,				/* tp_dictoffset */
-	meth__init,			/* tp_init */
+	meth_init,			/* tp_init */
 	0,				/* tp_alloc */
 	meth_new,			/* tp_new */
 };
@@ -2931,7 +2931,7 @@ map_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 }
 
 static int
-map__init(PyObject *self, PyObject *args, PyObject *kwargs)
+map_init(PyObject *self, PyObject *args, PyObject *kwargs)
 {
 	return c_pointer_super_init(&map_type, self, args, kwargs);
 }
@@ -3157,7 +3157,7 @@ static PyTypeObject map_type =
 	0,				/* tp_descr_get */
 	0,				/* tp_descr_set */
 	0,				/* tp_dictoffset */
-	map__init,			/* tp_init */
+	map_init,			/* tp_init */
 	0,				/* tp_alloc */
 	map_new,			/* tp_new */
 };
@@ -3204,7 +3204,7 @@ sys_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 }
 
 static int
-sys__init(PyObject *self, PyObject *args, PyObject *kwargs)
+sys_init(PyObject *self, PyObject *args, PyObject *kwargs)
 {
 	return c_pointer_super_init(&sys_type, self, args, kwargs);
 }
@@ -3252,13 +3252,13 @@ sys_richcompare(PyObject *v, PyObject *w, int op)
 	return result;
 }
 
-PyDoc_STRVAR(sys_init__doc__,
-"SYS.init(ctx, arch[, type[, ver[, opts]]]) -> status\n\
+PyDoc_STRVAR(sys_os_init__doc__,
+"SYS.os_init(ctx, arch[, type[, ver[, opts]]]) -> status\n\
 \n\
 Set up a translation system for a pre-defined operating system.");
 
 static PyObject *
-sys_init(PyObject *_self, PyObject *args, PyObject *kwargs)
+sys_os_init(PyObject *_self, PyObject *args, PyObject *kwargs)
 {
 	sys_object *self = (sys_object*)_self;
 	static char *keywords[] = {
@@ -3284,7 +3284,7 @@ sys_init(PyObject *_self, PyObject *args, PyObject *kwargs)
 		return NULL;
 
 	osdesc.type = type;
-	status = addrxlat_sys_init(self->sys, ctx, &osdesc);
+	status = addrxlat_sys_os_init(self->sys, ctx, &osdesc);
 	return ctx_status_result(ctxobj, status);
 }
 
@@ -3416,8 +3416,8 @@ sys_get_meth(PyObject *_self, PyObject *args, PyObject *kwargs)
 }
 
 static PyMethodDef sys_methods[] = {
-	{ "init", (PyCFunction)sys_init, METH_VARARGS | METH_KEYWORDS,
-	  sys_init__doc__ },
+	{ "os_init", (PyCFunction)sys_os_init, METH_VARARGS | METH_KEYWORDS,
+	  sys_os_init__doc__ },
 	{ "set_map", (PyCFunction)sys_set_map, METH_VARARGS | METH_KEYWORDS,
 	  sys_set_map__doc__ },
 	{ "get_map", (PyCFunction)sys_get_map, METH_VARARGS | METH_KEYWORDS,
@@ -3474,7 +3474,7 @@ static PyTypeObject sys_type =
 	0,				/* tp_descr_get */
 	0,				/* tp_descr_set */
 	0,				/* tp_dictoffset */
-	sys__init,			/* tp_init */
+	sys_init,			/* tp_init */
 	0,				/* tp_alloc */
 	sys_new,			/* tp_new */
 };

include/addrxlat.h.in
@@ -820,7 +820,7 @@ unsigned long addrxlat_sys_decref(addrxlat_sys_t *sys);
  * determine the translation map and page-table translation for an
  * operating system.
  */
-addrxlat_status addrxlat_sys_init(
+addrxlat_status addrxlat_sys_os_init(
 	addrxlat_sys_t *sys, addrxlat_ctx_t *ctx,
 	const addrxlat_osdesc_t *osdesc);
 

src/addrxlat/addrxlat-priv.h
@@ -238,7 +238,7 @@ INTERNAL_DECL(addrxlat_status, parse_opts,
 /* Translation system */
 
 /** Data used during translation system initialization. */
-struct sys_init_data {
+struct os_init_data {
 	/** Target translation system. */
 	addrxlat_sys_t *sys;
 
@@ -256,7 +256,7 @@ struct sys_init_data {
  * @param ctl    Initialization data.
  * @returns      Error status.
  */
-typedef addrxlat_status sys_arch_fn(struct sys_init_data *ctl);
+typedef addrxlat_status sys_arch_fn(struct os_init_data *ctl);
 
 INTERNAL_DECL(sys_arch_fn, sys_ia32, );
 
@@ -285,17 +285,17 @@ struct sys_region {
 #define SYS_REGION_END	{ 0, 0, ADDRXLAT_SYS_METH_NUM }
 
 INTERNAL_DECL(addrxlat_status, sys_ensure_meth,
-	      (struct sys_init_data *ctl, addrxlat_sys_meth_t idx));
+	      (struct os_init_data *ctl, addrxlat_sys_meth_t idx));
 
 INTERNAL_DECL(addrxlat_status, sys_set_layout,
-	      (struct sys_init_data *ctl, addrxlat_sys_map_t idx,
+	      (struct os_init_data *ctl, addrxlat_sys_map_t idx,
 	       const struct sys_region layout[]));
 
 INTERNAL_DECL(addrxlat_status, sys_set_physmaps,
-	      (struct sys_init_data *ctl, addrxlat_addr_t maxaddr));
+	      (struct os_init_data *ctl, addrxlat_addr_t maxaddr));
 
 INTERNAL_DECL(addrxlat_status, sys_sym_pgtroot,
-	      (struct sys_init_data *ctl, const char *reg, const char *sym));
+	      (struct os_init_data *ctl, const char *reg, const char *sym));
 
 /* internal aliases */
 

src/addrxlat/ia32.c
@@ -174,7 +174,7 @@ static const addrxlat_paging_form_t ia32_pf_pae = {
  * On successful return, this function sets @c OPT_pae accordingly.
  */
 static addrxlat_status
-check_pae(struct sys_init_data *ctl, const addrxlat_fulladdr_t *root,
+check_pae(struct os_init_data *ctl, const addrxlat_fulladdr_t *root,
 	  addrxlat_addr_t direct)
 {
 	addrxlat_step_t step;
@@ -242,7 +242,7 @@ check_pae(struct sys_init_data *ctl, const addrxlat_fulladdr_t *root,
  * @returns    PAE status, see @ref check_pae.
  */
 static addrxlat_status
-check_pae_sym(struct sys_init_data *ctl)
+check_pae_sym(struct os_init_data *ctl)
 {
 	addrxlat_fulladdr_t rootpgt;
 	addrxlat_status status;
@@ -264,7 +264,7 @@ check_pae_sym(struct sys_init_data *ctl)
  * @returns    Error status.
  */
 static addrxlat_status
-sys_ia32_nonpae(struct sys_init_data *ctl)
+sys_ia32_nonpae(struct os_init_data *ctl)
 {
 	addrxlat_meth_t *meth;
 	addrxlat_desc_t desc;
@@ -291,7 +291,7 @@ sys_ia32_nonpae(struct sys_init_data *ctl)
  * @returns    Error status.
  */
 static addrxlat_status
-sys_ia32_pae(struct sys_init_data *ctl)
+sys_ia32_pae(struct os_init_data *ctl)
 {
 	addrxlat_meth_t *meth;
 	addrxlat_desc_t desc;
@@ -319,7 +319,7 @@ sys_ia32_pae(struct sys_init_data *ctl)
  * @returns     Error status.
  */
 static addrxlat_status
-try_vmap_area_list(struct sys_init_data *ctl, addrxlat_addr_t *addr)
+try_vmap_area_list(struct os_init_data *ctl, addrxlat_addr_t *addr)
 {
 	addrxlat_fulladdr_t vmap_area_list;
 	addrxlat_addr_t va_start, va_list, list_next;
@@ -369,7 +369,7 @@ try_vmap_area_list(struct sys_init_data *ctl, addrxlat_addr_t *addr)
  * @returns     Error status.
  */
 static addrxlat_status
-try_vmlist(struct sys_init_data *ctl, addrxlat_addr_t *addr)
+try_vmlist(struct os_init_data *ctl, addrxlat_addr_t *addr)
 {
 	addrxlat_fulladdr_t vmlist;
 	addrxlat_addr_t vm_addr;
@@ -408,7 +408,7 @@ try_vmlist(struct sys_init_data *ctl, addrxlat_addr_t *addr)
  * @returns     Error status.
  */
 static addrxlat_status
-set_linux_directmap(struct sys_init_data *ctl, addrxlat_map_t *vtop)
+set_linux_directmap(struct os_init_data *ctl, addrxlat_map_t *vtop)
 {
 	addrxlat_addr_t vmalloc_start;
 	addrxlat_range_t range;
@@ -461,7 +461,7 @@ static const struct sys_region linux_directmap[] = {
  * @returns    Error status.
  */
 addrxlat_status
-sys_ia32(struct sys_init_data *ctl)
+sys_ia32(struct os_init_data *ctl)
 {
 	addrxlat_range_t range;
 	addrxlat_map_t *newmap;

src/addrxlat/libaddrxlat.map
@@ -29,7 +29,7 @@ LIBADDRXLAT_0 {
     addrxlat_sys_new;
     addrxlat_sys_incref;
     addrxlat_sys_decref;
-    addrxlat_sys_init;
+    addrxlat_sys_os_init;
     addrxlat_sys_set_map;
     addrxlat_sys_get_map;
     addrxlat_sys_set_meth;

src/addrxlat/ppc64.c
@@ -259,7 +259,7 @@ static const struct sys_region linux_layout[] = {
  * @returns    Error status.
  */
 static addrxlat_status
-get_vmemmap_desc(struct sys_init_data *ctl, addrxlat_desc_t *desc)
+get_vmemmap_desc(struct os_init_data *ctl, addrxlat_desc_t *desc)
 {
 	addrxlat_step_t step =	/* step state surrogate */
 		{ .ctx = ctl->ctx, .sys = ctl->sys };
@@ -342,7 +342,7 @@ get_vmemmap_desc(struct sys_init_data *ctl, addrxlat_desc_t *desc)
  * @returns       Error status.
  */
 static addrxlat_status
-map_linux_ppc64(struct sys_init_data *ctl)
+map_linux_ppc64(struct os_init_data *ctl)
 {
 	static const addrxlat_paging_form_t ppc64_pf_64k = {
 		.pte_format = ADDRXLAT_PTE_PPC64_LINUX_RPN30,
@@ -408,7 +408,7 @@ map_linux_ppc64(struct sys_init_data *ctl)
  * @returns    Error status.
  */
 addrxlat_status
-sys_ppc64(struct sys_init_data *ctl)
+sys_ppc64(struct os_init_data *ctl)
 {
 	switch (ctl->osdesc->type) {
 	case ADDRXLAT_OS_LINUX:

src/addrxlat/s390x.c
@@ -122,7 +122,7 @@ pgt_s390x(addrxlat_step_t *step)
  * @returns          Error status.
  */
 static addrxlat_status
-get_pgtroot(struct sys_init_data *ctl, addrxlat_fulladdr_t *root)
+get_pgtroot(struct os_init_data *ctl, addrxlat_fulladdr_t *root)
 {
 	addrxlat_status status;
 
@@ -149,7 +149,7 @@ get_pgtroot(struct sys_init_data *ctl, addrxlat_fulladdr_t *root)
  * @returns    Error status.
  */
 static addrxlat_status
-determine_pgttype(struct sys_init_data *ctl)
+determine_pgttype(struct os_init_data *ctl)
 {
 	addrxlat_step_t step =	/* step state surrogate */
 		{ .ctx = ctl->ctx, .sys = ctl->sys };
@@ -201,7 +201,7 @@ determine_pgttype(struct sys_init_data *ctl)
  * @returns    Error status.
  */
 addrxlat_status
-sys_s390x(struct sys_init_data *ctl)
+sys_s390x(struct os_init_data *ctl)
 {
 	addrxlat_map_t *newmap;
 	addrxlat_range_t range;

src/addrxlat/sys.c
@@ -85,10 +85,10 @@ addrxlat_sys_decref(addrxlat_sys_t *sys)
 }
 
 addrxlat_status
-addrxlat_sys_init(addrxlat_sys_t *sys, addrxlat_ctx_t *ctx,
-		  const addrxlat_osdesc_t *osdesc)
+addrxlat_sys_os_init(addrxlat_sys_t *sys, addrxlat_ctx_t *ctx,
+		     const addrxlat_osdesc_t *osdesc)
 {
-	struct sys_init_data ctl;
+	struct os_init_data ctl;
 	sys_arch_fn *arch_fn;
 	addrxlat_status status;
 
@@ -166,7 +166,7 @@ addrxlat_sys_get_meth(const addrxlat_sys_t *sys, addrxlat_sys_meth_t idx)
  * @returns    Error status.
  */
 addrxlat_status
-sys_ensure_meth(struct sys_init_data *ctl, addrxlat_sys_meth_t idx)
+sys_ensure_meth(struct os_init_data *ctl, addrxlat_sys_meth_t idx)
 {
 	if (ctl->sys->meth[idx])
 		return ADDRXLAT_OK;
@@ -188,7 +188,7 @@ sys_ensure_meth(struct sys_init_data *ctl, addrxlat_sys_meth_t idx)
  * maps the current region to kernel physical addresses starting at 0.
  */
 static addrxlat_status
-act_direct(struct sys_init_data *ctl,
+act_direct(struct os_init_data *ctl,
 	   addrxlat_meth_t *meth, const struct sys_region *region)
 {
 	struct sys_region layout[2] = {
@@ -257,7 +257,7 @@ act_ident_machphys(addrxlat_meth_t *meth)
  * @returns       Error status.
  */
 addrxlat_status
-sys_set_layout(struct sys_init_data *ctl, addrxlat_sys_map_t idx,
+sys_set_layout(struct os_init_data *ctl, addrxlat_sys_map_t idx,
 	       const struct sys_region layout[])
 {
 	const struct sys_region *region;
@@ -321,7 +321,7 @@ sys_set_layout(struct sys_init_data *ctl, addrxlat_sys_map_t idx,
  * @returns       Error status.
  */
 addrxlat_status
-sys_set_physmaps(struct sys_init_data *ctl, addrxlat_addr_t maxaddr)
+sys_set_physmaps(struct os_init_data *ctl, addrxlat_addr_t maxaddr)
 {
 	struct sys_region layout[2];
 	addrxlat_status status;
@@ -348,7 +348,7 @@ sys_set_physmaps(struct sys_init_data *ctl, addrxlat_addr_t maxaddr)
  * @returns    Error status.
  */
 addrxlat_status
-sys_sym_pgtroot(struct sys_init_data *ctl, const char *reg, const char *sym)
+sys_sym_pgtroot(struct os_init_data *ctl, const char *reg, const char *sym)
 {
 	addrxlat_meth_t *meth;
 	addrxlat_addr_t addr;

commit 4abe023f9046465c291eb00a11bf2fd5f40b27b5

Python: Fix addrxlat.System.os_init() argument parsing error message
The name of the method is "os_init", not just "init".

Fixes: ecbafcdff3def300b8208b2944912d6a34af6695

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/addrxlat.c
@@ -4160,7 +4160,7 @@ sys_os_init(PyObject *_self, PyObject *args, PyObject *kwargs)
 	type = ADDRXLAT_OS_UNKNOWN;
 	osdesc.ver = 0;
 	osdesc.opts = NULL;
-	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "Os|lkz:init",
+	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "Os|lkz:os_init",
 					 keywords, &ctxobj, &osdesc.arch,
 					 &type, &osdesc.ver, &osdesc.opts))
 		return NULL;