208a05128b7318f95f02fb54ba9573b97a850ab4
Petr Tesarik <ptesarik@suse.com>
2018-01-05 08:55:47 +0100

    Rewrite convenience attribute accessors using kdump_get_typed_attr
    Use the new function for convenience functions.
    
    Also, add a test case for these convenience functions.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- tests/typed-attr.c
+++ tests/typed-attr.c
@@ -32,11 +32,15 @@
 
 #define ATTR_CACHE_SIZE	"cache.size"
 #define ATTR_SYSNAME	"linux.uts.sysname"
+#define ATTR_PHYS_BASE	"linux.phys_base"
 
 int main(int argc, char **argv)
 {
 	kdump_ctx_t *ctx;
 	kdump_attr_t attr;
+	kdump_num_t num;
+	kdump_addr_t addr;
+	const char *str;
 	kdump_status status;
 	int rc = TEST_OK;
 
@@ -46,6 +50,7 @@ int main(int argc, char **argv)
 		return TEST_ERR;
 	}
 
+	puts("# Test kdump_get_typed_attr:");
 
 	attr.type = KDUMP_NUMBER;
 	status = kdump_get_typed_attr(ctx, ATTR_CACHE_SIZE, &attr);
@@ -92,7 +97,84 @@ int main(int argc, char **argv)
 	attr.type = KDUMP_NUMBER;
 	status = kdump_get_typed_attr(ctx, ATTR_SYSNAME, &attr);
 	if (status == KDUMP_OK) {
+		fprintf(stderr, "%s can be read as a number??\n",
+			ATTR_SYSNAME);
+		rc = TEST_FAIL;
+	} else if (status != KDUMP_ERR_INVALID) {
+		fprintf(stderr, "Cannot read %s: %s\n",
+			ATTR_SYSNAME, kdump_get_err(ctx));
+		rc = TEST_FAIL;
+	} else
+		printf("%s as a number: %s\n",
+		       ATTR_SYSNAME, kdump_get_err(ctx));
+
+	puts("\n# Test convenience functions:");
+
+	status = kdump_get_number_attr(ctx, ATTR_CACHE_SIZE, &num);
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "Cannot read %s: %s\n",
+			ATTR_CACHE_SIZE, kdump_get_err(ctx));
+		rc = TEST_FAIL;
+	} else
+		printf("%s: %lld\n",
+		       ATTR_CACHE_SIZE, (long long)num);
+
+
+	status = kdump_get_string_attr(ctx, ATTR_CACHE_SIZE, &str);
+	if (status == KDUMP_OK) {
 		fprintf(stderr, "%s can be read as a string??\n",
+			ATTR_CACHE_SIZE);
+		rc = TEST_FAIL;
+	} else if (status != KDUMP_ERR_INVALID) {
+		fprintf(stderr, "Cannot read %s: %s\n",
+			ATTR_CACHE_SIZE, kdump_get_err(ctx));
+		rc = TEST_FAIL;
+	} else
+		printf("%s as a string: %s\n",
+		       ATTR_CACHE_SIZE, kdump_get_err(ctx));
+
+	attr.type = KDUMP_ADDRESS;
+	attr.val.address = 0x1234;
+	status = kdump_set_attr(ctx, ATTR_PHYS_BASE, &attr);
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "Cannot set %s: %s\n",
+			ATTR_PHYS_BASE, kdump_get_err(ctx));
+		return TEST_FAIL;
+	}
+
+	status = kdump_get_address_attr(ctx, ATTR_PHYS_BASE, &addr);
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "Cannot read %s: %s\n",
+			ATTR_PHYS_BASE, kdump_get_err(ctx));
+		rc = TEST_FAIL;
+	} else
+		printf("%s: %"ADDRXLAT_PRIXADDR"\n",
+		       ATTR_PHYS_BASE, addr);
+
+	status = kdump_get_string_attr(ctx, ATTR_PHYS_BASE, &str);
+	if (status == KDUMP_OK) {
+		fprintf(stderr, "%s can be read as a string??\n",
+			ATTR_PHYS_BASE);
+		rc = TEST_FAIL;
+	} else if (status != KDUMP_ERR_INVALID) {
+		fprintf(stderr, "Cannot read %s: %s\n",
+			ATTR_PHYS_BASE, kdump_get_err(ctx));
+		rc = TEST_FAIL;
+	} else
+		printf("%s as a string: %s\n",
+		       ATTR_PHYS_BASE, kdump_get_err(ctx));
+
+	status = kdump_get_string_attr(ctx, ATTR_SYSNAME, &str);
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "Cannot read %s: %s\n",
+			ATTR_SYSNAME, kdump_get_err(ctx));
+		rc = TEST_FAIL;
+	} else
+		printf("%s: %s\n", ATTR_SYSNAME, str);
+
+	status = kdump_get_number_attr(ctx, ATTR_SYSNAME, &num);
+	if (status == KDUMP_OK) {
+		fprintf(stderr, "%s can be read as a number??\n",
 			ATTR_SYSNAME);
 		rc = TEST_FAIL;
 	} else if (status != KDUMP_ERR_INVALID) {
