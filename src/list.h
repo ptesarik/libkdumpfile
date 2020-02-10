d1ba6dda61afac5f4c60f3c8a25f29e07c564588
Petr Tesarik <ptesarik@suse.com>
2020-02-10 09:47:01 +0100

    Add a double linked list suitable for hash tables
    Idea and some code shamelessly copied from the Linux kernel
    sources.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- src/list.h
+++ src/list.h
@@ -109,4 +109,85 @@ list_del(struct list_head *node)
 	     &(cur)->field != (head);					\
 	     cur = list_entry((cur)->field.next, typeof(*(cur)), field))
 
+/**  A single-pointer list head to a double linked list.
+ *
+ * Mostly useful for hash tables where the two pointer list head is
+ * too wasteful (hence the 'h' in its name).
+ * You lose the ability to access the tail in O(1).
+ *
+ * This structure is modelled after the Linux kernel list API.
+ */
+struct hlist_head {
+	struct hlist_node *first;
+};
+
+/**  Node of a double linked list with a single pointer list head.
+ *
+ * This structure is modelled after the Linux kernel list API.
+ */
+struct hlist_node {
+	/** Pointer to the next node. */
+	struct hlist_node *next;
+
+	/**< Pointer to the previous node's @c next pointer, or to
+	 * the list head's @c first pointer. */
+	struct hlist_node **pprev;
+};
+
+/**  Get a pointer to the structure which contains the hlist node.
+ * @param node   Pointer to a @c struct @ref hlist_node.
+ * @param type   Type of the containing structure.
+ * @param field	 Nam of the @c struct @ref hlist_node field inside @p type.
+ * @returns      Pointer to the containing structure.
+ */
+#define hlist_entry(node, type, field)				\
+	({ typeof(node) ____ptr = (node);			\
+	   ____ptr ? list_entry(____ptr, type, field) : NULL;	\
+	})
+
+/**  Remove an element from a hlist.
+ * @param node  Node to be deleted.
+ */
+static inline void hlist_del(struct hlist_node *node)
+{
+        struct hlist_node *next = node->next;
+        struct hlist_node **pprev = node->pprev;
+
+	*pprev = next;
+        if (next)
+                next->pprev = pprev;
+}
+
+/**  Add an element to the beginning of a hlist.
+ * @param node  Node to be added
+ * @param head  List head.
+ */
+static inline void
+hlist_add_head(struct hlist_node *node, struct hlist_head *head)
+{
+        struct hlist_node *first = head->first;
+        node->next = first;
+        if (first)
+                first->pprev = &node->next;
+        head->first = node;
+        node->pprev = &head->first;
+}
+
+/**  Iterate over a hlist.
+ * @param cur   List node to be used as the loop cursor.
+ * @param head  List head.
+ */
+#define hlist_for_each(cur, head) \
+        for (cur = (head)->first; cur ; cur = cur->next)
+
+/**  Iterate over a hlist of a given type.
+ * @param cur    Typed pointer to be used as the loop cursor.
+ * @param head   List head.
+ * @param field  Name of the @c struct @ref hlist_node field inside @p cur.
+ */
+#define hlist_for_each_entry(cur, head, field)				\
+        for (cur = hlist_entry((head)->first, typeof(*(cur)), field);	\
+             cur;							\
+             cur = hlist_entry((cur)->field.next, typeof(*(cur)), field))
+
 #endif	/* list.h */
