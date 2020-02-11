ebe4b4b482b5ad86b596e6c3171f1609a136991b
Petr Tesarik <ptesarik@suse.com>
2020-02-11 22:16:06 +0100

    Convert ppc64 registers to use a PRSTATUS blob attribute
    Use a PRSTATUS blob attribute for ppc64 dumps.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- src/kdumpfile/ppc64.c
+++ src/kdumpfile/ppc64.c
@@ -37,54 +37,6 @@
 #include <stdlib.h>
 #include <elf.h>
 
-static const struct attr_template reg_names[] = {
-#define REG(name)	{ #name, NULL, KDUMP_NUMBER }
-	REG(r0),
-	REG(r1),
-	REG(r2),
-	REG(r3),
-	REG(r4),
-	REG(r5),
-	REG(r6),
-	REG(r7),
-	REG(r8),
-	REG(r9),
-	REG(r10),
-	REG(r11),
-	REG(r12),
-	REG(r13),
-	REG(r14),
-	REG(r15),
-	REG(r16),
-	REG(r17),
-	REG(r18),
-	REG(r19),
-	REG(r20),
-	REG(r21),
-	REG(r22),
-	REG(r23),
-	REG(r24),
-	REG(r25),
-	REG(r26),
-	REG(r27),
-	REG(r28),
-	REG(r29),
-	REG(r30),
-	REG(r31),
-	REG(pc),
-	REG(msr),
-	REG(or3),
-	REG(ctr),
-	REG(lr),
-	REG(xer),
-	REG(ccr),
-	REG(softe),
-	REG(trap),
-	REG(dar),
-	REG(dsisr),
-	REG(res),
-};
-
 /** @cond TARGET_ABI */
 
 #define ELF_NGREG 48
@@ -119,32 +71,86 @@ struct elf_prstatus
 
 /** @endcond */
 
-/**  Number of registers in struct pt_regs.
- *
- * This is the number of registers that are actually saved in a kernel crash
- * dump. Rest of the structure is always filled with zeroes.
- */
-#define ELF_NGREG_PTREGS	44
+#define PRINFO(name, field, bits) \
+	{ { #name, { .depth = 0 }, KDUMP_NUMBER },	\
+	  offsetof(struct elf_prstatus, field), \
+	  (bits) / BITS_PER_BYTE }
+
+#define REG(name, field, bits) \
+	{ { #name, { .depth = 1 }, KDUMP_NUMBER },	\
+	  offsetof(struct elf_prstatus, field), \
+	  (bits) / BITS_PER_BYTE }
+
+static struct blob_attr_def ppc64_reg_attrs[] = {
+	REG(r0,  pr_reg[0], 64),
+	REG(r1,  pr_reg[1], 64),
+	REG(r2,  pr_reg[2], 64),
+	REG(r3,  pr_reg[3], 64),
+	REG(r4,  pr_reg[4], 64),
+	REG(r5,  pr_reg[5], 64),
+	REG(r6,  pr_reg[6], 64),
+	REG(r7,  pr_reg[7], 64),
+	REG(r8,  pr_reg[8], 64),
+	REG(r9,  pr_reg[9], 64),
+	REG(r10, pr_reg[10], 64),
+	REG(r11, pr_reg[11], 64),
+	REG(r12, pr_reg[12], 64),
+	REG(r13, pr_reg[13], 64),
+	REG(r14, pr_reg[14], 64),
+	REG(r15, pr_reg[15], 64),
+	REG(r16, pr_reg[16], 64),
+	REG(r17, pr_reg[17], 64),
+	REG(r18, pr_reg[18], 64),
+	REG(r19, pr_reg[19], 64),
+	REG(r20, pr_reg[20], 64),
+	REG(r21, pr_reg[21], 64),
+	REG(r22, pr_reg[22], 64),
+	REG(r23, pr_reg[23], 64),
+	REG(r24, pr_reg[24], 64),
+	REG(r25, pr_reg[25], 64),
+	REG(r26, pr_reg[26], 64),
+	REG(r27, pr_reg[27], 64),
+	REG(r28, pr_reg[28], 64),
+	REG(r29, pr_reg[29], 64),
+	REG(r30, pr_reg[30], 64),
+	REG(r31, pr_reg[31], 64),
+	REG(pc,  pr_reg[32], 64),
+	REG(msr, pr_reg[33], 64),
+	REG(or3, pr_reg[34], 64),
+	REG(ctr, pr_reg[35], 64),
+	REG(lr,  pr_reg[36], 64),
+	REG(xer, pr_reg[37], 64),
+	REG(ccr, pr_reg[38], 64),
+	REG(softe, pr_reg[39], 64),
+	REG(trap, pr_reg[40], 64),
+	REG(dar,  pr_reg[41], 64),
+	REG(dsisr, pr_reg[42], 64),
+	REG(res, pr_reg[43], 64),
+	PRINFO(pid, pr_pid, 32),
+};
 
 static kdump_status
 process_ppc64_prstatus(kdump_ctx_t *ctx, const void *data, size_t size)
 {
-	const struct elf_prstatus *status = data;
-	kdump_status res;
+	unsigned cpu;
+	kdump_status status;
+
+	cpu = get_num_cpus(ctx);
+	set_num_cpus(ctx, get_num_cpus(ctx) + 1);
+
+	status = set_cpu_prstatus(ctx, cpu, data, size);
+	if (status != KDUMP_OK)
+		return set_error(ctx, status,
+				 "Cannot set CPU %u PRSTATUS", cpu);
 
 	if (size < sizeof(struct elf_prstatus))
 		return set_error(ctx, KDUMP_ERR_CORRUPT,
 				 "Wrong PRSTATUS size: %zu", size);
 
-	res = set_cpu_regs64(ctx, get_num_cpus(ctx), reg_names,
-			     status->pr_reg, ELF_NGREG_PTREGS);
+	status = create_cpu_regs(
+		ctx, cpu, ppc64_reg_attrs, ARRAY_SIZE(ppc64_reg_attrs));
 
-	if (res != KDUMP_OK)
-		return res;
-
-	set_num_cpus(ctx, get_num_cpus(ctx) + 1);
-
-	return KDUMP_OK;
+	return status;
 }
 
 const struct arch_ops ppc64_ops = {
