fd6e61b7576c26b5d465eb9acf315c40803bd918
Petr Tesarik <ptesarik@suse.com>
2020-02-18 10:20:58 +0100

    Python support for AArch64
    Add the new constant. Update the example script.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- python/showxlat.py
+++ python/showxlat.py
@@ -43,6 +43,7 @@ pte_formats = {
     addrxlat.PTE_NONE: 'none',
     addrxlat.PTE_PFN32: 'pfn32',
     addrxlat.PTE_PFN64: 'pfn64',
+    addrxlat.PTE_AARCH64: 'aarch64',
     addrxlat.PTE_IA32: 'ia32',
     addrxlat.PTE_IA32_PAE: 'ia32_pae',
     addrxlat.PTE_X86_64: 'x86_64',
