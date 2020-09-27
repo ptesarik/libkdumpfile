ptesarik/libkdumpfile

commit 330148658156ce9a20ef55781ef5055f1992221d

Use Python distutils to build and install Python bindings
Although GNU Automake supports building Python modules and
extensions, this approach does not allow to use standard Python
packaging methods.

One of the reasons to stay with GNU Automake has been lack of
support for GNU Libtool, which is used to build the main library.
In particular, linking to a non-installed library requires
libtool. This commit adds a `libtoolize` module, which modifies
the default compiler to use libtool.

Some parameters must be passed from GNU autotools to distutils.
This is solved by generating a `setup.cfg` file from Makefile.

There are a few gotchas:

1. To build a shared library, libtool requires the `-rpath` option
   at link time, but Python distutils do not care about the
   destination directory during build. I have to pass `pyexecdir`
   explicitly to the build command.

2. The build tree may be different from the source tree (aka VPATH
   build). The source directory is passed down via a `srcdir`
   option and used as `package_dir`. It must be also explicitly
   prepended to all extension source files.

3. The libtool files for libkdumpfile and libaddrxlat must be
   added as `extra_objects`, because the libraries are not yet
   installed. Full path to the objects must be provided, and that
   one is relative to the build tree, so we also pass down
   `top_builddir` as an option.

Note that tests have not been converted (yet), because they should
not be installed, but distutils do not seem to implement that
functionality.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/setup.py
@@ -1,0 +1,55 @@
+from distutils.core import setup, Extension
+import libtoolize
+import os
+
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+cfg = ConfigParser()
+cfg.read('setup.cfg')
+srcdir = cfg.get('kdumpfile', 'srcdir')
+top_builddir = cfg.get('kdumpfile', 'top_builddir')
+addrxlat_la = os.path.join(
+    top_builddir, 'src', 'addrxlat', 'libaddrxlat.la')
+kdumpfile_la = os.path.join(
+    top_builddir, 'src', 'kdumpfile', 'libkdumpfile.la')
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+) ",
+    "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)  ",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Debuggers "]
+
+setup(name='libkdumpfile',
+      version=cfg.get('kdumpfile', 'version'),
+      description='Python bindings for libkdumpfile',
+      author='Petr Tesarik',
+      author_email='ptesarik@suse.com',
+      url='https://github.com/ptesarik/libkdumpfile',
+      packages=['addrxlat', 'kdumpfile'],
+      package_dir={'': srcdir},
+      ext_modules=[
+          Extension('_addrxlat', [os.path.join(srcdir, 'addrxlat.c')],
+                    extra_objects=[addrxlat_la]),
+          Extension('_kdumpfile', [os.path.join(srcdir, 'kdumpfile.c')],
+                    extra_objects=[kdumpfile_la]),
+      ],
+      options={
+          'build_ext': {
+              'libtool': cfg.get('kdumpfile', 'libtool'),
+              'pyexecdir': cfg.get('kdumpfile', 'pyexecdir'),
+          },
+          'install_lib': {
+              'libtool_install': cfg.get('kdumpfile', 'libtool_install'),
+          },
+      },
+      cmdclass={
+          'build_ext': libtoolize.build_ext,
+          'install_lib': libtoolize.install_lib,
+      },
+)

configure.ac
@@ -108,8 +108,6 @@ AC_CONFIG_FILES([
 	src/addrxlat/Makefile
 	src/kdumpfile/Makefile
 	python/Makefile
-	python/addrxlat/Makefile
-	python/kdumpfile/Makefile
 	tests/Makefile
 	libaddrxlat.pc
 	libkdumpfile.pc

python/.gitignore
@@ -1,3 +1,9 @@
+# generated by Makefile
+setup.cfg
+
+# distutils build
+build
+
 # Test results
 *.log
 *.trs

python/Makefile.am
@@ -23,17 +23,27 @@ AM_CPPFLAGS = -I$(top_builddir)/include
 AM_CFLAGS = $(PYTHON_CFLAGS)
 AM_LDFLAGS = -module -avoid-version
 
-pyexec_LTLIBRARIES = _addrxlat.la _kdumpfile.la
+setup.cfg: Makefile
+	$(AM_V_GEN)
+	$(AM_V_at)echo "[kdumpfile]" > $@
+	$(AM_V_at)echo "version=$(VERSION)" >> $@
+	$(AM_V_at)echo "libtool=$(LIBTOOL)" >> $@
+	$(AM_V_at)echo "libtool_install=$(LIBTOOL) --mode=install $(INSTALL)" >> $@
+	$(AM_V_at)echo "pyexecdir=$(pyexecdir)" >> $@
+	$(AM_V_at)echo "srcdir=$(srcdir)" >> $@
+	$(AM_V_at)echo "top_builddir=$(top_builddir)" >> $@
 
-_addrxlat_la_SOURCES = addrxlat.c
-_addrxlat_la_LDFLAGS = $(AM_LDFLAGS) -export-symbols $(srcdir)/addrxlat.sym
-_addrxlat_la_LIBADD = $(PYTHON_LIBS) $(top_builddir)/src/addrxlat/libaddrxlat.la
-EXTRA__addrxlat_la_DEPENDENCIES = addrxlat.sym
+all-local: setup.cfg
+	$(PYTHON) $(srcdir)/setup.py build
 
-_kdumpfile_la_SOURCES = kdumpfile.c
-_kdumpfile_la_LDFLAGS = $(AM_LDFLAGS) -export-symbols $(srcdir)/kdumpfile.sym
-_kdumpfile_la_LIBADD = $(PYTHON_LIBS) $(top_builddir)/src/kdumpfile/libkdumpfile.la
-EXTRA__kdumpfile_la_DEPENDENCIES = kdumpfile.sym
+install-exec-local: setup.cfg
+	$(PYTHON) $(srcdir)/setup.py install \
+		--install-purelib $(pythondir) \
+		--install-platlib $(pyexecdir)
+
+mostlyclean-local: setup.cfg
+	$(PYTHON) $(srcdir)/setup.py clean --all
+	rm $<
 
 check_LTLIBRARIES = _test_addrxlat.la
 
@@ -45,12 +55,21 @@ _test_addrxlat_la_LDFLAGS = \
 _test_addrxlat_la_LIBADD = $(PYTHON_LIBS) $(top_builddir)/src/addrxlat/libaddrxlat.la
 EXTRA__test_addrxlat_la_DEPENDENCIES = test_addrxlat.sym
 
-dist_noinst_DATA = \
-	addrxlat.sym \
-	kdumpfile.sym \
-	test_addrxlat.sym \
+dist_noinst_SCRIPTS = \
+	setup.py \
+	libtoolize.py \
+	addrxlat/__init__.py \
+	addrxlat/exceptions.py \
+	kdumpfile/__init__.py \
+	kdumpfile/exceptions.py \
+	kdumpfile/views.py \
 	showxlat.py \
 	vtop.py
+
+dist_noinst_DATA = \
+	addrxlat.c \
+	kdumpfile.c \
+	test_addrxlat.sym
 
 noinst_HEADERS = \
 	addrxlatmod.h
@@ -68,5 +87,3 @@ LOG_COMPILER = $(PYTHON)
 AM_TESTS_ENVIRONMENT = \
 eval " $$($(LIBTOOL) --config)"; \
 PYTHONPATH="$$srcdir:$$objdir"
-
-SUBDIRS = addrxlat kdumpfile

python/libtoolize.py
@@ -1,0 +1,132 @@
+"""libtool
+
+Build and install extensions using libtool.
+"""
+
+import os
+from distutils import log
+from distutils.util import split_quoted
+from distutils.dir_util import mkpath
+from distutils.file_util import copy_file
+from distutils.spawn import spawn
+from distutils.errors import DistutilsFileError
+
+from distutils.command.build_ext import build_ext as _build_ext
+class build_ext(_build_ext):
+    def initialize_options(self):
+        _build_ext.initialize_options(self)
+        self.libtool = None
+        self.pyexecdir = None
+
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+
+        if self.libtool is None:
+            self.libtool = 'libtool'
+        if self.pyexecdir is None:
+            import sysconfig
+            self.pyexecdir = sysconfig.get_path('platlib')
+        self.cmd_libtool = split_quoted(self.libtool)
+
+    def libtoolize(self, key, mode, *args):
+        val = getattr(self.compiler, key)
+        val = val[:1] + list(args) + val[1:]
+        setattr(self.compiler, key,
+                self.cmd_libtool + [ '--mode='+mode ] + val)
+
+    def build_extensions(self):
+        self.compiler.obj_extension = '.lo'
+        self.libtoolize('compiler', 'compile')
+        self.libtoolize('compiler_so', 'compile')
+        self.libtoolize('linker_exe', 'link')
+        self.libtoolize('linker_so', 'link',
+                        '-module', '-avoid-version',
+                        '-export-symbols-regex', 'init.*|PyInit_.*',
+                        '-rpath', self.pyexecdir)
+        _build_ext.build_extensions(self)
+
+from distutils.command.install_lib import install_lib as _install_lib
+class install_lib(_install_lib):
+    def initialize_options(self):
+        _install_lib.initialize_options(self)
+        self.libtool_install = None
+
+    def finalize_options(self):
+        _install_lib.finalize_options(self)
+
+        if self.libtool_install is None:
+            self.libtool_install = 'libtool --mode=install install'
+        self.cmd_libtool_install = split_quoted(self.libtool_install)
+
+    def copy_tree(self, infile, outfile, preserve_mode=1, preserve_times=1,
+                   preserve_symlinks=0, level=1):
+        """Copy the build directory tree, respecting dry-run and force flags.
+        Special treatment of libtool files.
+        """
+        if not self.dry_run and not os.path.isdir(infile):
+            raise DistutilsFileError(
+                  "cannot copy tree '%s': not a directory" % infile)
+        try:
+            names = os.listdir(infile)
+        except OSError as e:
+            if self.dry_run:
+                names = []
+            else:
+                raise DistutilsFileError(
+                      "error listing files in '%s': %s" % (infile, e.strerror))
+
+        if not self.dry_run:
+            mkpath(outfile)
+
+        outputs = []
+
+        for n in names:
+            src_name = os.path.join(infile, n)
+            dst_name = os.path.join(outfile, n)
+
+            if n.startswith('.nfs'):
+                # skip NFS rename files
+                continue
+            if n in ('.libs', '_libs'):
+                # skip libtool directories
+                continue
+
+            if preserve_symlinks and os.path.islink(src_name):
+                link_dest = os.readlink(src_name)
+                log.info("linking %s -> %s", dst_name, link_dest)
+                if not self.dry_run:
+                    os.symlink(link_dest, dst_name)
+                outputs.append(dst_name)
+
+            elif os.path.isdir(src_name):
+                outputs.extend(
+                    self.copy_tree(src_name, dst_name, preserve_mode,
+                                   preserve_times, preserve_symlinks))
+
+            elif n.endswith('.la'):
+                spawn(self.cmd_libtool_install + [ src_name, dst_name ],
+                      dry_run=self.dry_run)
+
+            else:
+                copy_file(src_name, dst_name, preserve_mode,
+                          preserve_times, not self.force,
+                          dry_run=self.dry_run)
+                outputs.append(dst_name)
+
+        return outputs
+
+def sysconfig_replace_ext(varname, newext):
+    """Replace the extension in a system config variable.
+    """
+    from distutils.sysconfig import _config_vars
+    val = _config_vars.get(varname, '')
+    suffpos = val.rfind('.')
+    if suffpos >= 0:
+        val = val[:suffpos]
+    _config_vars[varname] = val + newext
+
+# change default shared object suffix to .la
+from distutils.sysconfig import get_config_vars
+get_config_vars()
+sysconfig_replace_ext('EXT_SUFFIX', '.la')
+sysconfig_replace_ext('SO', '.la')

commit 613192c375727ce190f6a4a793ffa3772b10a522

python: Specify includedir for extension modules
This bug was not found during development, because the include
files were already installed on the system, but of course, the
path to the not yet installed include directory must be given to
the compiler.

Fixes: 330148658156ce9a20ef55781ef5055f1992221d
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/setup.py
@@ -11,6 +11,7 @@ cfg = ConfigParser()
 cfg.read('setup.cfg')
 srcdir = cfg.get('kdumpfile', 'srcdir')
 top_builddir = cfg.get('kdumpfile', 'top_builddir')
+include_dir=os.path.join(top_builddir, 'include'),
 addrxlat_la = os.path.join(
     top_builddir, 'src', 'addrxlat', 'libaddrxlat.la')
 kdumpfile_la = os.path.join(
@@ -35,8 +36,10 @@ setup(name='libkdumpfile',
       package_dir={'': srcdir},
       ext_modules=[
           Extension('_addrxlat', [os.path.join(srcdir, 'addrxlat.c')],
+                    include_dirs=[include_dir],
                     extra_objects=[addrxlat_la]),
           Extension('_kdumpfile', [os.path.join(srcdir, 'kdumpfile.c')],
+                    include_dirs=[include_dir],
                     extra_objects=[kdumpfile_la]),
       ],
       cmdclass={