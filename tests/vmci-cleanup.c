ptesarik/libkdumpfile

commit b4b3c94cc24dc88d08701ebe35f72a1f1785046a

Convert vmcoreinfo.raw attributes to blob type
The string type are intended for relatively short strings that are
part of the attribute. This does not really fit the vmcoreinfo raw
value, but no better type was available at that time.

This has worked out quite well (mostly because few users are using
this attribute), but there are some subtle issues. For example,
the raw value must be NUL-terminated; the NUL byte is added
artificially, but it is not possible to know if NUL was actually
part of the value, or not. This is not so important for parsing
the VMCOREINFO content, but if the raw data should be processed in
other ways (e.g. saved to another file), we'd better provide an
interface that is binary clean.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/vmcoreinfo.c
@@ -214,7 +214,8 @@ static const struct attr_ops vmcoreinfo_lines_ops = {
 static kdump_status
 vmcoreinfo_raw_post_hook(kdump_ctx_t *ctx, struct attr_data *rawattr)
 {
-	const char *p, *endp, *val;
+	kdump_blob_t *blob;
+	const char *p, *endp, *endl, *val;
 	size_t len;
 	struct attr_data *dir;
 	kdump_status res;
@@ -222,24 +223,27 @@ vmcoreinfo_raw_post_hook(kdump_ctx_t *ctx, struct attr_data *rawattr)
 	dir = rawattr->parent;
 	dealloc_vmcoreinfo(dir);
 
-	for (p = attr_value(rawattr)->string; *p; p = endp) {
-		endp = strchrnul(p, '\n');
-		val = memchr(p, '=', endp - p);
+	blob = attr_value(rawattr)->blob;
+	p = internal_blob_pin(blob);
+	endp = p + blob->size;
+	while (p < endp) {
+		endl = memchr(p, '\n', endp - p) ?: endp;
+		val = memchr(p, '=', endl - p);
 		if (val) {
 			len = val - p;
 			++val;
 		} else {
-			val = endp;
+			val = endl;
 			len = val - p;
 		}
 
-		res = add_parsed_row(ctx, dir, p, len, val, endp - val);
+		res = add_parsed_row(ctx, dir, p, len, val, endl - val);
 		if (res != KDUMP_OK)
 			break;
 
-		if (*endp)
-			++endp;
+		p = endl + 1;
 	}
+	internal_blob_unpin(blob);
 
 	return res;
 }

tests/vmci-cleanup.c
@@ -39,23 +39,43 @@ static const char vmcore2[] =
 	"DIR.SUB=test2\n";
 
 static int
-check(kdump_ctx_t *ctx)
+set_vmcoreinfo_value(kdump_ctx_t *ctx, int cnt, const char *val)
 {
-	unsigned cnt;
+	kdump_blob_t *blob;
 	kdump_attr_t attr;
 	kdump_status status;
 
-	/* First value. */
-	cnt = 1;
-
-	attr.type = KDUMP_STRING;
-	attr.val.string = vmcore1;
+	blob = kdump_blob_new_dup(val, strlen(val));
+	if (!blob) {
+		fprintf(stderr, "#%d: Cannot allocate VMCOREINFO blob.\n",
+			cnt);
+		return TEST_ERR;
+	}
+	attr.type = KDUMP_BLOB;
+	attr.val.blob = blob;
 	status = kdump_set_attr(ctx, "linux.vmcoreinfo.raw", &attr);
 	if (status != KDUMP_OK) {
 		fprintf(stderr, "#%d: Cannot set vmcoreinfo: %s\n",
 			cnt, kdump_get_err(ctx));
 		return TEST_ERR;
 	}
+
+	return TEST_OK;
+}
+
+static int
+check(kdump_ctx_t *ctx)
+{
+	unsigned cnt;
+	kdump_attr_t attr;
+	kdump_status status;
+	int rc;
+
+	/* First value. */
+	cnt = 1;
+	rc = set_vmcoreinfo_value(ctx, cnt, vmcore1);
+	if (rc != TEST_OK)
+		return rc;
 
 	status = kdump_get_attr(ctx, "linux.vmcoreinfo.lines.DIR.SUB.VAL",
 				&attr);
@@ -73,15 +93,9 @@ check(kdump_ctx_t *ctx)
 
 	/* (Conflicting) second value */
 	cnt = 2;
-
-	attr.type = KDUMP_STRING;
-	attr.val.string = vmcore2;
-	status = kdump_set_attr(ctx, "linux.vmcoreinfo.raw", &attr);
-	if (status != KDUMP_OK) {
-		fprintf(stderr, "#%d: Cannot set vmcoreinfo: %s\n",
-			cnt, kdump_get_err(ctx));
-		return TEST_ERR;
-	}
+	rc = set_vmcoreinfo_value(ctx, cnt, vmcore2);
+	if (rc != TEST_OK)
+		return rc;
 
 	status = kdump_get_attr(ctx, "linux.vmcoreinfo.lines.DIR.SUB", &attr);
 	if (status != KDUMP_OK) {

src/kdumpfile/diskdump.c
@@ -506,6 +506,7 @@ static kdump_status
 read_vmcoreinfo(kdump_ctx_t *ctx, off_t off, size_t size)
 {
 	struct fcache_chunk fch;
+	kdump_attr_value_t val;
 	kdump_status ret;
 
 	ret = fcache_get_chunk(ctx->shared->fcache, &fch, size, off);
@@ -514,9 +515,12 @@ read_vmcoreinfo(kdump_ctx_t *ctx, off_t off, size_t size)
 				 "Cannot read %zu VMCOREINFO bytes at %llu",
 				 size, (unsigned long long) off);
 
-	ret = set_attr_sized_string(
-		ctx, gattr(ctx, GKI_linux_vmcoreinfo_raw),
-		ATTR_DEFAULT, fch.data, size);
+	val.blob = internal_blob_new_dup(fch.data, size);
+	if (!val.blob)
+		return set_error(ctx, KDUMP_ERR_SYSTEM,
+				 "Cannot allocate %s", "VMCOREINFO blob");
+	ret = set_attr(ctx, gattr(ctx, GKI_linux_vmcoreinfo_raw),
+		       ATTR_DEFAULT, &val);
 	if (ret != KDUMP_OK)
 		ret = set_error(ctx, ret, "Cannot set VMCOREINFO");
 

src/kdumpfile/global-attr.def
@@ -77,7 +77,7 @@ ATTR(linux_uts, "domainname", linux_uts_domainname, string, const char *)
 ATTR(linux, "vmcoreinfo", dir_linux_vmcoreinfo, directory, struct attr_data *)
 ATTR(linux_vmcoreinfo, "lines", linux_vmcoreinfo_lines,
      directory, struct attr_data *)
-ATTR(linux_vmcoreinfo, "raw", linux_vmcoreinfo_raw, string, const char *,
+ATTR(linux_vmcoreinfo, "raw", linux_vmcoreinfo_raw, blob, kdump_blob_t *,
 	.ops = &vmcoreinfo_raw_ops)
 ATTR(linux_vmcoreinfo, "LENGTH", linux_length, directory, struct attr_data *)
 ATTR(linux_vmcoreinfo, "NUMBER", linux_number, directory, struct attr_data *)
@@ -103,7 +103,7 @@ ATTR(xen, "version_code", xen_version_code, number, unsigned,
 ATTR(xen, "vmcoreinfo", dir_xen_vmcoreinfo, directory, struct attr_data *)
 ATTR(xen_vmcoreinfo, "lines", xen_vmcoreinfo_lines,
      directory, struct attr_data *)
-ATTR(xen_vmcoreinfo, "raw", xen_vmcoreinfo_raw, string, const char *,
+ATTR(xen_vmcoreinfo, "raw", xen_vmcoreinfo_raw, blob, kdump_addr_t *,
 	.ops = &vmcoreinfo_raw_ops)
 ATTR(xen_vmcoreinfo, "LENGTH", xen_length, directory, struct attr_data *)
 ATTR(xen_vmcoreinfo, "NUMBER", xen_number, directory, struct attr_data *)

src/kdumpfile/notes.c
@@ -348,12 +348,17 @@ static kdump_status
 set_vmcoreinfo(kdump_ctx_t *ctx, enum global_keyidx key,
 	       void *data, size_t sz, const char *what)
 {
+	kdump_attr_value_t val;
 	kdump_status res;
 
-	res = set_attr_sized_string(ctx, gattr(ctx, key),
-				    ATTR_DEFAULT, data, sz);
+	val.blob = internal_blob_new_dup(data, sz);
+	if (!val.blob)
+		return set_error(ctx, KDUMP_ERR_SYSTEM,
+				 "Cannot allocate %s", what);
+	res = set_attr(ctx, gattr(ctx, key), ATTR_DEFAULT, &val);
 	if (res != KDUMP_OK)
 		return set_error(ctx, res, "Cannot set %s", what);
+
 	return KDUMP_OK;
 }
 

src/kdumpfile/s390x.c
@@ -79,6 +79,7 @@ read_os_info_from_lowcore(kdump_ctx_t *ctx)
 	uint64_t magic;
 	uint32_t csum, csum_expect;
 	void *vmcoreinfo;
+	kdump_attr_value_t val;
 	kdump_status ret;
 
 	sz = sizeof(addr);
@@ -141,9 +142,14 @@ read_os_info_from_lowcore(kdump_ctx_t *ctx)
 				 (unsigned long) csum_expect);
 	}
 
-	ret = set_attr_sized_string(ctx, gattr(ctx, GKI_linux_vmcoreinfo_raw),
-				    ATTR_DEFAULT, vmcoreinfo, sz);
-	free(vmcoreinfo);
+	val.blob = internal_blob_new(vmcoreinfo, sz);
+	if (!val.blob) {
+		free(vmcoreinfo);
+		return set_error(ctx, KDUMP_ERR_SYSTEM,
+				 "Cannot allocate %s", "VMCOREINFO blob");
+	}
+	ret = set_attr(ctx, gattr(ctx, GKI_linux_vmcoreinfo_raw),
+		       ATTR_DEFAULT, &val);
 	if (ret != KDUMP_OK)
 		return set_error(ctx, ret, "Cannot set VMCOREINFO");
 

tests/vmci-lines-post.c
@@ -80,6 +80,30 @@ static const char vmcore[] =
 	"";
 
 static int
+set_vmcoreinfo_value(kdump_ctx_t *ctx, const char *val)
+{
+	kdump_blob_t *blob;
+	kdump_attr_t attr;
+	kdump_status status;
+
+	blob = kdump_blob_new_dup(val, strlen(val));
+	if (!blob) {
+		fprintf(stderr, "#%d: Cannot allocate VMCOREINFO blob.\n");
+		return TEST_ERR;
+	}
+	attr.type = KDUMP_BLOB;
+	attr.val.blob = blob;
+	status = kdump_set_attr(ctx, "linux.vmcoreinfo.raw", &attr);
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "Cannot set vmcoreinfo: %s\n",
+			kdump_get_err(ctx));
+		return TEST_ERR;
+	}
+
+	return TEST_OK;
+}
+
+static int
 check_string(kdump_ctx_t *ctx, const char *attrpath, const char *expect)
 {
 	kdump_attr_t attr;
@@ -151,16 +175,10 @@ check(kdump_ctx_t *ctx)
 		return TEST_ERR;
 	}
 
-	attr.type = KDUMP_STRING;
-	attr.val.string = vmcore;
-	status = kdump_set_attr(ctx, "linux.vmcoreinfo.raw", &attr);
-	if (status != KDUMP_OK) {
-		fprintf(stderr, "Cannot set vmcoreinfo: %s\n",
-			kdump_get_err(ctx));
-		return TEST_ERR;
-	}
+	rc = set_vmcoreinfo_value(ctx, vmcore);
+	if (rc != TEST_OK)
+		return rc;
 
-	rc = TEST_OK;
 	attr.type = KDUMP_STRING;
 
 	/* Check modifications */

tests/vmci-post.c
@@ -73,6 +73,30 @@ static const char vmcore[] =
 	"";
 
 static int
+set_vmcoreinfo_value(kdump_ctx_t *ctx, const char *val)
+{
+	kdump_blob_t *blob;
+	kdump_attr_t attr;
+	kdump_status status;
+
+	blob = kdump_blob_new_dup(val, strlen(val));
+	if (!blob) {
+		fprintf(stderr, "#%d: Cannot allocate VMCOREINFO blob.\n");
+		return TEST_ERR;
+	}
+	attr.type = KDUMP_BLOB;
+	attr.val.blob = blob;
+	status = kdump_set_attr(ctx, "linux.vmcoreinfo.raw", &attr);
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "Cannot set vmcoreinfo: %s\n",
+			kdump_get_err(ctx));
+		return TEST_ERR;
+	}
+
+	return TEST_OK;
+}
+
+static int
 check_string(kdump_ctx_t *ctx, const char *attrpath, const char *expect)
 {
 	kdump_attr_t attr;
@@ -144,14 +168,9 @@ check(kdump_ctx_t *ctx)
 		return TEST_ERR;
 	}
 
-	attr.type = KDUMP_STRING;
-	attr.val.string = vmcore;
-	status = kdump_set_attr(ctx, "linux.vmcoreinfo.raw", &attr);
-	if (status != KDUMP_OK) {
-		fprintf(stderr, "Cannot set vmcoreinfo: %s\n",
-			kdump_get_err(ctx));
-		return TEST_ERR;
-	}
+	rc = set_vmcoreinfo_value(ctx, vmcore);
+	if (rc != TEST_OK)
+		return rc;
 
 	rc = TEST_OK;
 

commit dd503c975ab392f1b657f6f6788d9b40b4f6a656

Fix kdump_vmcoreinfo_raw()
The conversion of raw vmcoreinfo attributes to a blob type
requires change of kdump_vmcoreinfo_raw(), as reported by
@sdimitro in issue #32.

This function has always been flawed, because the lifetime of the
return value is underspecified. For example, the pointer is
invalidated by any change to the vmcoreinfo attributes. The call
is inherently racy in a multi-threaded program; in an extreme
case, the pointer may become invalid before it is even returned to
the caller.

The above issue can be fixed by returning a dynamically allocated
copy of the value. However, such a fix also requires changing the
callers to avoid memory leaks. Luckily, the type of the parameter
must change, so compilers can issue a warning. so the breakage is
not completely silent.

Fixes: b4b3c94
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

include/libkdumpfile/kdumpfile.h.in
@@ -1001,13 +1001,16 @@ void kdump_attr_iter_end(kdump_ctx_t *ctx, kdump_attr_iter_t *iter);
 
 /**  Get VMCOREINFO raw data.
  * @param ctx  Dump file object.
- * @param raw  Filled with raw VMCOREINFO string on success.
+ * @param raw  Filled with a copy of the raw VMCOREINFO string on success.
  * @returns    Error status.
  *
  * The output string is always NUL-terminated, but if there was a NUL
  * byte inside VMCOREINFO, there is no way to know the full length.
+ *
+ * The output string is dynamically allocated and must be eventually
+ * deallocated by the caller using @c free(3).
  */
-kdump_status kdump_vmcoreinfo_raw(kdump_ctx_t *ctx, const char **raw);
+kdump_status kdump_vmcoreinfo_raw(kdump_ctx_t *ctx, char **raw);
 
 /**  Get a VMCOREINFO line.
  * @param ctx       Dump file object.

src/kdumpfile/vmcoreinfo.c
@@ -260,7 +260,7 @@ const struct attr_ops vmcoreinfo_raw_ops = {
 };
 
 static kdump_status
-get_raw_locked(kdump_ctx_t *ctx, const char **raw)
+get_raw_locked(kdump_ctx_t *ctx, char **raw)
 {
 	static const struct ostype_attr_map raw_map[] = {
 		{ ADDRXLAT_OS_LINUX, GKI_linux_vmcoreinfo_raw },
@@ -268,6 +268,7 @@ get_raw_locked(kdump_ctx_t *ctx, const char **raw)
 		{ ADDRXLAT_OS_UNKNOWN }
 	};
 	struct attr_data *attr = ostype_attr(ctx, raw_map);
+	size_t len;
 	kdump_status status;
 
 	if (!attr)
@@ -282,12 +283,19 @@ get_raw_locked(kdump_ctx_t *ctx, const char **raw)
 		return set_error(ctx, status,
 				 "Value cannot be revalidated");
 
-	*raw = attr_value(attr)->string;
+	len = attr_value(attr)->blob->size;
+	*raw = malloc(len + 1);
+	if (!*raw)
+		return set_error(ctx, KDUMP_ERR_SYSTEM,
+				 "Cannot allocate raw attribute value");
+	memcpy(*raw, attr_value(attr)->blob->data, len);
+	(*raw)[len] = 0;
+
 	return KDUMP_OK;
 }
 
 kdump_status
-kdump_vmcoreinfo_raw(kdump_ctx_t *ctx, const char **raw)
+kdump_vmcoreinfo_raw(kdump_ctx_t *ctx, char **raw)
 {
 	kdump_status ret;
 

tests/vmci-cleanup.c
@@ -27,6 +27,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <libkdumpfile/kdumpfile.h>
 
@@ -64,6 +65,29 @@ set_vmcoreinfo_value(kdump_ctx_t *ctx, int cnt, const char *val)
 }
 
 static int
+check_vmcoreinfo_raw(kdump_ctx_t *ctx, int cnt, const char *expect)
+{
+	char *rawval;
+	kdump_status status;
+
+	status = kdump_vmcoreinfo_raw(ctx, &rawval);
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "#%d: kdump_vmcoreinfo_raw() failed: %s\n",
+			cnt, kdump_get_err(ctx));
+		return TEST_ERR;
+	}
+	if (strcmp(rawval, expect)) {
+		fprintf(stderr, "#%d: Invalid raw value:\n%s\n",
+			cnt, rawval);
+		return TEST_FAIL;
+	}
+	printf("#%d: kdump_vmcoreinfo_raw() value match\n", cnt);
+	free(rawval);
+
+	return TEST_OK;
+}
+
+static int
 check(kdump_ctx_t *ctx)
 {
 	unsigned cnt;
@@ -71,9 +95,20 @@ check(kdump_ctx_t *ctx)
 	kdump_status status;
 	int rc;
 
+	status = kdump_set_string_attr(ctx, "addrxlat.ostype", "linux");
+	if (status != KDUMP_OK) {
+		fprintf(stderr, "Cannot set ostype: %s\n",
+			kdump_get_err(ctx));
+		return TEST_ERR;
+	}
+
 	/* First value. */
 	cnt = 1;
 	rc = set_vmcoreinfo_value(ctx, cnt, vmcore1);
+	if (rc != TEST_OK)
+		return rc;
+
+	rc = check_vmcoreinfo_raw(ctx, cnt, vmcore1);
 	if (rc != TEST_OK)
 		return rc;
 
@@ -94,6 +129,10 @@ check(kdump_ctx_t *ctx)
 	/* (Conflicting) second value */
 	cnt = 2;
 	rc = set_vmcoreinfo_value(ctx, cnt, vmcore2);
+	if (rc != TEST_OK)
+		return rc;
+
+	rc = check_vmcoreinfo_raw(ctx, cnt, vmcore2);
 	if (rc != TEST_OK)
 		return rc;
 