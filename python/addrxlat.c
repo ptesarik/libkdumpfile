ptesarik/libkdumpfile

commit ca27d79383129014522ae3278bba269b1fd99ae7

python/addrxlat: Perform number conversion in Number_As*()
If the argument is neither a long nor int, try the number protocol.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/addrxlat.c
@@ -106,6 +106,8 @@ PyDoc_STRVAR(attr_convert__doc__,
 static long
 Number_AsLong(PyObject *num)
 {
+	unsigned long long result;
+
 	if (PyLong_Check(num))
 		return PyLong_AsLong(num);
 #if PY_MAJOR_VERSION < 3
@@ -113,9 +115,12 @@ Number_AsLong(PyObject *num)
 		return PyInt_AsLong(num);
 #endif
 
-	PyErr_Format(PyExc_TypeError, "'%.200s' object is not an integer",
-		     Py_TYPE(num)->tp_name);
-	return -1;
+	num = PyNumber_Long(num);
+	if (!num)
+		return -1L;
+	result = PyLong_AsLong(num);
+	Py_DECREF(num);
+	return result;
 }
 
 /** Convert a PyLong or PyInt to a C unsigned long long.
@@ -128,6 +133,8 @@ Number_AsLong(PyObject *num)
 static unsigned long long
 Number_AsUnsignedLongLong(PyObject *num)
 {
+	unsigned long long result;
+
 	if (PyLong_Check(num))
 		return PyLong_AsUnsignedLongLong(num);
 #if PY_MAJOR_VERSION < 3
@@ -135,9 +142,12 @@ Number_AsUnsignedLongLong(PyObject *num)
 		return PyInt_AsLong(num);
 #endif
 
-	PyErr_Format(PyExc_TypeError, "'%.200s' object is not an integer",
-		     Py_TYPE(num)->tp_name);
-	return -1LL;
+	num = PyNumber_Long(num);
+	if (!num)
+		return -1LL;
+	result = PyLong_AsUnsignedLongLong(num);
+	Py_DECREF(num);
+	return result;
 }
 
 /** Convert a PyLong or PyInt to a C unsigned long long with no overflow.
@@ -150,6 +160,8 @@ Number_AsUnsignedLongLong(PyObject *num)
 static unsigned long long
 Number_AsUnsignedLongLongMask(PyObject *num)
 {
+	unsigned long long result;
+
 	if (PyLong_Check(num))
 		return PyLong_AsUnsignedLongLongMask(num);
 #if PY_MAJOR_VERSION < 3
@@ -157,9 +169,12 @@ Number_AsUnsignedLongLongMask(PyObject *num)
 		return PyInt_AsLong(num);
 #endif
 
-	PyErr_Format(PyExc_TypeError, "'%.200s' object is not an integer",
-		     Py_TYPE(num)->tp_name);
-	return -1LL;
+	num = PyNumber_Long(num);
+	if (!num)
+		return -1LL;
+	result = PyLong_AsUnsignedLongLongMask(num);
+	Py_DECREF(num);
+	return result;
 }
 
 /** Convert a Python sequence of integers to a memory buffer.

python/test_addrxlat.py
@@ -273,11 +273,11 @@ class TestMethod(unittest.TestCase):
             meth.tbl = ((1,),)
         with self.assertRaisesRegexp(ValueError, 'must be integer pairs'):
             meth.tbl = ((1, 2, 3),)
-        with self.assertRaisesRegexp(TypeError, 'not an integer'):
+        with self.assertRaisesRegexp(TypeError, 'must be.* a number'):
             meth.tbl = ((None, None),)
-        with self.assertRaisesRegexp(TypeError, 'not an integer'):
+        with self.assertRaisesRegexp(TypeError, 'must be.* a number'):
             meth.tbl = ((1, None),)
-        with self.assertRaisesRegexp(TypeError, 'not an integer'):
+        with self.assertRaisesRegexp(TypeError, 'must be.* a number'):
             meth.tbl = ((None, 1),)
 
     def test_memarr_defaults(self):
@@ -575,7 +575,7 @@ class TestStep(unittest.TestCase):
             step.idx = None
         with self.assertRaisesRegexp(ValueError, 'more than [0-9]+ indices'):
             step.idx = (0,) * (addrxlat.FIELDS_MAX + 2)
-        with self.assertRaisesRegexp(TypeError, 'not an integer'):
+        with self.assertRaisesRegexp(TypeError, 'must be.* a number'):
             step.idx = (None,)
 
 class TestOperator(unittest.TestCase):

commit 73432788f35e536a483faf889b37076d9a2ca4ac

python: Fix return temporary variable in Number_AsLong()
This is cosmetic, since the value is cast to a long by the return
statement, but one may wonder what is the trick with using an
unsigned long long as a temporary... Well, there is none, it was a
mistake from copy&paste.

Fixes: ca27d79383129014522ae3278bba269b1fd99ae7
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

python/addrxlat.c
@@ -105,7 +105,7 @@ PyDoc_STRVAR(attr_convert__doc__,
 static long
 Number_AsLong(PyObject *num)
 {
-	unsigned long long result;
+	long result;
 
 	if (PyLong_Check(num))
 		return PyLong_AsLong(num);