ptesarik/libkdumpfile

commit 768b5cac483cd7037dcb475569b85b37a492da5f

Fix `make check` when configured with `--with-pic`
The basic idea of having internal names was to see the source code
names in a debugger. Unfortunately, libtool is brain-dead, so there
is no way to distinguish whether a symbol can be hidden from other
users.

This is a modification of the change requested in PR 24. Sanitized
names will be used by default for shared libraries and static
libraries alike, but unmodified names can be still turned on by
running `configure` with `--enable-debug`.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/internal.h
@@ -47,7 +47,7 @@
 #define PUB_NAME(sym)		XTAG_PUB_NAME(LIBNAME, sym)
 
 /* Minimize chance of name clashes (in a static link) */
-#ifndef PIC
+#ifndef ENABLE_DEBUG
 #define INTERNAL_DECL(type, sym, param)	\
 	type sym param			\
 	__asm__(XSTRINGIFY(ASM_NAME(PRIV_NAME(sym))))
@@ -56,7 +56,7 @@
 	type sym param
 #endif
 
-#ifndef PIC
+#ifndef ENABLE_DEBUG
 #define INTERNAL_ALIAS(x)		PUB_NAME(x)
 #define _DECLARE_ALIAS(s, a)		\
 	extern typeof(s) (a) __asm__(XSTRINGIFY(ASM_NAME(s)))

configure.ac
@@ -88,6 +88,14 @@ AS_IF([test "x$have_pthread" = xyes],
   ])
 AC_SUBST(PTHREAD_LIBS)
 
+dnl check for useful debugging options
+AC_ARG_ENABLE(debug,
+  [AS_HELP_STRING([--enable-debug],
+    [Make a build suitable for debugging @<:@default=no@:>@])],
+  [], [enable_debug=no])
+AS_IF([test "x$enable_debug" = xyes],
+    [AC_DEFINE(ENABLE_DEBUG, 1, Define to enable extra debugging features)])
+
 dnl check for Python
 kdump_PYTHON([2.7.0])
 

commit 356b05b16633fb5cb2d6c89778c34c9ccda8026e

Fix internal library aliases
Internal aliases are broken, creating additional PLT entries for
calls to exported functions (when linked dynamically), or not
sanitizing the internal names (when linked statically).

Let's redesign the implementation from scratch.

First, all intra-library calls should use the internal alias name
in source code, e.g. internal_walk(). The INTERNAL_ALIAS() macro is
unused and can be removed.

Second, regardless of name sanitization, intra-library calls should
avoid the PLT when linked dynamically. It is therefore not possible
to use the public names directly. That's only possible with static
linking, where the distinction is pointless, but since the intended
linker type is not known at compile time, it is alway necessary to
create aliased symbols for the linker.

Third, for a debugging build, the assembler symbol names should
correspond to source code, because these are the names seen by a
debugger.

To recap, given a symbol name SYM, and library tag TAG, these
symbol names are used:

declaration          source code   --enable-debug  --disable-debug
INTERNAL_DECL(SYM)   SYM           SYM             _TAG_priv_SYM
INTERNAL_ALIAS(SYM)  internal_SYM  internal_SYM    _TAG_priv_SYM

In the above, internal_SYM is an alias for TAG_SYM (e.g.
internal_walk is an alias for addrxlat_walk), but for dynamic
linking, it is resolved locally within the shared object itself.
Using the public name (addrxlat_walk) would create a PLT entry and
get resolved by the dynamic linker at run time.

Fixes: 768b5cac483cd7037dcb475569b85b37a492da5f
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/internal.h
@@ -57,23 +57,22 @@
 #endif
 
 #ifndef ENABLE_DEBUG
-#define INTERNAL_ALIAS(x)		PUB_NAME(x)
-#define _DECLARE_ALIAS(s, a)		\
-	extern typeof(s) (a) __asm__(XSTRINGIFY(ASM_NAME(s)))
-#define _DEFINE_ALIAS(s, a)		_DECLARE_ALIAS(s, a)
+#define _ALIAS_ASM(sym)			\
+	__asm__(XSTRINGIFY(ASM_NAME(PRIV_NAME(sym))))
 #else
-#define INTERNAL_ALIAS(x)		internal_ ## x
-#define _DECLARE_ALIAS(s, a)		\
-	extern typeof(s) (a)
-#define _DEFINE_ALIAS(s, a)		\
-	extern typeof(s) (a)		\
-	__attribute__((alias(STRINGIFY(s))))
+#define _ALIAS_ASM(sym)
 #endif
 
 /** Internal alias declaration. */
-#define DECLARE_ALIAS(x) _DECLARE_ALIAS(PUB_NAME(x), internal_ ## x)
+#define DECLARE_ALIAS(sym)		\
+	extern typeof(PUB_NAME(sym))	\
+	(internal_ ## sym)		\
+	_ALIAS_ASM(sym)
 
 /** Define an internal alias for a symbol. */
-#define DEFINE_ALIAS(x) _DEFINE_ALIAS(PUB_NAME(x), internal_ ## x)
+#define DEFINE_ALIAS(sym)		\
+	extern typeof(PUB_NAME(sym))	\
+	(internal_ ## sym)		\
+	__attribute__((alias(XSTRINGIFY(PUB_NAME(sym)))))
 
 #endif	/* internal.h */