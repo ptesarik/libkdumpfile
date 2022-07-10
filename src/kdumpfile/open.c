ptesarik/libkdumpfile

commit 772e573a5731054dd8471b9b6cbad8523cd67a7e

Optimize ostype_attr() slightly
The full lookup can be optimized, because addrxlat.ostype must be a
known OS type name. This is enforced in the pre-set hook. Both OS
types have their correspondig global directory attribute index,
which can be then stored in struct kdump_xlat.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/open.c
@@ -268,11 +268,13 @@ static kdump_status
 ostype_pre_hook(kdump_ctx_t *ctx, struct attr_data *attr,
 		kdump_attr_value_t *val)
 {
-	if (!(strcmp(val->string, "linux")))
+	if (!(strcmp(val->string, "linux"))) {
 		ctx->xlat->ostype = ADDRXLAT_OS_LINUX;
-	else if (!strcmp(val->string, "xen"))
+		ctx->xlat->osdir = GKI_dir_linux;
+	} else if (!strcmp(val->string, "xen")) {
 		ctx->xlat->ostype = ADDRXLAT_OS_XEN;
-	else
+		ctx->xlat->osdir = GKI_dir_linux;
+	} else
 		return set_error(ctx, KDUMP_ERR_NOTIMPL,
 				 "Unsupported OS type");
 
@@ -315,6 +317,7 @@ static void
 ostype_clear_hook(kdump_ctx_t *ctx, struct attr_data *attr)
 {
 	ctx->xlat->ostype = ADDRXLAT_OS_UNKNOWN;
+	ctx->xlat->osdir = NR_GLOBAL_ATTRS;
 	ctx->xlat->dirty = true;
 }
 

src/kdumpfile/attr.c
@@ -1412,29 +1412,11 @@ ostype_attr(kdump_ctx_t *ctx, const char *name, struct attr_data **attr)
 	const char *ostype;
 	kdump_status status;
 
-	/* Get OS type name */
-	ostype = "addrxlat.ostype"; /* used in error messages */
-	d = gattr(ctx, GKI_ostype);
-	if (!attr_isset(d))
-		return set_error(ctx, KDUMP_ERR_NODATA,
-				 "%s is not set", ostype);
-	status = attr_revalidate(ctx, d);
-	if (status != KDUMP_OK)
-		return set_error(ctx, status, "Cannot get %s", ostype);
-	ostype = attr_value(d)->string;
-
 	/* Get OS directory attribute */
-	d = lookup_attr(ctx->dict, ostype);
-	if (!d || d->template->type != KDUMP_DIRECTORY)
-		return set_error(ctx, KDUMP_ERR_NOTIMPL,
-				 "Unknown OS type: %s", ostype);
-	if (!attr_isset(d))
+	if (ctx->xlat->osdir == NR_GLOBAL_ATTRS)
 		return set_error(ctx, KDUMP_ERR_NODATA,
-				 "%s OS attributes are not set", ostype);
-	status = attr_revalidate(ctx, d);
-	if (status != KDUMP_OK)
-		return set_error(ctx, status,
-				 "Cannot get %s OS attributes", ostype);
+				 "OS type is not set");
+	d = gattr(ctx, ctx->xlat->osdir);
 
 	/* Get attribute under the OS directory. */
 	d = lookup_dir_attr(ctx->dict, d, name, strlen(name));

src/kdumpfile/kdumpfile-priv.h
@@ -634,6 +634,11 @@ struct kdump_xlat {
 	struct list_head ctx;
 
 	addrxlat_ostype_t ostype; /**< OS for address translation. */
+	/** OS attribute base directory.
+	 * If OS type is not set, this field contains @xref NR_GLOBAL_ATTRS,
+	 * which is an invalid value.
+	 */
+	enum global_keyidx osdir;
 	addrxlat_sys_t *xlatsys;  /**< Address translation system. */
 	unsigned long xlat_caps;  /**< Address space capabilities. */
 };

src/kdumpfile/vtop.c
@@ -763,6 +763,8 @@ xlat_new(void)
 	xlat->refcnt = 1;
 	list_init(&xlat->ctx);
 
+	xlat->osdir = NR_GLOBAL_ATTRS;
+
 	xlat->xlatsys = addrxlat_sys_new();
 	if (!xlat->xlatsys)
 		goto err;

commit 128fe0dec9add2b4ea0b728ee7e6b0b61647bb1c

Set corrrect osdir for ostype=xen
This looks like a copy&paste mistake. The attribute root directory
should be GKI_dir_xen.

Among other things, examples/listxendoms has been broken.

Fixes: 772e573a5731054dd8471b9b6cbad8523cd67a7e
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/open.c
@@ -483,7 +483,7 @@ ostype_pre_hook(kdump_ctx_t *ctx, struct attr_data *attr,
 	if (!(strcmp(val->string, "linux"))) {
 		ctx->xlat->osdir = GKI_dir_linux;
 	} else if (!strcmp(val->string, "xen")) {
-		ctx->xlat->osdir = GKI_dir_linux;
+		ctx->xlat->osdir = GKI_dir_xen;
 	} else
 		return set_error(ctx, KDUMP_ERR_NOTIMPL,
 				 "Unsupported OS type");