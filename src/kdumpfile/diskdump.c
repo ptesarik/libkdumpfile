ptesarik/libkdumpfile

commit 8e906c3a5531904a4f9d7b78d7954acb4e474e9c

diskdump: Handle split dump files correctly
Ignore out-of-range bitmap entries for split dumps. This is
required to handle such files correctly, because the page bitmap
also includes non-zero bits for pages that were stored in other
split dump files.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/diskdump.c
@@ -168,6 +168,11 @@ struct disk_dump_priv {
 	struct pfn_rgn *pfn_rgn; /**< PFN region map. */
 	size_t pfn_rgn_num;	 /**< Number of elements in the map. */
 
+	/** PFN of the first page descriptor. */
+	kdump_pfn_t pd_start;
+	/** PFN of one beyond the page descriptor array. */
+	kdump_pfn_t pd_end;
+
 	/** Overridden methods for arch.page_size attribute. */
 	struct attr_override page_size_override;
 	int cbuf_slot;		/**< Compressed data per-context slot. */
@@ -630,6 +635,7 @@ static kdump_status
 read_bitmap(kdump_ctx_t *ctx, int32_t sub_hdr_size,
 	    int32_t bitmap_blocks)
 {
+	struct disk_dump_priv *ddp = ctx->shared->fmtdata;
 	off_t off = (1 + sub_hdr_size) * get_page_size(ctx);
 	off_t descoff;
 	size_t bitmapsize;
@@ -662,10 +668,14 @@ read_bitmap(kdump_ctx_t *ctx, int32_t sub_hdr_size,
 				 bitmapsize, (unsigned long long) off);
 
 	rgn.pos = descoff;
-	pfn = 0;
+	pfn = ddp->pd_start;
+	if (max_bitmap_pfn > ddp->pd_end)
+		max_bitmap_pfn = ddp->pd_end;
 	while (pfn < max_bitmap_pfn) {
 		rgn.pfn = skip_clear(fch.data, bitmapsize, pfn);
 		pfn = skip_set(fch.data, bitmapsize, rgn.pfn);
+		if (pfn > max_bitmap_pfn)
+			pfn = max_bitmap_pfn;
 		rgn.cnt = pfn - rgn.pfn;
 		if (rgn.cnt) {
 			ret = add_pfn_rgn(ctx, &rgn);
@@ -718,6 +728,7 @@ static kdump_status
 read_sub_hdr_32(struct setup_data *sdp, int32_t header_version)
 {
 	kdump_ctx_t *ctx = sdp->ctx;
+	struct disk_dump_priv *ddp = ctx->shared->fmtdata;
 	struct kdump_sub_header_32 subhdr;
 	kdump_status ret;
 
@@ -748,8 +759,19 @@ read_sub_hdr_32(struct setup_data *sdp, int32_t header_version)
 			return ret;
 	}
 
-	if (header_version >= 6)
+	ddp->pd_start = 0;
+	ddp->pd_end = KDUMP_PFN_MAX;
+	if (header_version >= 2 && subhdr.split) {
+		ddp->pd_start = subhdr.start_pfn;
+		ddp->pd_end = subhdr.end_pfn;
+	}
+	if (header_version >= 6) {
+		if (subhdr.split) {
+			ddp->pd_start = subhdr.start_pfn_64;
+			ddp->pd_end = subhdr.end_pfn_64;
+		}
 		set_max_pfn(ctx, dump64toh(ctx, subhdr.max_mapnr_64));
+	}
 
 	return KDUMP_OK;
 }
@@ -801,6 +823,7 @@ static kdump_status
 read_sub_hdr_64(struct setup_data *sdp, int32_t header_version)
 {
 	kdump_ctx_t *ctx = sdp->ctx;
+	struct disk_dump_priv *ddp = ctx->shared->fmtdata;
 	struct kdump_sub_header_64 subhdr;
 	kdump_status ret;
 
@@ -831,8 +854,19 @@ read_sub_hdr_64(struct setup_data *sdp, int32_t header_version)
 			return ret;
 	}
 
-	if (header_version >= 6)
+	ddp->pd_start = 0;
+	ddp->pd_end = KDUMP_PFN_MAX;
+	if (header_version >= 2 && subhdr.split) {
+		ddp->pd_start = subhdr.start_pfn;
+		ddp->pd_end = subhdr.end_pfn;
+	}
+	if (header_version >= 6) {
+		if (subhdr.split) {
+			ddp->pd_start = subhdr.start_pfn_64;
+			ddp->pd_end = subhdr.end_pfn_64;
+		}
 		set_max_pfn(ctx, dump64toh(ctx, subhdr.max_mapnr_64));
+	}
 
 	return KDUMP_OK;
 }

tests/Makefile.am
@@ -184,6 +184,7 @@ test_scripts = \
 	diskdump-basic-zstd \
 	diskdump-multiread \
 	diskdump-excluded \
+	diskdump-split \
 	early-version-code \
 	elf-empty-aarch64 \
 	elf-empty-i386 \
@@ -329,6 +330,10 @@ dist_check_DATA = \
 	multixlat-same.expect \
 	diskdump-excluded.data \
 	diskdump-excluded.expect \
+	diskdump-split.data \
+	diskdump-split.expect.1 \
+	diskdump-split.expect.2 \
+	diskdump-split.expect.3 \
 	sys-xlat-x86_64-linux.expect \
 	sys-xlat-x86_64-linux-xen.expect \
 	xlatmap.expect \

tests/diskdump-split
@@ -1,0 +1,128 @@
+#
+# Check reading data from a split dump file
+#
+
+mkdir -p out || exit 99
+
+name=$( basename "$0" )
+dumpfile="out/${name}.dump"
+resultfile="out/${name}.result"
+datafile="$srcdir/${name}.data"
+expectfile="$srcdir/${name}.expect"
+
+desc="
+version = 6
+arch_name = x86_64
+block_size = 4096
+phys_base = 0
+max_mapnr = 0x100
+sub_hdr_size = 1
+
+uts.sysname = Linux
+uts.nodename = test-node
+uts.release = 3.4.5-test
+uts.version = #1 SMP Fri Jan 22 14:02:42 UTC 2016 (1234567)
+uts.machine = x86_64
+uts.domainname = (none)
+
+nr_cpus = 1
+
+split = 1
+
+DATA = $datafile
+"
+
+# Create three split dump files
+
+./mkdiskdump "$dumpfile.1" <<EOF
+$desc
+start_pfn = 1
+end_pfn = 2
+DATA = $datafile
+EOF
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo "Cannot create diskdump file" >&2
+    exit $rc
+fi
+echo "Created split diskdump dump: $dumpfile.1"
+
+./mkdiskdump "$dumpfile.2" <<EOF
+$desc
+start_pfn = 2
+end_pfn = 3
+DATA = $datafile
+EOF
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo "Cannot create diskdump file" >&2
+    exit $rc
+fi
+echo "Created split diskdump dump: $dumpfile.2"
+
+./mkdiskdump "$dumpfile.3" <<EOF
+$desc
+start_pfn = 3
+end_pfn = 5
+DATA = $datafile
+EOF
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo "Cannot create diskdump file" >&2
+    exit $rc
+fi
+echo "Created split diskdump dump: $dumpfile.3"
+
+echo "Check that file 1 only contains PFN 1"
+./dumpdata "$dumpfile.1" 0x1000 8 > "$resultfile.1"
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo "Cannot dump DISKDUMP data" >&2
+    exit $rc
+fi
+if ! diff "$expectfile.1" "$resultfile.1"; then
+    echo "Results do not match" >&2
+    exit 1
+fi
+./dumpdata "$dumpfile.1" 0x2000 8 > "$resultfile.1.err" 2>/dev/null
+rc=$?
+if [ $rc -eq 0 ]; then
+    echo "Unexpected success!" >&2
+    exit 1
+fi
+
+echo "Check that file 2 only contains PFN 2"
+./dumpdata "$dumpfile.2" 0x2000 8 > "$resultfile.2"
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo "Cannot dump DISKDUMP data" >&2
+    exit $rc
+fi
+if ! diff "$expectfile.2" "$resultfile.2"; then
+    echo "Results do not match" >&2
+    exit 1
+fi
+./dumpdata "$dumpfile.2" 0x1000 8 > "$resultfile.2.err" 2>/dev/null
+rc=$?
+if [ $rc -eq 0 ]; then
+    echo "Unexpected success!" >&2
+    exit 1
+fi
+
+echo "Check that file 3 contains PFN 3 and PFN 4"
+./dumpdata "$dumpfile.3" 0x3ffc 8 > "$resultfile.3"
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo "Cannot dump DISKDUMP data" >&2
+    exit $rc
+fi
+if ! diff "$expectfile.3" "$resultfile.3"; then
+    echo "Results do not match" >&2
+    exit 1
+fi
+./dumpdata "$dumpfile.3" 0x2000 8 > "$resultfile.3.err" 2>/dev/null
+rc=$?
+if [ $rc -eq 0 ]; then
+    echo "Unexpected success!" >&2
+    exit 1
+fi

tests/diskdump-split.data
@@ -1,0 +1,10 @@
+@0x0000 raw
+ff*4096
+@0x1000 raw
+10*4096
+@0x2000 raw
+20*4096
+@0x3000 raw
+30*4096
+@0x4000 raw
+40*4096

tests/diskdump-split.expect.1
@@ -1,0 +1,1 @@
+10 10 10 10 10 10 10 10 

tests/diskdump-split.expect.2
@@ -1,0 +1,1 @@
+20 20 20 20 20 20 20 20 

tests/diskdump-split.expect.3
@@ -1,0 +1,2 @@
+30 30 30 30
+40 40 40 40 

commit 9184e5c9519ea10efc74d2c4c27a62bd4fbf50fa

Fix handling of kdump split files with non-native endianity
The start_pfn and end_pfn fields in the kdump file header must be
converted to host endianity.

References: #44
Fixes: 8e906c3a5531904a4f9d7b78d7954acb4e474e9c
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/diskdump.c
@@ -125,12 +125,8 @@ struct kdump_sub_header_64 {
 	uint64_t	phys_base;
 	int32_t		dump_level;	   /* header_version 1 and later */
 	int32_t		split;		   /* header_version 2 and later */
-	uint64_t	start_pfn;	   /* header_version 2 and later,
-					      OBSOLETE! 32bit only, full
-					      64bit in start_pfn_64. */
-	uint64_t	end_pfn;	   /* header_version 2 and later,
-					      OBSOLETE! 32bit only, full
-					      64bit in end_pfn_64. */
+	uint64_t	start_pfn;	   /* header_version 2 and later */
+	uint64_t	end_pfn;	   /* header_version 2 and later */
 	uint64_t	offset_vmcoreinfo; /* header_version 3 and later */
 	uint64_t	size_vmcoreinfo;   /* header_version 3 and later */
 	uint64_t	offset_note;	   /* header_version 4 and later */
@@ -840,13 +836,13 @@ read_sub_hdr_32(struct setup_data *sdp, struct page_desc_map *pdmap,
 	pdmap->pd_start = 0;
 	pdmap->pd_end = KDUMP_PFN_MAX;
 	if (header_version >= 2 && subhdr.split) {
-		pdmap->pd_start = subhdr.start_pfn;
-		pdmap->pd_end = subhdr.end_pfn;
+		pdmap->pd_start = dump32toh(ctx, subhdr.start_pfn);
+		pdmap->pd_end = dump32toh(ctx, subhdr.end_pfn);
 	}
 	if (header_version >= 6) {
 		if (subhdr.split) {
-			pdmap->pd_start = subhdr.start_pfn_64;
-			pdmap->pd_end = subhdr.end_pfn_64;
+			pdmap->pd_start = dump64toh(ctx, subhdr.start_pfn_64);
+			pdmap->pd_end = dump64toh(ctx, subhdr.end_pfn_64);
 		}
 		set_max_pfn(ctx, dump64toh(ctx, subhdr.max_mapnr_64));
 	}
@@ -961,13 +957,13 @@ read_sub_hdr_64(struct setup_data *sdp, struct page_desc_map *pdmap,
 	pdmap->pd_start = 0;
 	pdmap->pd_end = KDUMP_PFN_MAX;
 	if (header_version >= 2 && subhdr.split) {
-		pdmap->pd_start = subhdr.start_pfn;
-		pdmap->pd_end = subhdr.end_pfn;
+		pdmap->pd_start = dump64toh(ctx, subhdr.start_pfn);
+		pdmap->pd_end = dump64toh(ctx, subhdr.end_pfn);
 	}
 	if (header_version >= 6) {
 		if (subhdr.split) {
-			pdmap->pd_start = subhdr.start_pfn_64;
-			pdmap->pd_end = subhdr.end_pfn_64;
+			pdmap->pd_start = dump64toh(ctx, subhdr.start_pfn_64);
+			pdmap->pd_end = dump64toh(ctx, subhdr.end_pfn_64);
 		}
 		set_max_pfn(ctx, dump64toh(ctx, subhdr.max_mapnr_64));
 	}