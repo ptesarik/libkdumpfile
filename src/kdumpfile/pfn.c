ptesarik/libkdumpfile

commit 34a413e89624e38d5b40a99666c543173ecb532c

Extract pfn_regions_from_bitmap() from diskdump
Conversion from a PFN bitmap to a PFN-to-file mapping is also
reusable code. Move it to pfn.c.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/pfn.c
@@ -91,6 +91,104 @@ find_pfn_region(const struct pfn_file_map *map, kdump_pfn_t pfn)
 		: NULL;
 }
 
+/** Skip clear bits in a PFN bitmap.
+ * @param bitmap  PFN bitmap.
+ * @param size    Size of the bitmap in bytes.
+ * @param pfn     Starting PFN.
+ * @returns       Next PFN that is not clear.
+ */
+static kdump_pfn_t
+skip_clear(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
+{
+	const unsigned char *bp = bitmap + (pfn >> 3);
+	int lead;
+
+	if (bp >= bitmap + size)
+		return pfn;
+
+	lead = ffs(*bp >> (pfn & 7));
+	if (lead)
+		return pfn + lead - 1;
+
+	pfn = (pfn | 7) + 1;
+	++bp;
+	while (bp < bitmap + size && *bp == 0x00)
+		pfn += 8, ++bp;
+	if (bp < bitmap + size)
+		pfn += ffs(*bp) - 1;
+
+	return pfn;
+}
+
+/** Skip set bits in a PFN bitmap.
+ * @param bitmap  PFN bitmap.
+ * @param size    Size of the bitmap in bytes.
+ * @param pfn     Starting PFN.
+ * @returns       Next PFN that is not set.
+ */
+static kdump_pfn_t
+skip_set(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
+{
+	const unsigned char *bp = bitmap + (pfn >> 3);
+	int lead;
+
+	if (bp >= bitmap + size)
+		return pfn;
+
+	lead = ffs(~((signed char)*bp >> (pfn & 7)));
+	if (lead)
+		return pfn + lead - 1;
+
+	pfn = (pfn | 7) + 1;
+	++bp;
+	while (bp < bitmap + size && *bp == 0xff)
+		pfn += 8, ++bp;
+	if (bp < bitmap + size)
+		pfn += ffs(~(signed char)*bp) - 1;
+
+	return pfn;
+}
+
+/** Create PFN regions from a PFN bitmap.
+ * @param err        Error context.
+ * @param pfm        Target PFN-to-file mapping.
+ * @param bitmap     Source PFN bitmap.
+ * @param start_pfn  Lowest PFN to process.
+ * @param end_pfn    One above the highest PFN to process.
+ * @param fileoff    First target file offset.
+ * @param elemsz     Size of the mapped file object.
+ * @returns          Error status.
+ */
+kdump_status
+pfn_regions_from_bitmap(kdump_errmsg_t *err, struct pfn_file_map *pfm,
+			const unsigned char *bitmap, kdump_pfn_t start_pfn,
+			kdump_pfn_t end_pfn, off_t fileoff, off_t elemsz)
+{
+	size_t bitmapsize = end_pfn << 3;
+	kdump_pfn_t pfn = start_pfn;
+	struct pfn_region rgn;
+
+	rgn.pos = fileoff;
+	while (pfn < end_pfn) {
+		rgn.pfn = skip_clear(bitmap, bitmapsize, pfn);
+		pfn = skip_set(bitmap, bitmapsize, rgn.pfn);
+		if (pfn > end_pfn)
+			pfn = end_pfn;
+		rgn.cnt = pfn - rgn.pfn;
+		if (!rgn.cnt)
+			continue;
+
+		if (!add_pfn_region(pfm, &rgn))
+			return status_err(err, KDUMP_ERR_SYSTEM,
+					  "Cannot allocate more than"
+					  " %zu PFN region mappings",
+					  pfm->nregions);
+		rgn.pos += rgn.cnt * elemsz;
+	}
+
+	return KDUMP_OK;
+}
+
 /** Find the next mapped PFN.
  * @param maps   Array of PFN-to-file maps.
  * @param nmaps  Number of elements in @p maps.

src/kdumpfile/diskdump.c
@@ -479,52 +479,6 @@ read_notes(kdump_ctx_t *ctx, off_t off, size_t size)
 	return ret;
 }
 
-static kdump_pfn_t
-skip_clear(unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
-{
-	unsigned char *bp = bitmap + (pfn >> 3);
-	int lead;
-
-	if (bp >= bitmap + size)
-		return pfn;
-
-	lead = ffs(*bp >> (pfn & 7));
-	if (lead)
-		return pfn + lead - 1;
-
-	pfn = (pfn | 7) + 1;
-	++bp;
-	while (bp < bitmap + size && *bp == 0x00)
-		pfn += 8, ++bp;
-	if (bp < bitmap + size)
-		pfn += ffs(*bp) - 1;
-
-	return pfn;
-}
-
-static kdump_pfn_t
-skip_set(unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
-{
-	unsigned char *bp = bitmap + (pfn >> 3);
-	int lead;
-
-	if (bp >= bitmap + size)
-		return pfn;
-
-	lead = ffs(~((signed char)*bp >> (pfn & 7)));
-	if (lead)
-		return pfn + lead - 1;
-
-	pfn = (pfn | 7) + 1;
-	++bp;
-	while (bp < bitmap + size && *bp == 0xff)
-		pfn += 8, ++bp;
-	if (bp < bitmap + size)
-		pfn += ffs(~(signed char)*bp) - 1;
-
-	return pfn;
-}
-
 /** Read the page bitmap and translate it to PFN regions.
  * @param ctx            Dump file object.
  * @param pdmap          Target page descriptor map.
@@ -540,8 +494,6 @@ read_bitmap(kdump_ctx_t *ctx, struct pfn_file_map *pdmap,
 	off_t descoff;
 	size_t bitmapsize;
 	kdump_pfn_t max_bitmap_pfn;
-	kdump_pfn_t pfn;
-	struct pfn_region rgn;
 	struct fcache_chunk fch;
 	kdump_status ret;
 
@@ -568,32 +520,12 @@ read_bitmap(kdump_ctx_t *ctx, struct pfn_file_map *pdmap,
 				 " at %llu",
 				 bitmapsize, (unsigned long long) off);
 
-	rgn.pos = descoff;
-	pfn = pdmap->start_pfn;
 	if (max_bitmap_pfn > pdmap->end_pfn)
 		max_bitmap_pfn = pdmap->end_pfn;
-	while (pfn < max_bitmap_pfn) {
-		rgn.pfn = skip_clear(fch.data, bitmapsize, pfn);
-		pfn = skip_set(fch.data, bitmapsize, rgn.pfn);
-		if (pfn > max_bitmap_pfn)
-			pfn = max_bitmap_pfn;
-		rgn.cnt = pfn - rgn.pfn;
-		if (!rgn.cnt)
-			continue;
+	ret = pfn_regions_from_bitmap(&ctx->err, pdmap, fch.data,
+				      pdmap->start_pfn, max_bitmap_pfn,
+				      descoff, sizeof(struct page_desc));
 
-		if (!add_pfn_region(pdmap, &rgn)) {
-			ret = set_error(ctx, KDUMP_ERR_SYSTEM,
-					"Cannot allocate more than"
-					" %zu PFN region mappings",
-					pdmap->nregions);
-			goto out;
-		}
-		rgn.pos += rgn.cnt * sizeof(struct page_desc);
-	}
-
-	ret = KDUMP_OK;
-
- out:
 	fcache_put_chunk(&fch);
 	return ret;
 }

src/kdumpfile/kdumpfile-priv.h
@@ -1585,6 +1585,10 @@ INTERNAL_DECL(struct pfn_region *, add_pfn_region,
 	      (struct pfn_file_map *map, const struct pfn_region *rgn));
 INTERNAL_DECL(const struct pfn_region *, find_pfn_region,
 	      (const struct pfn_file_map *map, kdump_pfn_t pfn));
+INTERNAL_DECL(kdump_status, pfn_regions_from_bitmap,
+	      (kdump_errmsg_t *err, struct pfn_file_map *pfm,
+	       const unsigned char *bitmap, kdump_pfn_t start_pfn,
+	       kdump_pfn_t end_pfn, off_t fileoff, off_t elemsz));
 
 INTERNAL_DECL(bool, find_mapped_pfn,
 	      (const struct pfn_file_map *maps, size_t nmaps,

commit 9f0f35ac8eac9f4e354e72ac3a8bafba343265d2

pfn_regions_from_bitmap: Fix bitmap size calculation
The PFN must be shifted right, not left. And the result must be
rounded up to cover the last byte if it is partial.

Fixes: 34a413e89624e38d5b40a99666c543173ecb532c
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/pfn.c
@@ -261,7 +261,7 @@ pfn_regions_from_bitmap(kdump_errmsg_t *err, struct pfn_file_map *pfm,
 			kdump_pfn_t start_pfn, kdump_pfn_t end_pfn,
 			off_t fileoff, off_t elemsz)
 {
-	size_t bitmapsize = end_pfn << 3;
+	size_t bitmapsize = (end_pfn + 7) >> 3;
 	kdump_pfn_t pfn = start_pfn;
 	struct pfn_region rgn;
 