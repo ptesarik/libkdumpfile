ptesarik/libkdumpfile

commit d68e84464410469b228df58e9650be5cfe64fee4

skip_clear, skip_set: Loop over 32-bit integers
Instead of looping over individual bytes, loop over 32-bit
integers. If there are unaligned bytes at the beginning and/or end
of the bitmap, these are still scanned as bytes.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/pfn.c
@@ -101,9 +101,10 @@ static kdump_pfn_t
 skip_clear(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
 {
 	const unsigned char *bp = bitmap + (pfn >> 3);
+	const unsigned char *endp = bitmap + size;
 	unsigned char val;
 
-	if (bp >= bitmap + size)
+	if (bp >= endp)
 		return pfn;
 
 	val = *bp >> (pfn & 7);
@@ -112,10 +113,15 @@ skip_clear(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
 
 	pfn = (pfn | 7) + 1;
 	++bp;
-	while (bp < bitmap + size && *bp == 0x00)
-		pfn += 8, ++bp;
-	if (bp < bitmap + size)
-		pfn += ctz(*bp);
+	for (; endp - bp >= 1 && ((uintptr_t)bp & 3) != 0; pfn += 8, ++bp)
+		if (*bp)
+			return pfn + ctz(*bp);
+	for (; endp - bp >= 4; pfn += 32, bp += 4)
+		if (*(uint32_t*)bp)
+			return pfn + ctz(*(uint32_t*)bp);
+	for (; endp - bp >= 1; pfn += 8, ++bp)
+		if (*bp)
+			return pfn + ctz(*bp);
 
 	return pfn;
 }
@@ -130,9 +136,10 @@ static kdump_pfn_t
 skip_set(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
 {
 	const unsigned char *bp = bitmap + (pfn >> 3);
+	const unsigned char *endp = bitmap + size;
 	unsigned char val;
 
-	if (bp >= bitmap + size)
+	if (bp >= endp)
 		return pfn;
 
 	val = ~((signed char)*bp >> (pfn & 7));
@@ -141,10 +148,15 @@ skip_set(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
 
 	pfn = (pfn | 7) + 1;
 	++bp;
-	while (bp < bitmap + size && *bp == 0xff)
-		pfn += 8, ++bp;
-	if (bp < bitmap + size)
-		pfn += ctz(~*bp);
+	for (; endp - bp >= 1 && ((uintptr_t)bp & 3) != 0; pfn += 8, ++bp)
+		if (~*bp)
+			return pfn + ctz(~*bp);
+	for (; endp - bp >= 4; pfn += 32, bp += 4)
+		if (~*(uint32_t*)bp)
+			return pfn + ctz(~*(uint32_t*)bp);
+	for (; endp - bp >= 1; pfn += 8, ++bp)
+		if (~*bp)
+			return pfn + ctz(~*bp);
 
 	return pfn;
 }

commit 211dc88e1de1ebfd0af2439265a0d0defcf59cad

Fix search for a clear bit in a PFN bitmap
The bitwise NOT operator (~) extends the operand to an int, so the
high bits are always set after applying it to an unsigned char.
As a result, the scan never skips the first byte if the pointer is
unaligned.

The resulting region array is not incorrect, but much larger than
necessary.

Fixes: d68e84464410469b228df58e9650be5cfe64fee4
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/pfn.c
@@ -193,13 +193,13 @@ skip_set_lsb0(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
 	pfn = (pfn | 7) + 1;
 	++bp;
 	for (; endp - bp >= 1 && ((uintptr_t)bp & 3) != 0; pfn += 8, ++bp)
-		if (~*bp)
+		if (~*(signed char*)bp)
 			return pfn + ctz(~*bp);
 	for (; endp - bp >= 4; pfn += 32, bp += 4)
 		if (~*(uint32_t*)bp)
 			return pfn + ctz(~*(uint32_t*)bp);
 	for (; endp - bp >= 1; pfn += 8, ++bp)
-		if (~*bp)
+		if (~*(signed char*)bp)
 			return pfn + ctz(~*bp);
 
 	return pfn;
@@ -231,13 +231,13 @@ skip_set_msb0(const unsigned char *bitmap, size_t size, kdump_pfn_t pfn)
 	pfn = (pfn | 7) + 1;
 	++bp;
 	for (; endp - bp >= 1 && ((uintptr_t)bp & 3) != 0; pfn += 8, ++bp)
-		if (~*bp)
+		if (~*(signed char*)bp)
 			return pfn + clz(~((uint32_t)*bp << 24));
 	for (; endp - bp >= 4; pfn += 32, bp += 4)
 		if (~*(uint32_t*)bp)
 			return pfn + clz(~be32toh(*(uint32_t*)bp));
 	for (; endp - bp >= 1; pfn += 8, ++bp)
-		if (~*bp)
+		if (~*(signed char*)bp)
 			return pfn + clz(~((uint32_t)*bp << 24));
 
 	return pfn;