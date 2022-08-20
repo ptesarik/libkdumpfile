ptesarik/libkdumpfile

commit 894f367257203cb9dd68c0a54fe005b2cd0b8942

Replace kdump_shared refcounting with a list of refererring contexts
Tracking all referrers allows to make changes to foreign context data.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/list.h
@@ -1,0 +1,95 @@
+/** @internal @file list.h
+ * @brief API for doubly-linked lists.
+ */
+/* Copyright (C) 2016 Petr Tesarik <ptesarik@suse.com>
+
+   This file is free software; you can redistribute it and/or modify
+   it under the terms of either
+
+     * the GNU Lesser General Public License as published by the Free
+       Software Foundation; either version 3 of the License, or (at
+       your option) any later version
+
+   or
+
+     * the GNU General Public License as published by the Free
+       Software Foundation; either version 2 of the License, or (at
+       your option) any later version
+
+   or both in parallel, as here.
+
+   libkdumpfile is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received copies of the GNU General Public License and
+   the GNU Lesser General Public License along with this program.  If
+   not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LIST_H
+#define _LIST_H	1
+
+#include <stddef.h>
+
+/**  Get a pointer to the structure which contains the list.
+ * @param node   Pointer to a @c struct @ref list_head.
+ * @param type   Type of the containing structure.
+ * @param field	 Nam of the @c struct @ref list_head field inside @p type.
+ * @returns      Pointer to the containing structure.
+ */
+#define list_entry(node, type, field) \
+	(type*)((char*)(node) - offsetof(type, field))
+
+/**  A generic list head or node.
+ * This structure is modelled after the Linux kernel list API.
+ */
+struct list_head {
+	struct list_head *next;	/**< Pointer to the next node. */
+	struct list_head *prev;	/**< Pointer to the previous node. */
+};
+
+/**  Initialize an empty list.
+ * @param head  Uninitialized list head.
+ */
+static inline void
+list_init(struct list_head *head)
+{
+	head->next = head->prev = head;
+}
+
+/**  Check if a list is empty.
+ * @param head  List head to be checked.
+ */
+static inline int
+list_empty(struct list_head *head)
+{
+	return head->next == head;
+}
+
+/**  Add a node to the list.
+ * @param node  New node to be added.
+ * @param head  List head (or preceding node).
+ *
+ * The new node is added after @p head.
+ */
+static inline void
+list_add(struct list_head *node, struct list_head *head)
+{
+	node->next = head->prev;
+	node->prev = head;
+	head->prev = node->prev->next = node;
+}
+
+/**  Remove a node from the list.
+ * @param node  Node to be deleted.
+ */
+static inline void
+list_del(struct list_head *node)
+{
+	node->prev->next = node->next;
+	node->next->prev = node->prev;
+}
+
+#endif	/* list.h */

src/kdumpfile-priv.h
@@ -37,6 +37,8 @@
 #include "kdumpfile.h"
 #pragma GCC visibility pop
 
+#include "list.h"
+
 #include <endian.h>
 
 /* Minimize chance of name clashes (in a static link) */
@@ -542,7 +544,11 @@ struct cache;
  * which can be shared by many @ref kdump_ctx objects.
  */
 struct kdump_shared {
-	unsigned refcnt;	/**< Reference count. */
+	/** List of all refererring @c kdump_ctx structures.
+	 * Each @c kdump_ctx that holds a reference to this shared data
+	 * must be added to this list.
+	 */
+	struct list_head ctx;
 
 	int fd;			/**< Dump file descriptor. */
 
@@ -585,7 +591,10 @@ struct kdump_shared {
  * @ref kdump_shared.
  */
 struct _tag_kdump_ctx {
-	struct kdump_shared *shared;
+	struct kdump_shared *shared; /**< Dump file shared data. */
+
+	/** Node of the @c ctx list in @c struct @ref kdump_shared. */
+	struct list_head list;
 
 	void *priv;		/**< User private data. */
 

src/open.c
@@ -70,8 +70,10 @@ kdump_init_ctx(kdump_ctx *ctx)
 	if (!shared)
 		return set_error(ctx, kdump_syserr,
 				 "Cannot allocate shared info");
-	shared->refcnt = 1;
+	list_init(&shared->ctx);
+
 	ctx->shared = shared;
+	list_add(&ctx->list, &shared->ctx);
 
 	status = init_attrs(ctx);
 	if (status != kdump_ok) {
@@ -92,8 +94,9 @@ kdump_init_ctx(kdump_ctx *ctx)
 kdump_status
 kdump_init_clone(kdump_ctx *ctx, const kdump_ctx *orig)
 {
-	++orig->shared->refcnt;
 	ctx->shared = orig->shared;
+	list_add(&ctx->list, &orig->shared->ctx);
+
 	ctx->priv = orig->priv;
 	ctx->cb_get_symbol_val = orig->cb_get_symbol_val;
 	ctx->cb_get_symbol_val_xen = orig->cb_get_symbol_val_xen;
@@ -347,7 +350,9 @@ void
 kdump_free(kdump_ctx *ctx)
 {
 	struct kdump_shared *shared = ctx->shared;
-	if (! --shared->refcnt) {
+
+	list_del(&ctx->list);
+	if (list_empty(&shared->ctx)) {
 		if (shared->ops && shared->ops->cleanup)
 			shared->ops->cleanup(shared);
 		if (shared->arch_ops && shared->arch_ops->cleanup)

commit 97c42fd7ba8dcb2db02c6c20bdd78ff6fdcde88e

Add parentheses to the container_of() macro
Surrounding parentheses are missing, which breaks stuff like

  container_of(ptr, type, field)->something

Fixes: 894f367257203cb9dd68c0a54fe005b2cd0b8942
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/list.h
@@ -39,7 +39,7 @@
  * @param field  Name of the field within the containing struct.
  */
 #define container_of(ptr, type, field) \
-	(type *)((char*)(ptr) - offsetof(type, field))
+	((type *)((char*)(ptr) - offsetof(type, field)))
 
 /**  Get a pointer to the structure which contains the list.
  * @param node   Pointer to a @c struct @ref list_head.