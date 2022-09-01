ptesarik/libkdumpfile

commit dd1490fb68f9b8fca6dfc876aed95c64cc24a3b0

aarch64: Do not fail on FEAT_LPA and FEAT_LPA2 Block descriptors
When FEAT_LPA is implemented, block descriptors may appear in level
one tables with 64K paging.

When FEAT_LPA2 is implemented, block descriptors may appear in
level one tables with 16K paging, and even in level zero tables
with 4K paging.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/addrxlat/aarch64.c
@@ -50,9 +50,6 @@ enum pte_type {
 	PTE_TYPE_TABLE = 3,	/**< Table descriptor. */
 };
 
-/** 1G page mask. */
-#define PAGE_MASK_1G		ADDR_MASK(30)
-
 /** Descriptive names for page tables.
  * These names are used in error messages.
  */
@@ -131,11 +128,12 @@ pgt_aarch64(addrxlat_step_t *step)
 	step->base.as = step->meth->target_as;
 
 	if (PTE_TYPE(pte) == PTE_TYPE_BLOCK) {
-		addrxlat_addr_t mask = pf_table_mask(
-			&step->meth->param.pgt.pf, step->remain);
-		if (mask > PAGE_MASK_1G)
+		if (step->remain > 4 ||
+		    (step->remain > 3 &&
+		     step->meth->param.pgt.pf.fieldsz[0] != 9))
 			return pte_invalid(step);
-		step->base.addr &= ~mask;
+		step->base.addr &= ~pf_table_mask(
+			&step->meth->param.pgt.pf, step->remain);
 		return pgt_huge_page(step);
 	}
 

commit 8a00a4df0f6db494d9a557f507b52d49ab1736ba

AArch64: Fix handling of block descriptors
Fix the check for minimum table level where block descriptors are
allowed. They were incorrectly rejected in level zero tables with
a 4 KiB granule, because the page size is 12 bits, not 9 bits.

However, the check can be simplified as described in the comment
to MAX_REGION_MASK.

Note that 5-level paging has not been tested for AArch64, and the
test for invalid translations verifies the incorrect behaviour.
Another translation level must be added to fix it.

Fixes: dd1490fb68f9b8fca6dfc876aed95c64cc24a3b0
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/addrxlat/aarch64.c
@@ -41,6 +41,25 @@
 #define PA_MAX_BITS	48
 #define PA_MASK		ADDR_MASK(PA_MAX_BITS)
 
+/** Mask of the maximum region size.
+ * Largest region size depends on the translation granule size. This
+ * mask corresponds to the maximum value, i.e. a 4 TiB region in level
+ * one translation table for a 64 KiB granule.
+ *
+ * Note that this maximum can be used to check whether a block descriptor
+ * is valid in a given translation lookup level, because a region that
+ * would be defined by a (hypothetical) block descriptor in a translation
+ * table where block descriptors are not supported would be larger:
+ * - For a 4 KiB granule, a block descriptor in level -1 table would
+ *   correspond to a 256 TiB region.
+ * - For a 16 KiB granule, a block descriptor in level 0 table would
+ *   correspond to a 128 TiB region.
+ *
+ * The Answer to the Great Question of Life, the Universe, and Everything
+ * is thus Forty-two.
+ */
+#define MAX_REGION_MASK	ADDR_MASK(42)
+
 #define PTE_MASK(bits)		(((uint64_t)1<<bits) - 1)
 #define PTE_VAL(x, shift, bits)	(((x) >> (shift)) & PTE_MASK(bits))
 
@@ -131,13 +150,12 @@ pgt_aarch64(addrxlat_step_t *step)
 	step->base.as = step->meth->target_as;
 
 	if (PTE_TYPE(pte) == PTE_TYPE_BLOCK) {
-		if (step->remain == 1 ||
-		    step->remain > 4 ||
-		    (step->remain > 3 &&
-		     step->meth->param.pgt.pf.fieldsz[0] != 9))
-			return pte_invalid(step);
-		step->base.addr &= ~pf_table_mask(
+		addrxlat_addr_t mask = pf_table_mask(
 			&step->meth->param.pgt.pf, step->remain);
+		if (step->remain == 1 ||
+		    mask > MAX_REGION_MASK)
+			return pte_invalid(step);
+		step->base.addr &= ~mask;
 		return pgt_huge_page(step);
 	}
 

tests/addrxlat-invalid-aarch64-4k
@@ -4,23 +4,24 @@
 # Check invalid VTOP translation for AArch64 with 4k pages.
 #
 
-pf="aarch64:12,9,9,9,9"
+pf="aarch64:12,9,9,9,9,4"
 
 ptes="-e 0x0000:0x1003"		# PGD[0] -> 1000
-ptes="$ptes -e 0x0008:0x4000"	# PGD[1] -> INVALID
-ptes="$ptes -e 0x0010:0x5001"	# PGD[2] -> BLOCK (not allowed here)
-ptes="$ptes -e 0x1000:0x2003"	# PGD[0] -> PUD[0] -> 2000
-ptes="$ptes -e 0x1010:0x3c0000790" # PGD[0] -> PUD[2] -> INVALID
-ptes="$ptes -e 0x2000:0x3003"	# PGD[0] -> PUD[0] -> PMD[0] -> 3000
-ptes="$ptes -e 0x2010:0xe00790"	# PGD[0] -> PUD[0] -> PMD[2] -> INVALID
-ptes="$ptes -e 0x3000:0xa792"	# PGD[0] -> PUD[0] -> PMD[0] -> PTE[0] -> INVALID
-ptes="$ptes -e 0x3008:0xb791"	# PGD[0] -> PUD[0] -> PMD[0] -> PTE[1] -> RESERVED
+ptes="$ptes -e 0x0008:0xd000000000001"	# PGD[1] -> BLOCK (not allowed here)
+ptes="$ptes -e 0x1000:0x2003"	# PGD[0] -> P4D[0] -> 2000
+ptes="$ptes -e 0x1008:0x6000"	# PGD[0] -> P4D[1] -> INVALID
+ptes="$ptes -e 0x2000:0x3003"	# PGD[0] -> P4D[0] -> PUD[0] -> 3000
+ptes="$ptes -e 0x2010:0x3c0000790" # PGD[0] -> P4D[0] -> PUD[2] -> INVALID
+ptes="$ptes -e 0x3000:0x4003"	# PGD[0] -> P4D[0] -> PUD[0] -> PMD[0] -> 4000
+ptes="$ptes -e 0x3010:0xe00790"	# PGD[0] -> P4D[0] -> PUD[0] -> PMD[2] -> INVALID
+ptes="$ptes -e 0x4000:0xa792"	# PGD[0] -> P4D[0] -> PUD[0] -> PMD[0] -> PTE[0] -> INVALID
+ptes="$ptes -e 0x4008:0xb791"	# PGD[0] -> P4D[0] -> PUD[0] -> PMD[0] -> PTE[1] -> RESERVED
 
-list="0x123"			# PGD[0] -> PUD[0] -> PMD[0] -> PTE[0]
-list="0x1234"			# PGD[0] -> PUD[0] -> PMD[0] -> PTE[1]
-list="$list 0x523456"		# PGD[0] -> PUD[0] -> PMD[2]
-list="$list 0xa1234567"		# PGD[0] -> PUD[2]
-list="$list 0x808060489a"	# PGD[1]
-list="$list 0x123456789ab"	# PGD[2]
+list="0x123"			# PGD[0] -> P4D[0] -> PUD[0] -> PMD[0] -> PTE[0]
+list="$list 0x1234"		# PGD[0] -> P4D[0] -> PUD[0] -> PMD[0] -> PTE[1]
+list="$list 0x523456"		# PGD[0] -> P4D[0] -> PUD[0] -> PMD[2]
+list="$list 0xa1234567"		# PGD[0] -> P4D[0] -> PUD[2]
+list="$list 0x808060489a"	# PGD[0] -> P4D[1]
+list="$list 0x123456789abcd"	# PGD[1]
 
 . "$srcdir"/addrxlat-invalid