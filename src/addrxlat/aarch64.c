ptesarik/libkdumpfile

commit 22f4e8108704939a1d7ec398b94fc3b140dc8b99

Add AArch64 page table translation
Add code and test cases for Aarch64 page table translation. There is
only one PTE format. There are many paging variants on the AArch64,
but they differ only in the number and size of input address fields.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/addrxlat/aarch64.c
@@ -1,0 +1,141 @@
+/** @internal @file src/addrxlat/aarch64.c
+ * @brief Routines specific to ARM AArch64
+ */
+/* Copyright (C) 2020 Petr Tesarik <ptesarik@suse.com>
+
+   This file is free software; you can redistribute it and/or modify
+   it under the terms of either
+
+     * the GNU Lesser General Public License as published by the Free
+       Software Foundation; either version 3 of the License, or (at
+       your option) any later version
+
+   or
+
+     * the GNU General Public License as published by the Free
+       Software Foundation; either version 2 of the License, or (at
+       your option) any later version
+
+   or both in parallel, as here.
+
+   libkdumpfile is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received copies of the GNU General Public License and
+   the GNU Lesser General Public License along with this program.  If
+   not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+
+#include "addrxlat-priv.h"
+
+/* Maximum physical address bits (architectural limit) */
+#define PA_MAX_BITS	48
+#define PA_MASK		ADDR_MASK(PA_MAX_BITS)
+
+#define PTE_MASK(bits)		(((uint64_t)1<<bits) - 1)
+#define PTE_VAL(x, shift, bits)	(((x) >> (shift)) & PTE_MASK(bits))
+
+#define PTE_VALID(x)	PTE_VAL(x, 0, 1)
+#define PTE_TYPE(x)	((enum pte_type)PTE_VAL(x, 0, 2))
+
+/** Values for the @ref PTE_TYPE field. */
+enum pte_type {
+	PTE_TYPE_BLOCK = 1,	/**< Block descriptor. */
+	PTE_TYPE_TABLE = 3,	/**< Table descriptor. */
+};
+
+/** 1G page mask. */
+#define PAGE_MASK_1G		ADDR_MASK(30)
+
+/** Descriptive names for page tables.
+ * These names are used in error messages.
+ */
+static const char pgt_full_name[][16] = {
+	"Page",
+	"Level 3 table",
+	"Level 2 table",
+	"Level 1 table",
+};
+
+/** Short names for page table entries.
+ * These names are used in error messages. They are named after their
+ * use in the Linux kernel. This may have to change if you add support
+ * for other operating systems.
+ */
+static const char pte_name[][4] = {
+	"pte",
+	"pmd",
+	"pud",
+	"pgd",
+};
+
+/** Set an appropriate error message if the VALID bit is zero.
+ * @param step  Current step state.
+ * @returns     Error status.
+ */
+static addrxlat_status
+pte_not_present(addrxlat_step_t *step)
+{
+	return !step->ctx->noerr.notpresent
+		? set_error(step->ctx, ADDRXLAT_ERR_NOTPRESENT,
+			    "%s not present: %s[%u] = 0x%" ADDRXLAT_PRIxPTE,
+			    pgt_full_name[step->remain - 1],
+			    pte_name[step->remain - 1],
+			    (unsigned) step->idx[step->remain],
+			    step->raw.pte)
+		: ADDRXLAT_ERR_NOTPRESENT;
+}
+
+/** Set an appropriate error message if the entry contains invalid data.
+ * @param step  Current step state.
+ * @returns     Error status.
+ */
+static addrxlat_status
+pte_invalid(addrxlat_step_t *step)
+{
+	return set_error(step->ctx, ADDRXLAT_ERR_INVALID,
+			 "Invalid %s entry: %s[%u] = 0x%" ADDRXLAT_PRIxPTE,
+			 pgt_full_name[step->remain - 1],
+			 pte_name[step->remain - 1],
+			 (unsigned) step->idx[step->remain],
+			 step->raw.pte);
+}
+
+/** ARM AArch64 page table step function.
+ * @param step  Current step state.
+ * @returns     Error status.
+ */
+addrxlat_status
+pgt_aarch64(addrxlat_step_t *step)
+{
+	addrxlat_status status;
+
+	status = read_pte64(step);
+	if (status != ADDRXLAT_OK)
+		return status;
+
+	if (!PTE_VALID(step->raw.pte))
+		return pte_not_present(step);
+
+	step->base.addr = step->raw.pte & PA_MASK;
+	step->base.as = step->meth->target_as;
+
+	if (PTE_TYPE(step->raw.pte) == PTE_TYPE_BLOCK) {
+		addrxlat_addr_t mask = pf_table_mask(
+			&step->meth->param.pgt.pf, step->remain);
+		if (mask > PAGE_MASK_1G)
+			return pte_invalid(step);
+		step->base.addr &= ~mask;
+		return pgt_huge_page(step);
+	}
+
+	step->base.addr &= ~pf_page_mask(&step->meth->param.pgt.pf);
+	if (step->remain == 1)
+		step->elemsz = 1;
+
+	return ADDRXLAT_OK;
+}

examples/showxlat.c
@@ -68,6 +68,7 @@ print_pgt(const addrxlat_meth_t *meth)
 		[ADDRXLAT_PTE_NONE] = "none",
 		[ADDRXLAT_PTE_PFN32] = "pfn32",
 		[ADDRXLAT_PTE_PFN64] = "pfn64",
+		[ADDRXLAT_PTE_AARCH64] = "aarch64",
 		[ADDRXLAT_PTE_IA32] = "ia32",
 		[ADDRXLAT_PTE_IA32_PAE] = "ia32_pae",
 		[ADDRXLAT_PTE_X86_64] = "x86_64",

include/libkdumpfile/addrxlat.h.in
@@ -522,6 +522,7 @@ typedef enum _addrxlat_pte_format {
 	ADDRXLAT_PTE_NONE,	/**< Undefined. */
 	ADDRXLAT_PTE_PFN32,	/**< 32-bit page frame number. */
 	ADDRXLAT_PTE_PFN64,	/**< 64-bit page frame number. */
+	ADDRXLAT_PTE_AARCH64,	/**< ARM AArch64. */
 	ADDRXLAT_PTE_IA32,	/**< Original 32-bit Intel. */
 	ADDRXLAT_PTE_IA32_PAE,	/**< Intel IA32 with PAE. */
 	ADDRXLAT_PTE_X86_64,	/**< AMD64 (Intel 64).  */
@@ -544,6 +545,7 @@ addrxlat_pteval_shift(addrxlat_pte_format_t fmt)
 		return 2;
 
 	case ADDRXLAT_PTE_PFN64:
+	case ADDRXLAT_PTE_AARCH64:
 	case ADDRXLAT_PTE_IA32_PAE:
 	case ADDRXLAT_PTE_X86_64:
 	case ADDRXLAT_PTE_S390X:

src/addrxlat/Makefile.am
@@ -29,6 +29,7 @@ libaddrxlat_la_SOURCES = \
 	opt.c \
 	step.c \
 	sys.c \
+	aarch64.c \
 	ia32.c \
 	ppc64.c \
 	s390x.c \

src/addrxlat/addrxlat-priv.h
@@ -287,6 +287,8 @@ read_pte64(addrxlat_step_t *step)
 
 INTERNAL_DECL(addrxlat_status, pgt_huge_page, (addrxlat_step_t *state));
 
+INTERNAL_DECL(addrxlat_next_step_fn, pgt_aarch64, );
+
 INTERNAL_DECL(addrxlat_next_step_fn, pgt_ia32, );
 
 INTERNAL_DECL(addrxlat_next_step_fn, pgt_ia32_pae, );

src/addrxlat/step.c
@@ -274,6 +274,7 @@ first_step_pgt(addrxlat_step_t *step, addrxlat_addr_t addr)
 {
 	switch (step->meth->param.pgt.pf.pte_format) {
 	case ADDRXLAT_PTE_NONE:
+	case ADDRXLAT_PTE_AARCH64:
 	case ADDRXLAT_PTE_PPC64_LINUX_RPN30:
 		return first_step_pgt_generic(step, addr);
 
@@ -310,6 +311,9 @@ next_step_pgt(addrxlat_step_t *step)
 
 	case ADDRXLAT_PTE_PFN64:
 		return next_step_pfn64(step);
+
+	case ADDRXLAT_PTE_AARCH64:
+		return pgt_aarch64(step);
 
 	case ADDRXLAT_PTE_IA32:
 		return pgt_ia32(step);

tests/Makefile.am
@@ -146,6 +146,9 @@ test_scripts = \
 	addrxlat-identity \
 	addrxlat-pfn32 \
 	addrxlat-pfn64 \
+	addrxlat-aarch64-4k \
+	addrxlat-aarch64-16k \
+	addrxlat-aarch64-64k \
 	addrxlat-ia32 \
 	addrxlat-ia32-pae \
 	addrxlat-ppc64-64k \

tests/addrxlat-aarch64-16k
@@ -1,0 +1,31 @@
+#! /bin/sh
+
+#
+# Check VTOP translation for AArch64 with 16k pages.
+#
+
+pf="aarch64:14,11,11,11,1"
+
+ptes="-e 0x0000:0x4003"		# PGD[0] -> 4000
+ptes="$ptes -e 0x0008:0x14003"	# PGD[1] -> 14000
+ptes="$ptes -e 0x4000:0x8003"	# PGD[0] -> PUD[0] -> 8000
+ptes="$ptes -e 0x4918:0x3c003"	# PGD[0] -> PUD[291] -> 3c000
+ptes="$ptes -e 0x8000:0xc003"	# PGD[0] -> PUD[0] -> PMD[0] -> c000
+ptes="$ptes -e 0x8008:0x10003"	# PGD[0] -> PUD[0] -> PMD[1] -> 10000
+ptes="$ptes -e 0x8010:0xe000791" # PGD[0] -> PUD[0] -> PMD[2] -> e000000 (32M)
+ptes="$ptes -e 0xc000:0x28793"	# PGD[0] -> PUD[0] -> PMD[0] -> PTE[0] -> 28000
+ptes="$ptes -e 0xc008:0x2c793"	# PGD[0] -> PUD[0] -> PMD[0] -> PTE[1] -> 2c000
+ptes="$ptes -e 0x12000:0x30793"	# PGD[0] -> PUD[0] -> PMD[1] -> PTE[1024] -> 30000
+ptes="$ptes -e 0x14010:0x18003"	# PGD[1] -> PUD[2] -> 18000
+ptes="$ptes -e 0x18018:0x38003"	# PGD[1] -> PUD[2] -> PMD[3] -> 38000
+ptes="$ptes -e 0x38020:0x34793"	# PGD[1] -> PUD[2] -> PMD[3] -> PTE[4] -> 34000
+ptes="$ptes -e 0x3d158:0x123456000791" # PGD[0] -> PUD[291] -> PMD[555] -> directmap (32M)
+
+list="0x123:0x28123"		# PGD[0] -> PUD[0] -> PMD[0] -> PTE[0]
+list="$list 0x4234:0x2c234"	# PGD[0] -> PUD[0] -> PMD[0] -> PTE[1]
+list="$list 0x3000567:0x30567"	# PGD[0] -> PUD[0] -> PMD[1] -> PTE[1024]
+list="$list 0x80200601089a:0x3489a" # PGD[1] -> PUD[2] -> PMD[3] -> PTE[4]
+list="$list 0x4123456:0xe123456"    # PGD[0] -> PUD[0] -> PMD[1] (32M)
+list="$list 0xffff123456789abc:0x123456789abc" # PGD[0] -> PUD[291] -> PMD[555] (32M)
+
+. "$srcdir"/addrxlat-common

commit dac0d5669e18ee893ade3874b8f4268e8918d43b

AArch64: Fix the error message for an invalid page table entry
The table name is off by one.

Fixes: 22f4e8108704939a1d7ec398b94fc3b140dc8b99
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/addrxlat/aarch64.c
@@ -123,7 +123,7 @@ pte_invalid(addrxlat_step_t *step)
 {
 	return set_error(step->ctx, ADDRXLAT_ERR_INVALID,
 			 "Invalid %s entry: %s[%u] = 0x%" ADDRXLAT_PRIxPTE,
-			 pgt_full_name[step->remain - 1],
+			 pgt_full_name[step->remain],
 			 pte_name[step->remain - 1],
 			 (unsigned) step->idx[step->remain],
 			 step->raw.pte);