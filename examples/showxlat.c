8289427e814c7c3fc872cd7fa72a1914ffb027c5
Petr Tesarik <petr@tesarici.cz>
2023-07-04 15:42:53 +0200

    examples: add more override options to showxlat
    Allow to override more options if they cannot be guessed automatically:
    
    - architecture name,
    - any addrxlat options (default and forced).
    
    Signed-off-by: Petr Tesarik <petr@tesarici.cz>

--- examples/showxlat.c
+++ examples/showxlat.c
@@ -4,7 +4,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <inttypes.h>
+#include <getopt.h>
 
 #include <libkdumpfile/kdumpfile.h>
 #include <libkdumpfile/addrxlat.h>
@@ -235,29 +237,182 @@ dump_addrxlat(kdump_ctx_t *ctx)
 	return 0;
 }
 
+static int
+set_attr(kdump_ctx_t *ctx, const char *base, char *opt)
+{
+	kdump_attr_ref_t dirattr;
+	kdump_attr_ref_t subattr;
+	kdump_attr_t attr;
+	char *val, *endp;
+
+	val = strchr(opt, '=');
+	if (!val) {
+		fprintf(stderr, "%s missing value", opt);
+		return 1;
+	}
+	*val++ = 0;
+
+	if (kdump_attr_ref(ctx, base, &dirattr) != KDUMP_OK) {
+		fprintf(stderr, "Cannot reference %s: %s\n",
+			base, kdump_get_err(ctx));
+		return 2;
+	}
+
+	if (kdump_sub_attr_ref(ctx, &dirattr, opt, &subattr) != KDUMP_OK) {
+		fprintf(stderr, "Cannot reference %s.%s: %s\n",
+			base, opt, kdump_get_err(ctx));
+		return 2;
+	}
+
+	attr.type = kdump_attr_ref_type(&subattr);
+	switch (attr.type) {
+	case KDUMP_STRING:
+		attr.val.string = val;
+		break;
+
+	case KDUMP_NUMBER:
+		attr.val.number = strtoull(val, &endp, 0);
+		if (*endp) {
+			fprintf(stderr, "Invalid number: %s\n", val);
+			return 1;
+		}
+		break;
+
+	case KDUMP_ADDRESS:
+		attr.val.address = strtoull(val, &endp, 0);
+		if (*endp) {
+			fprintf(stderr, "Invalid address: %s\n", val);
+			return 1;
+		}
+		break;
+
+	default:
+		fprintf(stderr, "Unimplemented attribute type (%u)\n",
+			(unsigned) attr.type);
+		return 1;
+	}
+
+	if (kdump_attr_ref_set(ctx, &subattr, &attr) != KDUMP_OK) {
+		fprintf(stderr, "Cannot set %s.%s: %s\n",
+			base, opt, kdump_get_err(ctx));
+		return 2;
+	}
+
+	kdump_attr_unref(ctx, &subattr);
+	kdump_attr_unref(ctx, &dirattr);
+	return 0;
+}
+
+static int
+set_arch(kdump_ctx_t *ctx, const char *val)
+{
+	kdump_attr_t attr;
+
+	attr.type = KDUMP_STRING;
+	attr.val.string = val;
+	if (kdump_set_attr(ctx, KDUMP_ATTR_ARCH_NAME, &attr) != KDUMP_OK) {
+		fprintf(stderr, "Cannot set arch: %s\n",
+			kdump_get_err(ctx));
+		return 1;
+	}
+
+	return 0;
+}
+
+static int
+set_ostype(kdump_ctx_t *ctx, const char *val)
+{
+	kdump_attr_t attr;
+
+	attr.type = KDUMP_STRING;
+	attr.val.string = val;
+	if (kdump_set_attr(ctx, KDUMP_ATTR_OSTYPE, &attr) != KDUMP_OK) {
+		fprintf(stderr, "Cannot set ostype: %s\n",
+			kdump_get_err(ctx));
+		return 1;
+	}
+
+	return 0;
+}
+
+static void
+usage(FILE *out, const char *progname)
+{
+	fprintf(out, "Usage: %s [<options>] <dumpfile>\n\n", progname);
+	fputs("Options:\n"
+	      "  -h, --help\n\tShow this help\n"
+	      "  -a, --arch val\n\tSet architecture\n"
+	      "  -d, --default opt=val\n\tSet default addrxlat option\n"
+	      "  -f, --force opt=val\n\tForce addrxlat option\n"
+	      "  -o, --ostype val\n\tSet OS type\n",
+	      out);
+}
+
+static const struct option opts[] = {
+	{ "help", no_argument, NULL, 'h' },
+	{ "arch", required_argument, NULL, 'a' },
+	{ "default", required_argument, NULL, 'd' },
+	{ "force", required_argument, NULL, 'f' },
+	{ "ostype", required_argument, NULL, 'o' },
+	{ },
+};
+
 int
 main(int argc, char **argv)
 {
 	kdump_ctx_t *ctx;
+	int opt;
 	int fd;
-	kdump_attr_t attr;
 	kdump_status status;
-
-	if (argc < 2 || argc > 3) {
-		fprintf(stderr, "Usage: %s <dumpfile> [<ostype>]\n", argv[0]);
-		return 1;
-	}
-
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0) {
-		perror(argv[1]);
-		return 2;
-	}
 
 	ctx = kdump_new();
 	if (!ctx) {
 		perror("Cannot allocate kdump context");
 		return -1;
+	}
+
+	while ((opt = getopt_long(argc, argv, "ha:d:f:o:",
+				  opts, NULL)) != -1) {
+		switch (opt) {
+		case 'a':
+			if (set_arch(ctx, optarg))
+				return 1;
+			break;
+
+		case 'd':
+			if (set_attr(ctx, KDUMP_ATTR_XLAT_DEFAULT, optarg))
+				return 1;
+			break;
+
+		case 'f':
+			if (set_attr(ctx, KDUMP_ATTR_XLAT_FORCE, optarg))
+				return 1;
+			break;
+
+		case 'o':
+			if (set_ostype(ctx, optarg))
+				return 1;
+			break;
+
+		case 'h':
+			usage(stdout, argv[0]);
+			return 0;
+
+		case '?':
+			usage(stderr, argv[0]);
+			return 1;
+		}
+	}
+
+	if (optind != argc - 1) {
+		usage(stderr, argv[0]);
+		return 1;
+	}
+
+	fd = open(argv[optind], O_RDONLY);
+	if (fd < 0) {
+		perror(argv[optind]);
+		return 2;
 	}
 
 	status = kdump_open_fd(ctx, fd);
@@ -266,18 +421,6 @@ main(int argc, char **argv)
 			kdump_get_err(ctx));
 		kdump_free(ctx);
 		return 2;
-	}
-
-	if (argv[2]) {
-		attr.type = KDUMP_STRING;
-		attr.val.string = argv[2];
-	} else
-		attr.type = KDUMP_NIL;
-	status = kdump_set_attr(ctx, "addrxlat.ostype", &attr);
-	if (status != KDUMP_OK) {
-		fprintf(stderr, "Cannot set ostype: %s\n",
-			kdump_get_err(ctx));
-		return 1;
 	}
 
 	dump_addrxlat(ctx);
