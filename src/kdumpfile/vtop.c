ptesarik/libkdumpfile

commit 5049933c9523cf5238abb8a4662e4ae89156faa1

addrxlat: Split symbolic callback into one callback per type
The idea behind the clumsy API was that new symbolic information
types can be added without breaking binary compatibility. However,
the complexity moved into the callbacks. They had to deal with
unknown symbol types gracefully, they had to handle varying number
of arguments per type, etc.

After redesigning the callback API, new callbacks can be added
without all this complexity, so let's make life easier for library
users. Yes, it is more lines of code now, but those lines are
easier to grasp and maintain.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/kdumpfile/vtop.c
@@ -606,7 +606,7 @@ addrxlat_get_page(const addrxlat_cb_t *cb, addrxlat_buffer_t *buf)
  * @returns     Attribute data, or @c NULL on error.
  */
 static struct attr_data *
-sub_attr_addrxlat(kdump_ctx_t *ctx, struct attr_data *base, const char *name,
+sub_attr_xlat(kdump_ctx_t *ctx, struct attr_data *base, const char *name,
 		  const char *what)
 {
 	struct attr_data *attr;
@@ -614,104 +614,193 @@ sub_attr_addrxlat(kdump_ctx_t *ctx, struct attr_data *base, const char *name,
 	attr = lookup_dir_attr(ctx->dict, base, name, strlen(name));
 	if (!attr) {
 		addrxlat_ctx_err(ctx->xlatctx, ADDRXLAT_ERR_NODATA,
-				 "%s not found", what);
+				 "%s attribute not found", what);
 		return NULL;
 	}
 	if (!attr_isset(attr)) {
 		addrxlat_ctx_err(ctx->xlatctx, ADDRXLAT_ERR_NODATA,
-				 "%s has no value", what);
+				 "%s attribute is unset", what);
 		return NULL;
 	}
 	if (attr_revalidate(ctx, attr) != KDUMP_OK) {
 		addrxlat_ctx_err(ctx->xlatctx, ADDRXLAT_ERR_NODATA,
-				 "%s cannot be revalidated", what);
+				 "%s attribute cannot be revalidated", what);
 		return NULL;
 	}
 	return attr;
 }
 
-/** Symbolic callback using vmcoreinfo.
- * @param cb   This callback definition.
- * @param sym  Symbolic info metadata.
- * @returns    Error status.
- */
+/** Get register value from attributes (with locks held). */
 static addrxlat_status
-addrxlat_sym(const addrxlat_cb_t *cb, addrxlat_sym_t *sym)
+reg_value_locked(const addrxlat_cb_t *cb, const char *name,
+		 addrxlat_addr_t *val)
 {
 	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
-	struct attr_data *base;
 	struct attr_data *attr;
-	kdump_status status;
+
+	attr = lookup_attr(ctx->dict, "cpu.0.reg");
+	if (!attr)
+		return addrxlat_ctx_err(ctx->xlatctx, ADDRXLAT_ERR_NODATA,
+					"No registers");
+
+	attr = sub_attr_xlat(ctx, attr, name, "Register");
+	if (!attr)
+		return ADDRXLAT_ERR_NODATA;
+
+	*val = attr_value(attr)->number;
+	return ADDRXLAT_OK;
+}
+
+/** Register value callback. */
+static addrxlat_status
+reg_value(const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
 	addrxlat_status ret;
 
 	rwlock_rdlock(&ctx->shared->lock);
+	ret = reg_value_locked(cb, name, val);
+	rwlock_unlock(&ctx->shared->lock);
+	return ret;
+}
 
-	status = KDUMP_OK;
-	ret = ADDRXLAT_OK;
+/** Get symbol value from attributes (with locks held). */
+static addrxlat_status
+sym_value_locked(const addrxlat_cb_t *cb, const char *name,
+		 addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	struct attr_data *attr;
+	kdump_status status;
 
-	switch (sym->type) {
-	case ADDRXLAT_SYM_VALUE:
-		status = ostype_attr(ctx, "vmcoreinfo.SYMBOL", &base);
-		break;
-
-	case ADDRXLAT_SYM_SIZEOF:
-		status = ostype_attr(ctx, "vmcoreinfo.SIZE", &base);
-		break;
-
-	case ADDRXLAT_SYM_OFFSETOF:
-		status = ostype_attr(ctx, "vmcoreinfo.OFFSET", &base);
-		break;
-
-        case ADDRXLAT_SYM_NUMBER:
-		status = ostype_attr(ctx, "vmcoreinfo.NUMBER", &base);
-		break;
-
-	case ADDRXLAT_SYM_REG:
-		base = lookup_attr(ctx->dict, "cpu.0.reg");
-		if (!base)
-			ret = addrxlat_ctx_err(ctx->xlatctx, ADDRXLAT_ERR_NODATA,
-					   "No registers");
-		break;
-
-	default:
-		ret = addrxlat_ctx_err(ctx->xlatctx, ADDRXLAT_ERR_NOTIMPL,
-				       "Unhandled symbolic type");
-	}
+	status = ostype_attr(ctx, "vmcoreinfo.SYMBOL", &attr);
 	if (status != KDUMP_OK)
-		ret = kdump2addrxlat(ctx, status);
-	if (ret != ADDRXLAT_OK)
-		goto out;
+		return kdump2addrxlat(ctx, status);
 
-	attr = sub_attr_addrxlat(ctx, base, sym->args[0], "Symbol");
-	if (!attr) {
-		ret = ADDRXLAT_ERR_NODATA;
-		goto out;
-	}
+	attr = sub_attr_xlat(ctx, attr, name, "Symbol value");
+	if (!attr)
+		return ADDRXLAT_ERR_NODATA;
 
-	if (sym->type == ADDRXLAT_SYM_OFFSETOF) {
-		attr = sub_attr_addrxlat(ctx, attr, sym->args[1], "Field");
-		if (!attr) {
-			ret = ADDRXLAT_ERR_NODATA;
-			goto out;
-		}
-	}
+	*val = attr_value(attr)->address;
+	return ADDRXLAT_OK;
+}
 
-	ret = ADDRXLAT_OK;
-	switch (attr->template->type) {
-	case KDUMP_NUMBER:
-		sym->val = attr_value(attr)->number;
-		break;
+/** Symbol value callback. */
+static addrxlat_status
+sym_value(const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	addrxlat_status ret;
 
-	case KDUMP_ADDRESS:
-		sym->val = attr_value(attr)->address;
-		break;
+	rwlock_rdlock(&ctx->shared->lock);
+	ret = sym_value_locked(cb, name, val);
+	rwlock_unlock(&ctx->shared->lock);
+	return ret;
+}
 
-	default:
-		ret = addrxlat_ctx_err(ctx->xlatctx, ADDRXLAT_ERR_NOTIMPL,
-				       "Unhandled attribute type");
-	}
+/** Get symbol size from attributes (with locks held). */
+static addrxlat_status
+sym_sizeof_locked(const addrxlat_cb_t *cb, const char *name,
+		  addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	struct attr_data *attr;
+	kdump_status status;
 
- out:
+	status = ostype_attr(ctx, "vmcoreinfo.SIZE", &attr);
+	if (status != KDUMP_OK)
+		return kdump2addrxlat(ctx, status);
+
+	attr = sub_attr_xlat(ctx, attr, name, "Symbol size");
+	if (!attr)
+		return ADDRXLAT_ERR_NODATA;
+
+	*val = attr_value(attr)->number;
+	return ADDRXLAT_OK;
+}
+
+/** Symbol size callback. */
+static addrxlat_status
+sym_sizeof(const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	addrxlat_status ret;
+
+	rwlock_rdlock(&ctx->shared->lock);
+	ret = sym_sizeof_locked(cb, name, val);
+	rwlock_unlock(&ctx->shared->lock);
+	return ret;
+}
+
+/** Get element offset within an object from attributes (with locks held). */
+static addrxlat_status
+sym_offsetof_locked(const addrxlat_cb_t *cb, const char *obj, const char *elem,
+		    addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	struct attr_data *attr;
+	kdump_status status;
+
+	status = ostype_attr(ctx, "vmcoreinfo.OFFSET", &attr);
+	if (status != KDUMP_OK)
+		return kdump2addrxlat(ctx, status);
+
+	attr = sub_attr_xlat(ctx, attr, obj, "Container object");
+	if (!attr)
+		return ADDRXLAT_ERR_NODATA;
+
+	attr = sub_attr_xlat(ctx, attr, obj, "Field");
+	if (!attr)
+		return ADDRXLAT_ERR_NODATA;
+
+	*val = attr_value(attr)->number;
+	return ADDRXLAT_OK;
+}
+
+/** Element offset callback. */
+static addrxlat_status
+sym_offsetof(const addrxlat_cb_t *cb, const char *obj, const char *elem,
+	     addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	addrxlat_status ret;
+
+	rwlock_rdlock(&ctx->shared->lock);
+	ret = sym_offsetof_locked(cb, obj, elem, val);
+	rwlock_unlock(&ctx->shared->lock);
+	return ret;
+}
+
+/** Get number value from attributes (with locks held). */
+static addrxlat_status
+num_value_locked(const addrxlat_cb_t *cb, const char *name,
+		 addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	struct attr_data *attr;
+	kdump_status status;
+
+	status = ostype_attr(ctx, "vmcoreinfo.NUMBER", &attr);
+	if (status != KDUMP_OK)
+		return kdump2addrxlat(ctx, status);
+
+	attr = sub_attr_xlat(ctx, attr, name, "Number");
+	if (!attr)
+		return ADDRXLAT_ERR_NODATA;
+
+	*val = attr_value(attr)->number;
+	return ADDRXLAT_OK;
+}
+
+/** Number value callback. */
+static addrxlat_status
+num_value(const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val)
+{
+	kdump_ctx_t *ctx = (kdump_ctx_t*) cb->priv;
+	addrxlat_status ret;
+
+	rwlock_rdlock(&ctx->shared->lock);
+	ret = num_value_locked(cb, name, val);
 	rwlock_unlock(&ctx->shared->lock);
 	return ret;
 }
@@ -737,9 +826,13 @@ init_addrxlat(kdump_ctx_t *ctx)
 	}
 
 	cb->priv = ctx;
-	cb->sym = addrxlat_sym;
 	cb->get_page = addrxlat_get_page;
 	cb->read_caps = addrxlat_read_caps;
+	cb->reg_value = reg_value;
+	cb->sym_value = sym_value;
+	cb->sym_sizeof = sym_sizeof;
+	cb->sym_offsetof = sym_offsetof;
+	cb->num_value = num_value;
 
 	ctx->xlatctx = addrxlat;
 	ctx->xlatcb = cb;

include/libkdumpfile/addrxlat.h.in
@@ -225,99 +225,52 @@ const char *addrxlat_ctx_get_err(const addrxlat_ctx_t *ctx);
 /* Forward declaration to solve circular reference. */
 typedef struct _addrxlat_cb addrxlat_cb_t;
 
-/** Type of symbolic information. */
-typedef enum _addrxlat_sym_type {
-	/** Register value.
-	 * Input:
-	 * - @c args[0] = register name
-	 * Output:
-	 * - @c val = register value
-	 */
-	ADDRXLAT_SYM_REG,
-
-	/** Symbol value.
-	 * Input:
-	 * - @c args[0] = symbol name
-	 * Output:
-	 * - @c val = symbol value (e.g. address of a variable)
-	 */
-	ADDRXLAT_SYM_VALUE,
-
-	/** Size of an object.
-	 * Input:
-	 * - @c args[0] = name of symbol or data type
-	 * Output:
-	 * - @c val = @c sizeof(args[0])
-	 */
-	ADDRXLAT_SYM_SIZEOF,
-
-	/** Offset of a member within a structure.
-	 * Input:
-	 * - @c args[0] = container name (e.g. of a @c struct)
-	 * - @c args[1] = element name (e.g. a structure member)
-	 * Output:
-	 * - @c val = @c offsetof(args[0],args[1])
-	 */
-	ADDRXLAT_SYM_OFFSETOF,
-
-	/** Number value.
-	 * Input:
-	 * - @c args[0] = number name
-	 * Output:
-	 * - @c val = number value
-	 */
-	ADDRXLAT_SYM_NUMBER,
-} addrxlat_sym_type_t;
-
-/** Maximum argument count for @ref addrxlat_sym_t. */
-#define ADDRXLAT_SYM_ARGC_MAX	2
-
-/** Get the number of arguments for a given type.
- * @param type  Type of symbolic information.
- * @returns     Number of arguments, or -1 if unknown.
- *
- * The return value must not be greater than @ref ADDRXLAT_SYM_ARGC_MAX.
+/** Type of the register value callback.
+ * @param cb        This callback definition.
+ * @param name[in]  Register name.
+ * @param val[out]  Register value.
+ * @returns         Error status.
  */
-static inline int
-addrxlat_sym_argc(addrxlat_sym_type_t type)
-{
-	switch (type) {
-	case ADDRXLAT_SYM_REG:
-	case ADDRXLAT_SYM_VALUE:
-	case ADDRXLAT_SYM_SIZEOF:
-	case ADDRXLAT_SYM_NUMBER:
-		return 1;
+typedef addrxlat_status addrxlat_cb_reg_value_fn(
+	const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val);
 
-	case ADDRXLAT_SYM_OFFSETOF:
-		return 2;
-	}
-	return -1;
-}
-
-/** Data structure used to hold symbolic information. */
-typedef struct _addrxlat_sym {
-	/** [out] Resolved value. */
-	addrxlat_addr_t val;
-
-	/** [in] Type of information. */
-	addrxlat_sym_type_t type;
-
-	/** [in] Symbolic arguments. */
-	const char *args[ADDRXLAT_SYM_ARGC_MAX];
-} addrxlat_sym_t;
-
-/** Type of the symbolic information callback.
- * @param cb    This callback definition.
- * @param sym   Symbolic information, updated on success.
- * @returns     Error status.
- *
- * The callback function should check the information type and fill in
- * the output fields in @c sym. If it is called for a type that is not
- * handled (including an unknown type, not listed above), it must return
- * @ref ADDRXLAT_ERR_NOTIMPL.
+/** Type of the symbol value callback.
+ * @param cb        This callback definition.
+ * @param name[in]  Symbol name.
+ * @param val[out]  Symbol value (e.g. address of a variable).
+ * @returns         Error status.
  */
-typedef addrxlat_status addrxlat_sym_fn(
-	const addrxlat_cb_t *cb, addrxlat_sym_t *sym);
+typedef addrxlat_status addrxlat_cb_sym_value_fn(
+	const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val);
+
+/** Type of the symbol size callback.
+ * @param cb        This callback definition.
+ * @param name[in]  Name of a symbol or data type.
+ * @param val[out]  Symbol size, @c sizeof(name).
+ * @returns         Error status.
+ */
+typedef addrxlat_status addrxlat_cb_sym_sizeof_fn(
+	const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val);
+
+/** Type of the element offset callback.
+ * @param cb        This callback definition.
+ * @param obj[in]   Name of the container object (e.g. of a @c struct).
+ * @param elem[in]  Name of the element (e.g. a structure field).
+ * @param val[out]  Offset of @p elem within @p obj, @c offsetof(obj, elem).
+ * @returns         Error status.
+ */
+typedef addrxlat_status addrxlat_cb_sym_offsetof_fn(
+	const addrxlat_cb_t *cb, const char *obj, const char *elem,
+	addrxlat_addr_t *val);
+
+/** Type of the number value callback.
+ * @param cb        This callback definition.
+ * @param name[in]  Name of the numeric parameter.
+ * @param val[out]  Parameter value.
+ * @returns         Error status.
+ */
+typedef addrxlat_status addrxlat_cb_num_value_fn(
+	const addrxlat_cb_t *cb, const char *name, addrxlat_addr_t *val);
 
 /**  Data byte order.
  *
@@ -420,14 +373,26 @@ struct _addrxlat_cb {
 	 * The library will not touch it.  */
 	void *priv;
 
-	/** Symbolic information callback. */
-	addrxlat_sym_fn *sym;
-
 	/** Callback to get a page buffer. */
 	addrxlat_get_page_fn *get_page;
 
 	/** Read capabilities callback. */
 	addrxlat_read_caps_fn *read_caps;
+
+	/** Register value callback. */
+	addrxlat_cb_reg_value_fn *reg_value;
+
+	/** Symbol value callback. */
+	addrxlat_cb_sym_value_fn *sym_value;
+
+	/** Symbol size callback. */
+	addrxlat_cb_sym_sizeof_fn *sym_sizeof;
+
+	/** Element offset callback. */
+	addrxlat_cb_sym_offsetof_fn *sym_offsetof;
+
+	/** Number value callback. */
+	addrxlat_cb_num_value_fn *num_value;
 };
 
 /** Add a callback implementation.

commit b3d1d7db06408e90f59666b51b4ae7b4836f26d2

kdumpfile: Fix the sym_offsetof callback
Fix the element offset lookup in VMCOREINFO. The struct name was
incorrectly used instead again instead of the element name.

Fixes: 5049933c9523 ("addrxlat: Split symbolic callback into one callback per type")
Signed-off-by: Petr Tesarik <petr@tesarici.cz>

src/kdumpfile/vtop.c
@@ -750,7 +750,7 @@ sym_offsetof_locked(const addrxlat_cb_t *cb, const char *obj, const char *elem,
 	if (!attr)
 		return ADDRXLAT_ERR_NODATA;
 
-	attr = sub_attr_xlat(ctx, attr, obj, "Field");
+	attr = sub_attr_xlat(ctx, attr, elem, "Field");
 	if (!attr)
 		return ADDRXLAT_ERR_NODATA;
 