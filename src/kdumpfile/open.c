4555fca62ce438d5ef7752dbb738516f6b5d1213
Petr Tesarik <petr@tesarici.cz>
2023-11-08 09:34:19 +0100

    Fix kdump_open_fdset() for reopen
    It is wrong to clear the file.set.number attribute. It does not have a
    pre-clear hook anyway. The individual file descriptor attributes must be
    cleared instead, which is achieved by setting the attribute to zero.
    
    Signed-off-by: Petr Tesarik <petr@tesarici.cz>

--- src/kdumpfile/open.c
+++ src/kdumpfile/open.c
@@ -360,10 +360,11 @@ kdump_open_fdset(kdump_ctx_t *ctx, unsigned nfds, const int *fds)
 	clear_error(ctx);
 
 	/* Make sure we do not use a stale file descriptor value. */
-	clear_attr(ctx, gattr(ctx, GKI_num_files));
-
 	status = set_attr_number(ctx, gattr(ctx, GKI_num_files),
-				 ATTR_PERSIST, nfds);
+				 ATTR_PERSIST, 0);
+	if (status == KDUMP_OK)
+		status = set_attr_number(ctx, gattr(ctx, GKI_num_files),
+					 ATTR_PERSIST, nfds);
 	if (status != KDUMP_OK)
 		return set_error(ctx, status,
 				 "Cannot initialize file set size");
