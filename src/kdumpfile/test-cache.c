ptesarik/libkdumpfile

commit 487a8042ea5da580e1fdb5b8f91c8bd7cad05cd6

Cache: Calculate eprobe in reinit_entry()
If this function is called to reuse a ghost entry, the probe list
has not been walked yet, so eprobe is left uninitialized.

This passed the test case, because the correct old value was left
on stack. Modify the test case to poison the stack.

Signed-off-by: Petr Tesarik <petr@tesarici.cz>

src/kdumpfile/cache.c
@@ -269,7 +269,11 @@ reinit_entry(struct cache *cache, struct cache_entry *entry,
 
 	if (cache->nprec + cache->nprobe + cache->ninflight < cache->cap) {
 		/* Get an entry from the unused partition. */
-		evict = &cache->ce[cs->eprobe];
+		unsigned eprobe = cs->gprobe;
+		unsigned n = cache->ngprobe;
+		while (n--)
+			eprobe = cache->ce[eprobe].prev;
+		evict = &cache->ce[eprobe];
 	} else {
 		/* Get an unused cached entry. */
 		if (cs->nuprobe != 0 &&

src/kdumpfile/test-cache.c
@@ -40,6 +40,17 @@
 
 #define CACHE_SIZE  8
 
+static void
+poison_stack(void)
+{
+	unsigned short largearray[2048];
+	unsigned i;
+	for (i = 0; i < ARRAY_SIZE(largearray); ++i)
+		largearray[i] = 0xDEAD;
+	/* Do not let the compiler optimize it out. */
+	__asm__ volatile("" :: "g" (largearray) : "memory");
+}
+
 int
 main(int argc, char **argv)
 {
@@ -98,6 +109,7 @@ main(int argc, char **argv)
 	}
 	/* Put the newly allocated entry into the unused partition. */
 	cache_discard(cache, entry);
+	poison_stack();
 
 	/* Re-allocate the evicted entry. */
 	entry = cache_get_entry(cache, CACHE_SIZE - 1);
@@ -120,6 +132,7 @@ main(int argc, char **argv)
 	}
 	cache_insert(cache, entry);
 	cache_put_entry(cache, entry);
+	poison_stack();
 
 	/* Re-initialize from ghost list. */
 	entry = cache_get_entry(cache, CACHE_SIZE - 1);

commit e63dab40c8cefdfc013dc57915de9852dbb283e4

cache: Get data buffer before adjusting ghost partition size
Fix a bug in the handling of reused ghost entries. The problem is that
reclaim_data() finds the unused partition by skipping over the ghost probe
partition, but when reusing a ghost probe entry, cache->ngprobe was already
decremented in get_ghost_or_missed_entry().

Fixes: 487a8042ea5d ("Cache: Calculate eprobe in reinit_entry()")
Signed-off-by: Petr Tesarik <petr@tesarici.cz>

src/kdumpfile/cache.c
@@ -369,15 +369,12 @@ get_missed_entry(struct cache *cache, cache_key_t key,
  * @param cache  Cache object.
  * @param entry  Ghost entry to be reused.
  * @param idx    Index of @p entry.
- * @param cs     Cache search info.
  * @returns      The value of @p entry.
  */
 static struct cache_entry *
 reuse_ghost_entry(struct cache *cache, struct cache_entry *entry,
-		  unsigned idx, struct cache_search *cs)
+		  unsigned idx)
 {
-	entry->data = reclaim_data(cache, cs, 0);
-
 	if (cache->split == idx)
 		cache->split = entry->prev;
 
@@ -414,8 +411,9 @@ get_ghost_or_missed_entry(struct cache *cache, cache_key_t key,
 				cache->dprobe -= delta;
 			else
 				cache->dprobe = 0;
+			entry->data = reclaim_data(cache, cs, 0);
 			--cache->ngprec;
-			return reuse_ghost_entry(cache, entry, idx, cs);
+			return reuse_ghost_entry(cache, entry, idx);
 		}
 		idx = entry->next;
 	}
@@ -434,8 +432,9 @@ get_ghost_or_missed_entry(struct cache *cache, cache_key_t key,
 				cache->dprobe += delta;
 			else
 				cache->dprobe = cache->cap;
+			entry->data = reclaim_data(cache, cs, 0);
 			--cache->ngprobe;
-			return reuse_ghost_entry(cache, entry, idx, cs);
+			return reuse_ghost_entry(cache, entry, idx);
 		}
 		idx = entry->prev;
 	}

src/kdumpfile/test-cache.c
@@ -147,6 +147,59 @@ main(int argc, char **argv)
 	cache_insert(cache, entry);
 	cache_put_entry(cache, entry);
 
+	/*******************************************************************
+	 * Check NULL-pointer dereference reported by Stephen Brennan.
+	 */
+
+	/* Put something onto the probe list. */
+	entry = cache_get_entry(cache, CACHE_SIZE + 1);
+	if (!entry) {
+		fprintf(stderr, "Cannot allocate for probe\n");
+		return TEST_FAIL;
+	}
+	cache_insert(cache, entry);
+	cache_put_entry(cache, entry);
+
+	/* Evict the probe entry to the ghost probe partition. */
+	for (i = 0; i < CACHE_SIZE; ++i) {
+		poison_stack();
+		entry = cache_get_entry(cache, i);
+		if (!entry) {
+			fprintf(stderr, "Cannot find cached entry %u\n", i);
+			return TEST_FAIL;
+		}
+		if (!cache_entry_valid(entry))
+			cache_insert(cache, entry);
+		cache_put_entry(cache, entry);
+	}
+
+	/* Move an entry into the unused partition. */
+	entry = cache_get_entry(cache, CACHE_SIZE + 2);
+	if (!entry) {
+		fprintf(stderr, "Cannot allocate after reinit\n");
+		return TEST_FAIL;
+	}
+	if (!entry->data) {
+		fprintf(stderr, "NULL data for entry %u\n", CACHE_SIZE);
+		return TEST_FAIL;
+	}
+	cache_discard(cache, entry);
+	poison_stack();
+
+	/* Reuse the ghost probe entry. */
+	entry = cache_get_entry(cache, CACHE_SIZE + 1);
+	if (!entry) {
+		fprintf(stderr, "Cannot reuse ghost probe entry\n");
+		return TEST_FAIL;
+	}
+	if (!entry->data) {
+		fprintf(stderr, "NULL data for reused ghost probe entry %u\n",
+			CACHE_SIZE + 1);
+		return TEST_FAIL;
+	}
+	cache_insert(cache, entry);
+	cache_put_entry(cache, entry);
+
 	cache_free(cache);
 	return TEST_OK;
 }